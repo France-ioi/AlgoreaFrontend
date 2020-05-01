d626851c11d7411fd879d003a69a05a7496cb8b8
knight212 <topace124@outlook.com>
2020-05-01 23:53:02 +0800

    Desing/Dev multi-app restructured with angular.json update

---
+++ projects/dev/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+const providers = [];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+@NgModule({ })
+export class DevAppModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: AppModule,
+      providers: providers
+    }
+  }
+}
