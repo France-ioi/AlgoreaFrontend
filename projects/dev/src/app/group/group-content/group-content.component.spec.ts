97641079418ecd51edd5441d43020b271621779a
knight212 <topace124@outlook.com>
2020-05-21 23:24:45 +0800

    Add NoopAnimationsModule for material tab animations

--- projects/dev/src/app/group/group-content/group-content.component.spec.ts
+++ projects/dev/src/app/group/group-content/group-content.component.spec.ts
@@ -4,6 +4,7 @@ import { GroupContentComponent } from './group-content.component';
 import { GroupOverviewComponent } from './group-overview/group-overview.component';
 import { CoreModule } from 'core';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('GroupContentComponent', () => {
   let component: GroupContentComponent;
@@ -12,7 +13,8 @@ describe('GroupContentComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        CoreModule
+        CoreModule,
+        NoopAnimationsModule
       ],
       declarations: [GroupContentComponent, GroupOverviewComponent],
       schemas: [ NO_ERRORS_SCHEMA ]
