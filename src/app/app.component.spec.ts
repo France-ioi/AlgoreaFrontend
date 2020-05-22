5383b7ea38fbc71d24ae9754090068181669fe86
knight212 <topace124@outlook.com>
2020-05-22 23:44:05 +0800

    ng test for algorea completed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fracne-ioi'`, () => {
+  it(`should have as title 'algorea'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fracne-ioi');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fracne-ioi app is running!');
+    expect(app.title).toEqual('algorea');
   });
 });
