862ad01335be555de9fe27d2337c1b75c3333e04
knight212 <topace124@outlook.com>
2020-05-27 22:44:18 +0800

    Remove unnecessary end to end test case

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,12 +7,7 @@ describe('workspace-project App', () => {
   beforeEach(() => {
     page = new AppPage();
   });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('fracne-ioi app is running!');
-  });
-
+  
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
