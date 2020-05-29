9dd8caca0dfdcdf0c853341030d2c3e3374a68b3
knight212 <topace124@outlook.com>
2020-05-30 04:52:58 +0800

    Remove input values and get the group data from service and make each tab navigation possible

--- projects/dev/src/app/group/group-content/group-content.component.ts
+++ projects/dev/src/app/group/group-content/group-content.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ElementRef, EventEmitter, Input, Output } from '@angular/core';
 import * as _ from 'lodash';
+import { GroupService } from '../../shared/services/api/group.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Group, initializeGroup } from '../../shared/models/group.model';
 
 @Component({
   selector: 'app-group-content',
@@ -7,33 +10,57 @@ import * as _ from 'lodash';
   styleUrls: ['./group-content.component.scss'],
 })
 export class GroupContentComponent implements OnInit {
-  @Input() data;
-  @Input() empty;
 
-  @Output() expandWholeWidth = new EventEmitter<void>();
+  group: Group;
+  groupId: number = 0;
 
-  constructor(private elementRef: ElementRef) {}
-
-  ngOnInit() {}
-
-  onTabChange(e) {
-    const tabs = this.elementRef.nativeElement.querySelectorAll(
-      '.mat-tab-labels .mat-tab-label'
-    );
-    const activeTab = this.elementRef.nativeElement.querySelector(
-      '.mat-tab-labels .mat-tab-label.mat-tab-label-active'
-    );
-    tabs.forEach((tab) => {
-      tab.classList.remove('mat-tab-label-before-active');
-    });
-
-    const i = _.findIndex(tabs, activeTab);
-    if (i > 0) {
-      tabs[i - 1].classList.add('mat-tab-label-before-active');
-    }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private elementRef: ElementRef,
+    private groupService: GroupService,
+    private router: Router) {
+    this.group = initializeGroup();
   }
 
-  onExpandWidth(e) {
-    this.expandWholeWidth.emit(e);
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((routeParams) => {
+      this.groupId = routeParams.id;
+
+      this.groupService.getGroup(this.groupId).subscribe((group) => {
+        this.group = group;
+      });
+    });
+  }
+
+  onTabChange(_e) {
+    const tabsDom = this.elementRef.nativeElement.querySelectorAll(
+      '.mat-tab-labels .mat-tab-label'
+    );
+    const activeTabDom = this.elementRef.nativeElement.querySelector(
+      '.mat-tab-labels .mat-tab-label.mat-tab-label-active'
+    );
+    tabsDom.forEach((tabDom) => {
+      tabDom.classList.remove('mat-tab-label-before-active');
+    });
+
+    const iTab = _.findIndex(tabsDom, activeTabDom);
+    if (iTab > 0) {
+      tabsDom[iTab - 1].classList.add('mat-tab-label-before-active');
+    }
+
+    switch(iTab) {
+      case 0:
+        this.router.navigateByUrl(`/groups/${this.groupId}`);
+        break;
+      case 1:
+        this.router.navigateByUrl(`/groups/${this.groupId}/members`);
+        break;
+      case 2:
+        this.router.navigateByUrl(`/groups/${this.groupId}/managers`);
+        break;
+      case 3:
+        this.router.navigateByUrl(`/groups/${this.groupId}/settings`);
+        break;
+    }
   }
 }
