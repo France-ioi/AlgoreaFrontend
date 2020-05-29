be73e94740e7bd5595a70e08943355e6f2d6952a
knight212 <topace124@outlook.com>
2020-05-30 01:24:41 +0800

    Update group model according to api update

--- projects/dev/src/app/shared/models/group.model.ts
+++ projects/dev/src/app/shared/models/group.model.ts
@@ -4,13 +4,19 @@ export interface Group {
   type: string;
   grade: number;
   description: string;
-  activity_id: string;
   code: string;
   code_lifetime: string;
   code_expires_at: Date;
 
   current_user_is_manager: boolean;
   current_user_is_member: boolean;
+  current_user_can_grant_group_access: boolean;
+  current_user_can_manage: string;
+  current_user_can_watch_members: boolean;
+
+  open_activity_when_joining: boolean;
+  root_activity_id: string;
+  root_skill_id: string;
 
   is_open: boolean;
   is_public: boolean;
