87f7f1e1ee5c3ad5217c977f998be8578e822bd2
knight212 <topace124@outlook.com>
2020-05-30 04:39:26 +0800

    Update group routing module

--- projects/dev/src/app/group/group-routing.module.ts
+++ projects/dev/src/app/group/group-routing.module.ts
@@ -2,21 +2,58 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GroupComponent } from './group.component';
 import { GroupManageComponent } from './group-manage/group-manage.component';
+import { GroupContentComponent } from './group-content/group-content.component';
 
 const routes: Routes = [
   {
     path: 'managed',
-    component: GroupComponent,
     children: [
       {
         path: ':id',
         component: GroupManageComponent,
-      },
-    ],
+      }
+    ]
   },
   {
     path: 'memberships/:id',
     component: GroupComponent,
+  },
+  {
+    path: ':id',
+    children: [
+      {
+        path: 'overview',
+        component: GroupContentComponent,
+        data: {
+          active: 0
+        }
+      },
+      {
+        path: 'members',
+        component: GroupContentComponent,
+        data: {
+          active: 1
+        }
+      },
+      {
+        path: 'managers',
+        component: GroupContentComponent,
+        data: {
+          active: 2
+        }
+      },
+      {
+        path: 'settings',
+        component: GroupContentComponent,
+        data: {
+          active: 3
+        }
+      },
+      {
+        path: '**',
+        redirectTo: 'overview'
+      }
+    ]
   },
   {
     path: '**',
