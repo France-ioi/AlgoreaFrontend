c839c9822eb8739bac18c45f4cac635cdede653a
knight212 <topace124@outlook.com>
2020-05-30 04:37:31 +0800

    Update group model to export initialization function

--- projects/dev/src/app/shared/models/group.model.ts
+++ projects/dev/src/app/shared/models/group.model.ts
@@ -4,9 +4,9 @@ export interface Group {
   type: string;
   grade: number;
   description: string;
-  code: string;
-  code_lifetime: string;
-  code_expires_at: Date;
+  code?: string;
+  code_lifetime?: string;
+  code_expires_at?: Date;
 
   current_user_is_manager: boolean;
   current_user_is_member: boolean;
@@ -23,4 +23,29 @@ export interface Group {
   open_contest: boolean;
 
   created_at: Date;
+}
+
+export const initializeGroup = () : Group => {
+  return {
+    id: '',
+    name: '',
+    type: '',
+    grade: 0,
+    description: '',
+    code: '',
+    code_lifetime: '',
+    code_expires_at: new Date(),
+    current_user_is_manager: false,
+    current_user_is_member: false,
+    current_user_can_grant_group_access: false,
+    current_user_can_manage: 'none',
+    current_user_can_watch_members: false,
+    open_activity_when_joining: false,
+    root_activity_id: '',
+    root_skill_id: '',
+    is_open: false,
+    is_public: false,
+    open_contest: false,
+    created_at: new Date()
+  };
 }
