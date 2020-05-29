602b662819fcbc0675718eec75786f06fe8f8917
knight212 <topace124@outlook.com>
2020-05-30 05:04:27 +0800

    Fix 'ng test' errors

--- projects/dev/src/app/group/group-content/group-content.component.spec.ts
+++ projects/dev/src/app/group/group-content/group-content.component.spec.ts
@@ -4,21 +4,33 @@ import { GroupContentComponent } from './group-content.component';
 import { CoreModule } from 'core';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeGroup } from '../../shared/models/group.model';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('GroupContentComponent', () => {
   let component: GroupContentComponent;
   let fixture: ComponentFixture<GroupContentComponent>;
-  const mockData = {
-    ID: 50
-  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         CoreModule,
+        HttpClientTestingModule,
         NoopAnimationsModule,
+        RouterTestingModule
       ],
       declarations: [GroupContentComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({id: 50})
+          }
+        },
+      ],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
       .compileComponents();
@@ -27,7 +39,7 @@ describe('GroupContentComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GroupContentComponent);
     component = fixture.componentInstance;
-    component.data = mockData;
+    component.group = initializeGroup();
     fixture.detectChanges();
   });
 
