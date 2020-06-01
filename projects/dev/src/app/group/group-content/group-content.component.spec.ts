8cd70b927a37c34b461a43077b2e48592ed2addf
knight212 <topace124@outlook.com>
2020-06-01 17:28:29 +0800

    Add value to ActivatedRoute to fix `ng test` errors

--- projects/dev/src/app/group/group-content/group-content.component.spec.ts
+++ projects/dev/src/app/group/group-content/group-content.component.spec.ts
@@ -27,7 +27,8 @@ describe('GroupContentComponent', () => {
         {
           provide: ActivatedRoute,
           useValue: {
-            params: of({id: 50})
+            params: of({id: 50}),
+            url: of([{path: 'overview'}])
           }
         },
       ],
