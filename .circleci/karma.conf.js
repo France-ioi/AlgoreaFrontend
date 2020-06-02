5272ba6ae56e9bb634f6387d0a4a34874fae98fe
Damien Leroy <dleroy@smad.be>
2020-06-02 17:21:22 +0200

    small tweaks for testing on CI

--- .circleci/karma.conf.js
+++ .circleci/karma.conf.js
@@ -27,6 +27,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: true,
     restartOnFileChange: false
   });
