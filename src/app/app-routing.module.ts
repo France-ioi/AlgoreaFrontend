2ef90cf63cf583eff30a8625bc0dbae15fbad88a
Damien Leroy <dleroy@smad.be>
2020-06-25 17:49:32 +0200

    fix recursive routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: AppComponent,
-    children: [
-      {
-        path: 'groups',
-        loadChildren: () => import('./group/group.module').then(m => m.GroupModule)
-      }
-    ]
+    path: 'groups',
+    loadChildren: () => import('./group/group.module').then(m => m.GroupModule)
   }
 ];
 
