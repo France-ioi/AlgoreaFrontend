46b76e3e27b45abfa44b3892ca572b9f9ccfce9a
Damien Leroy <dleroy@smad.be>
2020-06-29 14:31:09 +0200

    use only one lint file to avoid confusion

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,12 @@
-/**
- * We are using the .JS version of an ESLint config file here so that we can
- * add lots of comments to better explain and document the setup.
- */
 module.exports = {
-  // these are not required if we do not require type check
-  // parser: '@typescript-eslint/parser',
-  // parserOptions: {
-  //   tsconfigRootDir: __dirname,
-  //   project: './tsconfig.json'
-  // },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
   extends: [
     "eslint:recommended",
     'plugin:@typescript-eslint/eslint-recommended',
-    // 'plugin:@typescript-eslint/recommended-requiring-type-checking', // very slow
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:@angular-eslint/recommended'
   ],
   rules: {
@@ -38,26 +32,12 @@ module.exports = {
     "@typescript-eslint/prefer-for-of": ["error"],
   },
   overrides: [
-    /**
-     * This extra piece of configuration is only necessary if you make use of inline
-     * templates within Component metadata, e.g.:
-     *
-     * @Component({
-     *  template: `<h1>Hello, World!</h1>`
-     * })
-     * ...
-     *
-     * It is not necessary if you only use .html files for templates.
-     */
-    // {
-    //   files: ['*.component.ts'],
-    //   parser: '@typescript-eslint/parser',
-    //   parserOptions: {
-    //     ecmaVersion: 2020,
-    //     sourceType: 'module',
-    //   },
-    //   plugins: ['@angular-eslint/template'],
-    //   processor: '@angular-eslint/template/extract-inline-html',
-    // },
+    {
+      files: ['*.spec.ts'],
+      rules: {
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/unbound-method': 'off',
+      }
+    },
   ],
 };
