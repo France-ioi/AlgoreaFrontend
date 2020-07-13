aa482147fd9e322690bcc665d4d84ebe00cdecf5
Damien Leroy <dleroy@smad.be>
2020-07-13 12:11:39 +0200

    lodash->lodash-es as lodash itself is not an ECMAScript module

--- src/app/core/http-services/get-joined-groups.service.ts
+++ src/app/core/http-services/get-joined-groups.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
-import * as _ from 'lodash';
+import * as _ from 'lodash-es';
 
 export interface Group {
   id: string,
