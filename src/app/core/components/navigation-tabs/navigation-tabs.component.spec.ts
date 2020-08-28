900e1e3dfd6dc7a5e472fa4b3f85c11cad18b442
Damien Leroy <dleroy@smad.be>
2020-08-28 14:58:20 +0200

    fix unit (after removing the static data)

--- src/app/core/components/navigation-tabs/navigation-tabs.component.spec.ts
+++ src/app/core/components/navigation-tabs/navigation-tabs.component.spec.ts
@@ -4,130 +4,14 @@ import { NavigationTabsComponent } from './navigation-tabs.component';
 import { AppModule } from '../../../core/app.module';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+import { CurrentUserService } from 'src/app/shared/services/current-user.service';
+import { empty } from 'rxjs';
 
 describe('NavigationTabsComponent', () => {
   let component: NavigationTabsComponent;
   let fixture: ComponentFixture<NavigationTabsComponent>;
-  const mockCurrentUser = {
-    ID: 1,
-    title: 'Lionel MESSI',
-    avatar: '_messi.jpg',
-    type: 'user'
-  };
-
-  const mockItems = {
-    groups: {
-      manage: [
-        {
-          ID: 50,
-          title: 'Group 50',
-          type: 'leaf',
-          icons: 'fa fa-home'
-        },
-        {
-          ID: 51,
-          title: 'Group 51',
-          type: 'leaf',
-          icons: 'fa fa-home'
-        }
-      ],
-      join: [
-        {
-          title: 'Pictures',
-          type: 'folder',
-          children: [
-            {
-              title: 'barcelona.jpg',
-              type: 'leaf'
-            },
-            {
-              title: 'logo.jpg',
-              type: 'leaf'
-            },
-            {
-              title: 'primeui.png',
-              type: 'leaf'
-            }
-          ]
-        },
-        {
-          title: 'Movies',
-          type: 'folder',
-          children: [
-            {
-              title: 'Al Pacino',
-              type: 'folder'
-            },
-            {
-              title: 'Robert De Niro',
-              type: 'folder'
-            }
-          ]
-        }
-      ]
-    },
-    skills: {
-      title: 'Algorithmic skills',
-      data: [
-        {
-          ID: 12,
-          title: 'Graphs: methods',
-          type: 'leaf',
-          ring: false,
-          state: 'started',
-          progress: {
-            displayedScore: 100,
-            currentScore: 100
-          }
-        },
-        {
-          ID: 13,
-          title: 'List graph caracteristics',
-          type: 'leaf',
-          ring: false,
-          state: 'never opened',
-          progress: {
-            displayedScore: 0,
-            currentScore: 0
-          }
-        }
-      ]
-    },
-    activities: {
-      title: 'My Activities',
-      data: [
-        {
-          ID: 26,
-          title: 'Activities to test mosaic/list modes',
-          type: 'leaf',
-          ring: true,
-          state: 'never opened',
-          progress: {
-            displayedScore: 0,
-            currentScore: 0
-          },
-          category: {
-            icon: 'fa fa-book-open',
-            type: 0
-          }
-        },
-        {
-          ID: 37,
-          title: 'Activities to test headers',
-          type: 'leaf',
-          ring: true,
-          state: 'opened',
-          progress: {
-            displayedScore: 20,
-            currentScore: 20
-          },
-        },
-      ]
-    }
-  };
 
   beforeEach(async(() => {
-    const locationSpy = jasmine.createSpyObj<Location>('Location', []);
     TestBed.configureTestingModule({
       imports: [
         AppModule,
@@ -135,8 +19,10 @@ describe('NavigationTabsComponent', () => {
       ],
       providers: [
         {
-          provide: Location,
-          useClass: locationSpy
+          provide: CurrentUserService,
+          useValue: {
+            currentUser: () => empty()
+          }
         }
       ],
       declarations: [ NavigationTabsComponent ],
@@ -148,13 +34,10 @@ describe('NavigationTabsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavigationTabsComponent);
     component = fixture.componentInstance;
-    component.currentUser = mockCurrentUser;
-    component.items = mockItems;
     fixture.detectChanges();
   });
 
-  // Test to be fixed !
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
