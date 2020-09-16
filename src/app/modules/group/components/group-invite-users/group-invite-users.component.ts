863d0b6f82ff52b7ca6b2c07d8ba45a47167e4be
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-09-16 09:53:01 +0200

    update invite button depending on textarea content

--- src/app/modules/group/components/group-invite-users/group-invite-users.component.ts
+++ src/app/modules/group/components/group-invite-users/group-invite-users.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import { Group } from '../../http-services/get-group-by-id.service';
 
+interface Message
+{
+  type: 'success' | 'info' | 'error',
+  summary?:string,
+  detail:string
+}
+
 @Component({
   selector: 'alg-group-invite-users',
   templateUrl: './group-invite-users.component.html',
@@ -11,14 +18,54 @@ export class GroupInviteUsersComponent implements OnInit {
   @Input() group: Group
   @Output() refreshRequired = new EventEmitter<void>();
 
-  showMsg = false;
+
+  logins:string[];
+
+  processing = false;
+
+  messages : Message[] = [];
+
+  canClick:boolean;
 
   constructor() { }
 
   ngOnInit(): void {}
 
-  onInvite() {
-    this.showMsg = !this.showMsg;
+  sendInvites(_logins:string[])
+  {
+    //TODO
   }
 
+  /* events */
+
+  changeText(text:string)
+  {
+    this.canClick = true;
+
+    if (text.length == 0)
+    {
+      this.canClick = false;
+      return;
+    }
+
+    this.logins = text.split(',');
+
+    if (this.logins.length >= 100)
+    {
+      this.canClick = false;
+      this.messages = [{type:'error', detail:'You cannot invite more than 100 users at once'}];
+      return;
+    }
+
+    this.messages = [];
+  }
+
+  invite() {
+    this.processing = true;
+
+    this.messages = [];
+
+    this.sendInvites(this.logins);
+
+  }
 }
