5c472beba01fdece7faea82226c53464f791aa33
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-09-17 09:02:40 +0200

    add missing HttpClientTestingModule import

--- src/app/modules/group/http-services/create-group-invitations.service.spec.ts
+++ src/app/modules/group/http-services/create-group-invitations.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CreateGroupInvitationsService } from './create-group-invitations.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('CreateGroupInvitationsService', () => {
   let service: CreateGroupInvitationsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(CreateGroupInvitationsService);
   });
 
