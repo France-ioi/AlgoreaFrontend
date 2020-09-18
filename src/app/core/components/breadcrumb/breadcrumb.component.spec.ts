c2c61de171ebafc56cb269cdd5fbf5010fa35429
Damien Leroy <dleroy@smad.be>
2020-09-18 17:54:37 +0200

    fix unit tests

--- src/app/core/components/breadcrumb/breadcrumb.component.spec.ts
+++ src/app/core/components/breadcrumb/breadcrumb.component.spec.ts
@@ -5,21 +5,6 @@ import { BreadcrumbComponent } from './breadcrumb.component';
 describe('BreadcrumbComponent', () => {
   let component: BreadcrumbComponent;
   let fixture: ComponentFixture<BreadcrumbComponent>;
-  const mockItems = {
-    selectedID: '42',
-    path: [
-      { ID: '1', label: 'Contest', separator: 'slash' },
-      {
-        ID: '42',
-        label: 'Personalized contest',
-        attempt: 12,
-        separator: 'arrow'
-      },
-      { ID: '43', label: 'Personalized contests', attempt: 12 },
-      { ID: '23', label: 'IOI Selection 2012', attempt: 2 },
-      { ID: '24', label: 'Individuals', separator: 'slash' }
-    ]
-  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -31,7 +16,6 @@ describe('BreadcrumbComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BreadcrumbComponent);
     component = fixture.componentInstance;
-    component.items = mockItems;
     fixture.detectChanges();
   });
 
