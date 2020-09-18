7610036db7d99bba1dc0a4a03a5460ab210393ec
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-09-18 14:34:38 +0200

    add get group managers service

---
+++ src/app/modules/group/http-services/get-group-managers.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface Manager {
+  id: string,
+  name: string,
+
+  can_manage: string,
+  can_grant_group_access: boolean,
+  can_watch_members: boolean,
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetGroupManagersService {
+
+  constructor(private http: HttpClient) { }
+
+  getGroupManagers(
+    group_id: string,
+    sort: string[] = [],
+    ): Observable<Manager[]> {
+    let params = new HttpParams();
+    if (sort.length > 0) {
+      params = params.set('sort', sort.join(','));
+    }
+    return this.http
+      .get<Manager[]>(`${environment.apiUrl}/groups/${group_id}/managers`, { params: params });
+  }
+}
