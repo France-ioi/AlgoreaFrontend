1bcd48a842abdd041ace860dfdaa556d9b49534f
Damien Leroy <dleroy@smad.be>
2020-09-21 16:04:27 +0200

    fetch breadcrumb and inject it to breadcrumb

--- src/app/modules/item/pages/item-details/item-details.component.spec.ts
+++ src/app/modules/item/pages/item-details/item-details.component.spec.ts
@@ -5,6 +5,7 @@ import { CurrentContentService } from 'src/app/shared/services/current-content.s
 import { NavItem } from 'src/app/shared/services/nav-types';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { GetBreadcrumbService } from '../../http-services/get-breadcrumb.service';
 
 describe('ItemDetailsComponent', () => {
   let component: ItemDetailsComponent;
@@ -21,7 +22,17 @@ describe('ItemDetailsComponent', () => {
         { provide: ActivatedRoute, useValue: {
           paramMap: of({
             get: (_s: string) => '30'
-          })
+          }),
+          snapshot: {
+            paramMap: {
+              get: (_s: string) => '30',
+              has: (_s: string) => true,
+            }
+          }
+        }},
+        { provide: GetBreadcrumbService, useValue: {
+          getBreadcrumb: (_p: any, _a: any) => of([]),
+          getBreadcrumbWithParentAttempt: (_p: any, _a: any) => of([]),
         }}
       ]
     })
--- src/app/modules/item/pages/item-details/item-details.component.ts
+++ src/app/modules/item/pages/item-details/item-details.component.ts
@@ -1,39 +1,81 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CurrentContentService } from 'src/app/shared/services/current-content.service';
+import { Component, OnDestroy } from '@angular/core';
+import { CurrentContentService, PageInfo } from 'src/app/shared/services/current-content.service';
 import { ActivatedRoute } from '@angular/router';
-import { itemFromDetailParams } from 'src/app/shared/services/nav-types';
+import { itemFromDetailParams, NavItem, pathGiven } from 'src/app/shared/services/nav-types';
+import { GetBreadcrumbService, BreadcrumbItem } from 'src/app/modules/item/http-services/get-breadcrumb.service';
+import { EMPTY, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'alg-item-details',
   templateUrl: './item-details.component.html',
   styleUrls: ['./item-details.component.scss']
 })
-export class ItemDetailsComponent implements OnInit, OnDestroy {
+export class ItemDetailsComponent implements OnDestroy {
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private currentContent: CurrentContentService,
+    private getBreadcrumbService: GetBreadcrumbService,
   ) {
     activatedRoute.paramMap.subscribe((params) => {
-      const item = itemFromDetailParams(params);
-      if (item) currentContent.setCurrent(item);
-    });
-  }
-
-  ngOnInit() {
-    this.currentContent.setPageInfo({
-      category: 'Items',
-      breadcrumb: [
-        { title: 'RootChapter' },
-        { title: 'MyChapter', attemptOrder: 1 },
-        { title: 'Content' }
-      ],
-      currentPageIndex: 2
+      const navItem = itemFromDetailParams(params);
+      if (navItem) {
+        currentContent.setCurrent(navItem);
+        this.fetchItem();
+      }
     });
   }
 
   ngOnDestroy() {
     this.currentContent.setPageInfo(null);
+  }
+
+  private fetchItem() {
+    if (!pathGiven(this.activatedRoute.snapshot.paramMap)) {
+      // TODO: handle no path given
+      return;
+    }
+    const navItem = itemFromDetailParams(this.activatedRoute.snapshot.paramMap);
+    if (!navItem) return; // unexpected anyway
+    if (!navItem.attemptId && !navItem.parentAttemptId) {
+      // TODO: handle no attempt given
+      return;
+    }
+    this.breadcumbRequest(navItem).subscribe((pageInfo) => {
+      this.currentContent.setPageInfo(pageInfo);
+    });
+  }
+
+  private breadcumbRequest(navItem: NavItem): Observable<PageInfo> {
+    const service = this.breadcrumbService(navItem);
+    if (!service) return EMPTY; // unexpected as it should verified by the caller of this function
+    return service.pipe(
+      map((res) => {
+        // TODO: handle forbidden currently (handle like no path given) -- while not handled: consider as the other errors
+        if (res === 'forbidden') throw new Error('unhandled forbidden');
+        return {
+          category: 'Items',
+          breadcrumb: res.map((el) => {
+            return {
+              title: el.title,
+              attemptOrder: el.attemptCnt
+            };
+          }),
+          currentPageIndex: res.length - 1
+        };
+      })
+    );
+  }
+
+  /**
+   * Return the observable to the suitable breadcrumb service depending on the navitem, or undefined if no attempt is given.
+   */
+  private breadcrumbService(navItem: NavItem): Observable<BreadcrumbItem[]|'forbidden'>|undefined {
+    const fullPath = navItem.itemPath.concat([navItem.itemId]);
+    if (navItem.attemptId) return this.getBreadcrumbService.getBreadcrumb(fullPath, navItem.attemptId);
+    else if (navItem.parentAttemptId) return this.getBreadcrumbService.getBreadcrumbWithParentAttempt(fullPath, navItem.parentAttemptId);
+    else return undefined;
   }
 
 }
