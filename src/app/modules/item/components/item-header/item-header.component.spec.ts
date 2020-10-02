ecd86278ad9fb1eacae8eba8f44c731b66829329
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-10-02 15:28:39 +0200

    fix item header testing

--- src/app/modules/item/components/item-header/item-header.component.spec.ts
+++ src/app/modules/item/components/item-header/item-header.component.spec.ts
@@ -1,21 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { AppModule } from 'src/app/core/app.module';
+import { Item } from '../../http-services/get-item-by-id.service';
 
 import { ItemHeaderComponent } from './item-header.component';
 
 describe('ItemHeaderComponent', () => {
   let component: ItemHeaderComponent;
   let fixture: ComponentFixture<ItemHeaderComponent>;
+  const mockData: Item = {
+    id: '11',
+    requires_explicit_entry: true,
+    string: {
+    title: 'mock item',
+    },
+    permissions: {
+      can_view: 'content',
+    },
+  };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ItemHeaderComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule],
+      declarations: [ ItemHeaderComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemHeaderComponent);
     component = fixture.componentInstance;
+    component.item = mockData;
     fixture.detectChanges();
   });
 
