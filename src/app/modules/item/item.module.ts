97d87e0eb2b5916e7f129bc2b65a15c7e50a1135
Damien Leroy <dleroy@smad.be>
2020-10-06 14:43:20 +0200

    add empty item edit comp

--- src/app/modules/item/item.module.ts
+++ src/app/modules/item/item.module.ts
@@ -4,11 +4,13 @@ import { ItemRoutingModule } from './item-routing.module';
 import { SharedComponentsModule } from '../shared-components/shared-components.module';
 import { ItemDetailsComponent } from './pages/item-details/item-details.component';
 import { ItemHeaderComponent } from './components/item-header/item-header.component';
+import { ItemEditComponent } from './pages/item-edit/item-edit.component';
 
 @NgModule({
   declarations: [
     ItemDetailsComponent,
     ItemHeaderComponent,
+    ItemEditComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/item/pages/item-edit/item-edit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alg-item-edit',
+  templateUrl: './item-edit.component.html',
+  styleUrls: ['./item-edit.component.scss']
+})
+export class ItemEditComponent {}
