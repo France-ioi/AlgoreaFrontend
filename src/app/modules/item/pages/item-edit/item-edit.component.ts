3126e0af36b2a1e0fc1240383f611cbf030e2e27
Amaury Delprat <amaury.delprat@epita.fr>
2020-10-14 15:46:59 +0200

    Initializing form input values

--- src/app/modules/item/pages/item-edit/item-edit.component.ts
+++ src/app/modules/item/pages/item-edit/item-edit.component.ts
@@ -1,5 +1,19 @@
-import { Component, OnDestroy } from '@angular/core';
-import { CurrentContentService } from 'src/app/shared/services/current-content.service';
+import { Component, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { CurrentContentService, EditAction } from 'src/app/shared/services/current-content.service';
+import { ItemDataSource } from '../../services/item-datasource.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { UpdateItemStringService } from '../../http-services/update-item-string.service';
+import { Item } from '../../http-services/get-item-by-id.service';
+
+interface itemValues {
+  title: string
+}
+
+const defaultItemValues: itemValues = {
+  title: ''
+};
 
 @Component({
   selector: 'alg-item-edit',
@@ -7,15 +21,51 @@ import { CurrentContentService } from 'src/app/shared/services/current-content.s
   styleUrls: ['./item-edit.component.scss']
 })
 export class ItemEditComponent implements OnDestroy {
+  item: Item;
+  itemForm: FormGroup;
+  itemValues: itemValues;
+
+  itemSubscription: Subscription;
+
+  changedInput: { [input: string]: string } = {};
 
   constructor(
     private currentContent: CurrentContentService,
+    private itemDataSource: ItemDataSource,
+    private formBuilder: FormBuilder,
   ) {
     this.currentContent.editState.next('editing');
+    this.getCurrentItem();
+    this.initForm();
+    this.itemValues = defaultItemValues;
   }
 
   ngOnDestroy() {
     this.currentContent.editState.next('non-editable');
+    this.itemSubscription.unsubscribe();
+  }
+
+  initForm(): void {
+    this.itemForm = this.formBuilder.group({
+      title: [defaultItemValues.title]
+    });
+  }
+
+  getCurrentItem(): void {
+    this.itemSubscription = this.itemDataSource.item$.subscribe(item => {
+      this.item = item;
+      this.itemValues.title = item.string.title ? item.string.title : '';
+      this.itemForm.patchValue({
+        title: item.string.title
+      });
+    });
+  }
+
+  onInputChange(name: string, value: string): void {
+    if (typeof (value) == 'object')
+      return;
+
+    this.changedInput[name] = value;
   }
 
 }
