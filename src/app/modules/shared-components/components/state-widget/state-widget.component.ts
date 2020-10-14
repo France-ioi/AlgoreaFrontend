ea484405f46769e4b98a1b37545a005000a61590
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-10-14 17:17:13 +0200

    refact state widget component

--- src/app/modules/shared-components/components/state-widget/state-widget.component.ts
+++ src/app/modules/shared-components/components/state-widget/state-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'alg-state-widget',
@@ -6,15 +6,30 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./state-widget.component.scss'],
 })
 export class StateWidgetComponent implements OnInit {
-  @Input() icon: string;
-  @Input() type: number;
+
+  @Input() type: 'Undefined'|'Discovery'|'Application'|'Validation'|'Challenge';
   @Input() disabled = false;
 
-  labels = ['Discovery', 'Practice', 'Validation', 'Challenge', 'Review'];
+  icon = '';
 
-  classes = ['discovery', 'practice', 'validation', 'challenge', 'review'];
+  ngOnInit(): void {
 
-  constructor() {}
-
-  ngOnInit() {}
+    switch (this.type) {
+      case 'Undefined':
+        this.icon = 'fa fa-book-open';
+        break;
+      case 'Discovery':
+        this.icon = 'fa fa-book-open';
+        break;
+      case 'Application':
+        this.icon = 'fa fa-code';
+        break;
+      case 'Validation':
+        this.icon = 'fa fa-video';
+        break;
+      case 'Challenge':
+        this.icon = 'fa fa-laptop-code';
+        break;
+    }
+  }
 }
