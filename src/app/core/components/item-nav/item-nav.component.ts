e0bd5b54c7b66f01b68e0d5e1202ed55303442ab
Damien Leroy <dleroy@smad.be>
2020-10-21 17:30:14 +0200

    do not update the current item already displayed in the switch map as it might cancel the ongoing children fetching

--- src/app/core/components/item-nav/item-nav.component.ts
+++ src/app/core/components/item-nav/item-nav.component.ts
@@ -126,11 +126,13 @@ export class ItemNavComponent implements OnInit, OnDestroy {
   ngOnInit() {
 
     this.subscriptions.push(
+
+      // This first subscription only follow change in the current item id and use switch map to cancel previous requests
       this.currentContent.currentContent$.pipe(
 
         // we are only interested in items
         map(content => (content !== null && isItemInfo(content) ? content.data : null)),
-        distinctUntilChanged((v1, v2) => v1 === null && v2 === null), // only to avoid sending multiple null
+        distinctUntilChanged((v1, v2) => v1 === null && v2 === null || ( v1 !== null && v2 !== null && v1.nav.itemId === v2.nav.itemId)),
 
         // switchMap may cancel ongoing network calls if item is changed while the request is not over. That's what we want!
         switchMap((item):Observable<NavMenuDataState> => {
@@ -143,30 +145,34 @@ export class ItemNavComponent implements OnInit, OnDestroy {
             return this.loadRootNav();
           }
 
-          // CASE 2: the content is the currently selected element in the menu -> update info or do nothing
-          if (this.isSelectedItem(item.nav)) {
-            if (!item.result) return EMPTY;
-            return of(navMenuDataWithUpdatedNode(this.data as NavMenuData, item.nav, item.result));
-          }
-
-          // CASE 3: the content is among the displayed items at the root of the tree -> select the right one (might load children)
+          // CASE 2: the content is among the displayed items at the root of the tree -> select the right one (might load children)
           if (this.hasItemAmongTreeRoots(item.nav)) {
             const data = navMenuDataWithSelection(this.data as NavMenuData, item.nav);
             return merge(of(data), this.loadChildrenIfNeeded(data));
           }
 
-          // CASE 4: the content is a child of one item at the root of the tree -> shift the tree and select it (might load children)
+          // CASE 3: the content is a child of one item at the root of the tree -> shift the tree and select it (might load children)
           if (this.hasItemAmongKnownTreeChildren(item.nav)) {
             return this.treeShiftedToChild(item.nav);
           }
 
-          // CASE 5: the content is an item not in case 2 or 3 -> load the tree and select the right one
+          // CASE 4: the content is an item not in case 2 or 3 -> load the tree and select the right one
           return this.loadNewNav(item.nav);
         })
       ).subscribe({
         next: change => this.data = change,
         error: _e => this.data = 'error'
       }),
+
+      // If the new current item is already the selected one, just update its data (do not cancel ongoing requests)
+      this.currentContent.currentContent$.pipe(
+        map(content => (content !== null && isItemInfo(content) ? content.data : null)),
+      ).subscribe(item => {
+        if (item !== null && this.isLoaded() && (this.data as NavMenuData).selectedItem?.parentAttemptId === item.nav.itemId) {
+          // only update in case 'result' (the score) is given
+          if (item.result) this.data = navMenuDataWithUpdatedNode(this.data as NavMenuData, item.nav, item.result);
+        }
+      })
     );
 
 
