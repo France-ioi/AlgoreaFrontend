e1c11632dfe36d972b7f5924b71a03ef36de0f01
Damien Leroy <dleroy@smad.be>
2020-10-22 14:23:52 +0200

    extract nav menu data from nav component

--- src/app/core/components/item-nav/item-nav.component.ts
+++ src/app/core/components/item-nav/item-nav.component.ts
@@ -4,32 +4,9 @@ import { CurrentContentService, isItemInfo } from 'src/app/shared/services/curre
 import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { of, Observable, merge, throwError, EMPTY, Subscription } from 'rxjs';
 import { NavItem } from 'src/app/shared/services/nav-types';
+import { ItemNavMenuData } from './item-nav-menu-data';
 
-interface NavMenuData extends NavMenuRootItem {
-  pathToItems: string[]; // path from root to the elements in `items` (so including the parent if any)
-  selectedItem?: NavItem
-}
-
-type NavMenuDataState = 'init'|'loading'|'error'|NavMenuData;
-
-function navMenuDataWith(items: NavMenuRootItem, path: string[], selectedItem?: NavItem): NavMenuData {
-  return Object.assign(items, { pathToItems: path, selectedItem: selectedItem });
-}
-function navMenuDataWithSelection(items: NavMenuData, selectedItem?: NavItem): NavMenuData {
-  return Object.assign(items, { selectedItem: selectedItem });
-}
-interface ItemResultInfo { attemptId: string, bestScore: number, currentScore: number, validated: boolean }
-function navMenuDataWithUpdatedNode(menuData: NavMenuData, item: NavItem, result: ItemResultInfo): NavMenuData {
-  const currentItemIdx = menuData.items.findIndex(i => i.id === item.itemId);
-  if (currentItemIdx === -1) return menuData; // unexpected
-  const newItems = [...menuData.items];
-  newItems[currentItemIdx] = {
-    ...menuData.items[currentItemIdx],
-    attemptId: result.attemptId,
-    score: { best: result.bestScore, current: result.currentScore, validated: result.validated }
-  };
-  return { ...menuData, items: newItems};
-}
+type NavMenuDataState = 'init'|'loading'|'error'|ItemNavMenuData;
 
 @Component({
   selector: 'alg-item-nav',
@@ -52,7 +29,7 @@ export class ItemNavComponent implements OnInit, OnDestroy {
     return merge(
       of('loading'), // first change items to loading
       this.itemNavService.getRoot(this.type).pipe(
-        map(items => navMenuDataWith(items, []))
+        map(items => new ItemNavMenuData(items.items, [], undefined, items.parent))
       )
     );
   }
@@ -70,38 +47,26 @@ export class ItemNavComponent implements OnInit, OnDestroy {
     return merge(
       of('loading'), // as the menu change completely, display the loader
       dataFetcher.pipe(
-        map(items => navMenuDataWith(items, item.itemPath, item)), // the new items (only first level loaded)
+        map(items => new ItemNavMenuData(items.items, item.itemPath, item, items.parent)), // the new items (only first level loaded)
         // already update the tree loaded with the first level, and if needed, load (async) children as well
         switchMap(data => merge( of(data), this.loadChildrenIfNeeded(data) )),
       )
     );
   }
 
-  loadChildrenIfNeeded(data: NavMenuData): Observable<NavMenuDataState> {
-    const selectedItem = data.selectedItem;
+  loadChildrenIfNeeded(data: ItemNavMenuData): Observable<NavMenuDataState> {
+    const selectedItem = data.selectedElement;
     if (!selectedItem) return EMPTY; // if nothing selected, no need to load more (this function should not be called in this case)
 
     // the selected item should be one of the items at the first level
-    const itemData = data.items.find(item => item.id === selectedItem.itemId);
+    const itemData = data.elements.find(item => item.id === selectedItem.itemId);
     if (!itemData) return throwError(new Error('Cannot find the item (unexpected)'));
     if (!itemData.hasChildren) return EMPTY; // if no children, no need to fetch children
     if (!selectedItem.attemptId) return throwError(new Error('Cannot fetch children without attempt (unexpected'));
 
     // We do not check if children were already known. So we might re-load again the same children, which is intended.
     return this.itemNavService.getNavData(itemData.id, selectedItem.attemptId).pipe(
-      map(nav => ({
-        parent: data.parent,
-        pathToItems: data.pathToItems,
-        selectedItem: data.selectedItem,
-        items: data.items.map(i => {
-          if (i.id === itemData.id) {
-            // replace the previous item by new item (the same with possibly an attempt) with the fetched children
-            return {...i, ...nav.parent, children: nav.items};
-          } else {
-            return i;
-          }
-        })
-      }))
+      map(nav => data.withUpdatedElement(itemData.id, {...nav.parent, children: nav.items }))
     );
   }
 
@@ -111,15 +76,8 @@ export class ItemNavComponent implements OnInit, OnDestroy {
    * Then load 'item' 's children  if it has some.
    */
   treeShiftedToChild(item: NavItem): Observable<NavMenuDataState> {
-    const menuItems = this.data as NavMenuData;
-    const newParent = menuItems.items.find(i => i.children && i.children.some(c => c.id === item.itemId));
-    if (!newParent || !newParent.children /* unexpected */) return of(this.data);
-    const newData = {
-      parent: newParent,
-      items: newParent.children,
-      pathToItems: menuItems.pathToItems.concat([newParent.id]),
-      selectedItem: item,
-    };
+    const menuItems = this.data as ItemNavMenuData;
+    const newData = menuItems.subNavMenuData(item);
     return merge(of(newData), this.loadChildrenIfNeeded(newData));
   }
 
@@ -146,7 +104,7 @@ export class ItemNavComponent implements OnInit, OnDestroy {
 
           // CASE 2: the content is among the displayed items at the root of the tree -> select the right one (might load children)
           if (this.hasItemAmongTreeRoots(item.nav)) {
-            const data = navMenuDataWithSelection(this.data as NavMenuData, item.nav);
+            const data = (this.data as ItemNavMenuData).withSelection(item.nav);
             return merge(of(data), this.loadChildrenIfNeeded(data));
           }
 
@@ -167,9 +125,15 @@ export class ItemNavComponent implements OnInit, OnDestroy {
       this.currentContent.currentContent$.pipe(
         map(content => (content !== null && isItemInfo(content) ? content.data : null)),
       ).subscribe(item => {
-        if (item !== null && this.isLoaded() && (this.data as NavMenuData).selectedItem?.parentAttemptId === item.nav.itemId) {
+        if (item !== null && this.isLoaded() && (this.data as ItemNavMenuData).selectedElement?.itemId === item.nav.itemId) {
           // only update in case 'result' (the score) is given
-          if (item.result) this.data = navMenuDataWithUpdatedNode(this.data as NavMenuData, item.nav, item.result);
+          if (item.result) this.data = (this.data as ItemNavMenuData).withUpdatedElement(
+            item.nav.itemId,
+            {
+              attemptId: item.result.attemptId,
+              score: { best:  item.result.bestScore, current:  item.result.currentScore, validated: item.result.validated }
+            }
+          );
         }
       })
     );
@@ -190,22 +154,16 @@ export class ItemNavComponent implements OnInit, OnDestroy {
     return typeof this.data !== 'string';
   }
 
-  isSelectedItem(item: NavItem): boolean {
-    if (!this.isLoaded()) return false;
-    const menuItems = this.data as NavMenuData;
-    return menuItems.selectedItem?.itemId === item.itemId;
-  }
-
   hasItemAmongTreeRoots(item: NavItem): boolean {
     if (!this.isLoaded()) return false;
-    const menuItems = this.data as NavMenuData;
-    return menuItems.items.some(i => i.id === item.itemId);
+    const menuItems = this.data as ItemNavMenuData;
+    return menuItems.hasLevel1Element(item.itemId);
   }
 
   hasItemAmongKnownTreeChildren(item: NavItem): boolean {
     if (!this.isLoaded()) return false;
-    const menuItems = this.data as NavMenuData;
-    return menuItems.items.some(i => i.children && i.children.some(c => c.id === item.itemId));
+    const menuItems = this.data as ItemNavMenuData;
+    return menuItems.hasLevel2Element(item.itemId);
   }
 
 }
