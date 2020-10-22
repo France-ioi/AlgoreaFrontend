93a97e228edd5d832dd6d05487a33ca95e5e020e
Amaury Delprat <amaury.delprat@epita.fr>
2020-10-22 11:55:21 +0200

    adding subscription for the item item-edition-service

--- src/app/modules/item/pages/item-edit/item-edit.component.ts
+++ src/app/modules/item/pages/item-edit/item-edit.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy } from '@angular/core';
-import { CurrentContentService } from 'src/app/shared/services/current-content.service';
+import { CurrentContentService, EditAction } from 'src/app/shared/services/current-content.service';
 import { ItemData, ItemDataSource } from '../../services/item-datasource.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { FetchError, Fetching, isReady, Ready } from '../../../../shared/helpers/state';
+import { UpdateItemStringService } from '../../http-services/update-item-string.service';
+import { Item } from '../../http-services/get-item-by-id.service';
 
 
 @Component({
@@ -13,37 +15,53 @@ import { FetchError, Fetching, isReady, Ready } from '../../../../shared/helpers
   styleUrls: ['./item-edit.component.scss']
 })
 export class ItemEditComponent implements OnDestroy {
+  item: Item;
   itemForm = this.formBuilder.group({
     // eslint-disable-next-line @typescript-eslint/unbound-method
     title: ['', [Validators.required, Validators.minLength(3),]],
   });
   itemLoadingState$ = this.itemDataSource.state$;
 
-  itemSubscription: Subscription;
+  subscriptions: Subscription[] = [];
 
   constructor(
     private currentContent: CurrentContentService,
     private itemDataSource: ItemDataSource,
     private formBuilder: FormBuilder,
+    private updateItemStringService: UpdateItemStringService
   ) {
     this.currentContent.editState.next('editing');
     this.getCurrentItem();
+    this.onSubmit();
   }
 
   ngOnDestroy() {
     this.currentContent.editState.next('non-editable');
-    this.itemSubscription.unsubscribe();
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
   getCurrentItem(): void {
-    this.itemSubscription = this.itemLoadingState$.pipe(
+    this.subscriptions.push(this.itemLoadingState$.pipe(
       filter<Ready<ItemData> | Fetching | FetchError, Ready<ItemData>>(isReady),
     ).subscribe(state => {
-      const item = state.data.item;
+      this.item = state.data.item;
       this.itemForm.patchValue({
-        title: item.string.title
+        title: this.item.string.title
       });
-    });
+    }));
+  }
+
+  onSubmit(): void {
+    this.subscriptions.push(this.currentContent.editAction$
+      .pipe(filter(action => action === EditAction.Save))
+      .subscribe(_action => this.saveInput()));
+  }
+
+  saveInput(): void {
+    this.updateItemStringService.updateItem(
+      this.item.id,
+      this.itemForm.value
+    ).subscribe();
   }
 
 }
