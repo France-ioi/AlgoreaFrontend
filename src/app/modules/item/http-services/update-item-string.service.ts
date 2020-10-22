bbbba84009e0dde8716798ee61c96460d658afef
Amaury Delprat <amaury.delprat@epita.fr>
2020-10-22 11:51:44 +0200

    adding update item string service.

---
+++ src/app/modules/item/http-services/update-item-string.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActionResponse, assertSuccess } from '../../../shared/http-services/action-response';
+import { environment } from '../../../../environments/environment';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateItemStringService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  updateItem(
+    itemId: string,
+    changes: object,
+    languageTag?: string
+  ) {
+    const tag = languageTag || 'default';
+    return this.http.put<ActionResponse<Object>>(
+      `${environment.apiUrl}/items/${itemId}/strings/${tag}`,
+      changes,
+    ).pipe(
+      map(assertSuccess)
+    );
+  }
+}
