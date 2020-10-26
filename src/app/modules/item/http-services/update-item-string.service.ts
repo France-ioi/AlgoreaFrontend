5f744bf1d19609438d5077b061ce45cea50e372f
Amaury Delprat <amaury.delprat@epita.fr>
2020-10-26 14:51:22 +0100

    fixed response type

--- src/app/modules/item/http-services/update-item-string.service.ts
+++ src/app/modules/item/http-services/update-item-string.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActionResponse, assertSuccess } from '../../../shared/http-services/action-response';
+import { assertSuccess, SimpleActionResponse } from '../../../shared/http-services/action-response';
 import { environment } from '../../../../environments/environment';
 import { map } from 'rxjs/operators';
 
@@ -19,7 +19,7 @@ export class UpdateItemStringService {
   ) {
     // Fixme: Get languageTag properly
     const tag = languageTag || 'default';
-    return this.http.put<ActionResponse<Object>>(
+    return this.http.put<SimpleActionResponse>(
       `${environment.apiUrl}/items/${itemId}/strings/${tag}`,
       changes,
     ).pipe(
