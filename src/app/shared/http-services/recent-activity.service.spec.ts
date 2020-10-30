69a0488d181350bcf380321f163abbf335a5f9b3
Michel Blockelet <blockelet@crans.org>
2020-10-30 14:38:13 +0000

    tests for item-log-view and recent-activity

--- src/app/shared/http-services/recent-activity.service.spec.ts
+++ src/app/shared/http-services/recent-activity.service.spec.ts
@@ -1,12 +1,16 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { RecentActivityService } from './recent-activity.service';
 
 describe('RecentActivityService', () => {
   let service: RecentActivityService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+    });
     service = TestBed.inject(RecentActivityService);
   });
 
