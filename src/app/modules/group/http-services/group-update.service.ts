c7b5b37f04c589d78dc09143589594f89c069e59
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-11-03 18:36:01 +0100

    add group-update service

---
+++ src/app/modules/group/http-services/group-update.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { appConfig } from 'src/app/shared/helpers/config';
+import { assertSuccess, SimpleActionResponse } from 'src/app/shared/http-services/action-response';
+
+export interface GroupChanges {
+  name?: string,
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupUpdateService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  updateGroup(
+    groupId: string,
+    changes: GroupChanges
+  ) : Observable<void> {
+    return this.http.put<SimpleActionResponse>(
+      `${appConfig().apiUrl}/groups/${groupId}`,
+      changes,
+    ).pipe(map(assertSuccess))
+  }
+}
