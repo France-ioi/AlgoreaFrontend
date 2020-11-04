cb2267af7dbe16a6b426a8a5b8056c8e5285ca97
Amaury Delprat <amaury.delprat@epita.fr>
2020-11-04 16:37:20 +0100

    modifying item-edit-content to work with the new textarea

--- src/app/modules/item/pages/item-edit-content/item-edit-content.component.ts
+++ src/app/modules/item/pages/item-edit-content/item-edit-content.component.ts
@@ -1,24 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
+
 @Component({
   selector: 'alg-item-edit-content',
   templateUrl: './item-edit-content.component.html',
   styleUrls: [ './item-edit-content.component.scss' ]
 })
 export class ItemEditContentComponent {
-  @Input() inputName = '';
   @Input() parentForm?: FormGroup;
-
-  value = '';
-
-  constructor() {
-    this.value = this.parentForm?.get(this.inputName)?.value as string;
-  }
-
-  updateForm(value: string): void {
-    const input = this.parentForm?.get(this.inputName);
-    input?.setValue(value);
-  }
-
 }
