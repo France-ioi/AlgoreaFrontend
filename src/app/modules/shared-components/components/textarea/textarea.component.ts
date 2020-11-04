891a71f28d26a4fba30f61b1945505fa2b02743f
Amaury Delprat <amaury.delprat@epita.fr>
2020-11-04 16:35:40 +0100

    changing text area to allow use with a reactive from

--- src/app/modules/shared-components/components/textarea/textarea.component.ts
+++ src/app/modules/shared-components/components/textarea/textarea.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'alg-textarea',
@@ -6,10 +7,12 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
   styleUrls: [ './textarea.component.scss' ],
 })
 export class TextareaComponent implements OnInit {
+  @Input() inputName = ''; // name of the input in the parent form
+  @Input() parentForm?: FormGroup;
+
   @Input() icon = '';
   @Input() placeholder = '';
   @Input() disabled = false;
-  @Input() value = '';
 
   @Output() textChange = new EventEmitter<string>();
 
@@ -18,12 +21,16 @@ export class TextareaComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  getValue(): string {
+    return this.parentForm?.get(this.inputName)?.value as string;
+  }
+
   setValue(text: string): void {
-    this.value = text;
+    this.parentForm?.get(this.inputName)?.patchValue(text);
     this.onChange();
   }
 
   onChange(): void {
-    this.textChange.emit(this.value);
+    this.textChange.emit(this.getValue());
   }
 }
