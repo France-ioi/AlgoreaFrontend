3d618c2e0b4406802b31df012e59e528b30c0e08
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-11-05 15:14:51 +0100

    add sub-skills component

---
+++ src/app/modules/item/components/sub-skills/sub-skills.component.ts
@@ -0,0 +1,87 @@
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { bestAttemptFromResults } from 'src/app/shared/helpers/attempts';
+import { itemDetailsUrl } from 'src/app/shared/helpers/item-route';
+import { canCurrentUserViewItemContent } from '../../helpers/item-permissions';
+import { GetItemChildrenService, ItemChild } from '../../http-services/get-item-children.service';
+import { ItemData } from '../../services/item-datasource.service';
+
+interface SubSkillAdditions {
+  isLocked: boolean,
+  result?: {
+    attempt_id: string,
+    score: number,
+  },
+}
+
+@Component({
+  selector: 'alg-sub-skills',
+  templateUrl: './sub-skills.component.html',
+  styleUrls: [ './sub-skills.component.scss' ]
+})
+export class SubSkillsComponent implements OnChanges, OnDestroy {
+  @Input() itemData?: ItemData;
+
+  state: 'loading' | 'error' | 'empty' | 'ready' = 'loading';
+  children: (ItemChild&SubSkillAdditions)[] = [];
+
+  private subscription?: Subscription;
+
+  constructor(
+    private getItemChildrenService: GetItemChildrenService,
+    private router: Router,
+  ) {}
+
+  ngOnChanges(_changes: SimpleChanges): void {
+    this.reloadData();
+  }
+
+  click(child: ItemChild&SubSkillAdditions): void {
+    if (!this.itemData || child.isLocked) return;
+    const attemptId = child.result?.attempt_id;
+    const parentAttemptId = this.itemData.currentResult?.attemptId;
+    if (!parentAttemptId) return; // unexpected: children have been loaded, so we are sure this item has an attempt
+    void this.router.navigate(itemDetailsUrl({
+      id: child.id,
+      path: this.itemData.route.path.concat([ this.itemData.item.id ]),
+      ...attemptId ? { attemptId: attemptId } : { parentAttemptId: parentAttemptId }
+      }));
+  }
+
+  private reloadData(): void {
+    if (this.itemData?.currentResult) {
+      this.state = 'loading';
+      this.subscription?.unsubscribe();
+      this.subscription = this.getItemChildrenService
+        .get(this.itemData.item.id, this.itemData.currentResult.attemptId)
+        .subscribe(
+          children => {
+            this.children = children.map(child => {
+              const res = bestAttemptFromResults(child.results);
+              return {
+                ...child,
+                isLocked: !canCurrentUserViewItemContent(child),
+                result: res === null ? undefined : {
+                  attempt_id: res.attempt_id,
+                  score: res.score,
+                },
+              };
+            });
+
+            if (this.children.length === 0) this.state = 'empty';
+            else this.state = 'ready';
+          },
+          _err => {
+            this.state = 'error';
+          }
+        );
+    } else {
+      this.state = 'error';
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+}
