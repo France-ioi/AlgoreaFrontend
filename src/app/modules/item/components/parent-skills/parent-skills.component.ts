157c2e2ea0d2be6751d1f2a192962d9509b1cb66
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-11-06 09:59:13 +0100

    add parent-skills component

---
+++ src/app/modules/item/components/parent-skills/parent-skills.component.ts
@@ -0,0 +1,73 @@
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { itemDetailsUrl } from 'src/app/shared/helpers/item-route';
+import { canCurrentUserViewItemContent } from '../../helpers/item-permissions';
+import { GetItemParentsService, ItemParent } from '../../http-services/get-item-parents.service';
+import { ItemData } from '../../services/item-datasource.service';
+
+interface ParentSkillAdditions {
+  isLocked: boolean,
+}
+
+@Component({
+  selector: 'alg-parent-skills',
+  templateUrl: './parent-skills.component.html',
+  styleUrls: [ './parent-skills.component.scss' ]
+})
+export class ParentSkillsComponent implements OnChanges, OnDestroy {
+  @Input() itemData?: ItemData;
+
+  state: 'loading' | 'error' | 'empty' | 'ready' = 'loading';
+  parents: (ItemParent&ParentSkillAdditions)[] = [];
+
+  private subscription?: Subscription;
+
+  constructor(
+    private getItemParentsService: GetItemParentsService,
+    private router: Router,
+  ) {}
+
+  ngOnChanges(_changes: SimpleChanges): void {
+    this.reloadData();
+  }
+
+  click(parent: ItemParent&ParentSkillAdditions): void {
+    if (!this.itemData || parent.isLocked) return;
+
+    void this.router.navigate(itemDetailsUrl({
+      id: parent.id,
+      path: this.itemData.route.path.concat([ this.itemData.item.id ]),
+      attemptId: parent.result.attempt_id,
+    }));
+  }
+
+  private reloadData(): void {
+    if (this.itemData?.currentResult) {
+      this.state = 'loading';
+      this.subscription?.unsubscribe();
+      this.subscription = this.getItemParentsService
+        .get(this.itemData.item.id, this.itemData.currentResult.attemptId)
+        .subscribe(
+          parents => {
+            this.parents = parents.map(parent => ({
+              ...parent,
+              isLocked: !canCurrentUserViewItemContent(parent),
+            }));
+
+            if (this.parents.length === 0) this.state = 'empty';
+            else this.state = 'ready';
+          },
+          _err => {
+            this.state = 'error';
+          }
+        );
+    } else {
+      this.state = 'error';
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+}
