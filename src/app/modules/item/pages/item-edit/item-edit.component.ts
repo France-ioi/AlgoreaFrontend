6bd96a105e9c7ce07dc7cd9862851dd54275f70a
Amaury Delprat <amaury.delprat@epita.fr>
2020-11-13 16:29:08 +0100

    triming the title

--- src/app/modules/item/pages/item-edit/item-edit.component.ts
+++ src/app/modules/item/pages/item-edit/item-edit.component.ts
@@ -75,7 +75,7 @@ export class ItemEditComponent implements OnDestroy {
 
   getItemStringChanges(): ItemStringChanges {
     return {
-      title: this.itemForm.get('title')?.value as string,
+      title: (this.itemForm.get('title')?.value as string).trim(),
       description: (this.itemForm.get('description')?.value as string).trim() || null
     };
   }
