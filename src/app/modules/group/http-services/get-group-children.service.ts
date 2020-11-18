bb625a813c34f4950bcbfc3416abc588f99b8085
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-11-19 00:34:30 +0100

    add get-group-children service

---
+++ src/app/modules/group/http-services/get-group-children.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { appConfig } from 'src/app/shared/helpers/config';
+
+interface RawGroupChild {
+  id: string,
+  name: string,
+  type: 'Class'|'Team'|'Club'|'Friends'|'Other'|'User'|'Session'|'Base',
+  user_count: number,
+}
+
+export interface GroupChild {
+  id: string,
+  name: string,
+  type: 'Class'|'Team'|'Club'|'Friends'|'Other'|'User'|'Session'|'Base',
+  userCount: number,
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetGroupChildrenService {
+
+  constructor(private http: HttpClient) { }
+
+  getGroupChildren(
+    groupId: string,
+    sort: string[] = []
+  ): Observable<GroupChild[]> {
+    let params = new HttpParams();
+    if (sort.length > 0) params = params.set('sort', sort.join(','));
+    return this.http
+      .get<RawGroupChild[]>(`${appConfig().apiUrl}/groups/${groupId}/children`, { params: params })
+      .pipe(
+        map(rawGroupChildren => rawGroupChildren.map(c => ({
+          id: c.id,
+          name: c.name,
+          type: c.type,
+          userCount: c.user_count,
+        })))
+      );
+  }
+}
