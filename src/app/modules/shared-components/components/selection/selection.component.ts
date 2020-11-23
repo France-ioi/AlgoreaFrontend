ee96f1d89e730b175c8698d21e4eb6b33bb5cb6e
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-11-23 23:00:28 +0100

    add default value input to selection component

--- src/app/modules/shared-components/components/selection/selection.component.ts
+++ src/app/modules/shared-components/components/selection/selection.component.ts
@@ -9,13 +9,16 @@ export class SelectionComponent implements OnInit {
   @Input() type: 'rounded' | 'square' = 'rounded';
   @Input() items: any;
   @Input() selected = 0;
+  @Input() default?: number;
   @Input() mode: 'light' | 'dark' | 'basic' = 'light';
 
   @Output() change = new EventEmitter<number>();
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.default) this.selected = this.default;
+  }
 
   itemChanged(index: number): void {
     this.selected = index;
