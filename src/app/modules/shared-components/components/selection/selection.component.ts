1e941d9a2bca5edc74947412f3f131228cd7875d
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-11-25 18:37:37 +0100

    remove default value field of selection component

--- src/app/modules/shared-components/components/selection/selection.component.ts
+++ src/app/modules/shared-components/components/selection/selection.component.ts
@@ -9,16 +9,13 @@ export class SelectionComponent implements OnInit {
   @Input() type: 'rounded' | 'square' = 'rounded';
   @Input() items: any;
   @Input() selected = 0;
-  @Input() default?: number;
   @Input() mode: 'light' | 'dark' | 'basic' = 'light';
 
   @Output() change = new EventEmitter<number>();
 
   constructor() {}
 
-  ngOnInit(): void {
-    if (this.default) this.selected = this.default;
-  }
+  ngOnInit(): void {}
 
   itemChanged(index: number): void {
     this.selected = index;
