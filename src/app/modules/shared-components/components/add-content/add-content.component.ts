43e579de84dfe39f1101ade02829dfbd2238c491
Amaury Delprat <amaury.delprat@epita.fr>
2020-11-25 14:09:58 +0100

    opening add content button when there is at least 3 char in the input and clearing the input when the x is clicked

--- src/app/modules/shared-components/components/add-content/add-content.component.ts
+++ src/app/modules/shared-components/components/add-content/add-content.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'alg-add-content',
   templateUrl: './add-content.component.html',
   styleUrls: [ './add-content.component.scss' ],
 })
-export class AddContentComponent implements OnInit {
-  newContentForm: FormGroup = this.formBuilder.group({
-    title: '',
-  });
+export class AddContentComponent implements OnInit, OnDestroy {
+  @Input() allowSkills = false;
+
+  newContentForm: FormGroup = this.formBuilder.group({ title: '' });
 
   state: 'opened' | 'closed' = 'closed';
+  subscription?: Subscription;
 
 
   constructor(
@@ -19,9 +21,17 @@ export class AddContentComponent implements OnInit {
   ) {
   }
 
-
   ngOnInit(): void {
-    this.newContentForm.get('title')?.valueChanges.subscribe((change: string) => this.state = change.length > 2 ? 'opened' : 'closed');
+    this.subscription = this.newContentForm.get('title')?.valueChanges
+      .subscribe((change: string) => this.state = change.length > 2 ? 'opened' : 'closed');
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  clearInput(): void {
+    this.newContentForm.reset({ title: '' });
   }
 
 }
