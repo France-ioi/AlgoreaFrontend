27f0ac18bada07d1ed238c3212f97dd8344ef22d
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-12-02 14:09:05 +0100

    fix tests

--- src/app/modules/group/components/pending-request/pending-request.component.spec.ts
+++ src/app/modules/group/components/pending-request/pending-request.component.spec.ts
@@ -77,27 +77,27 @@ describe('PendingRequestComponent', () => {
     expect(component.selection).toEqual([]);
     expect(component.panel.length).toEqual(1);
     expect(component.currentSort).toEqual([]);
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
   });
 
   it('should, when none is selected and "select all" is clicked, select all rows', () => {
     component.onSelectAll();
     expect(component.selection).toEqual(MOCK_RESPONSE);
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
   });
 
   it('should, when some are selected and "select all" is clicked, select all rows', () => {
     component.selection = MOCK_RESPONSE.slice(1);
     component.onSelectAll();
     expect(component.selection).toEqual(MOCK_RESPONSE);
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
   });
 
   it('should, when all are selected and "select all" is clicked, deselect all rows', () => {
     component.selection = MOCK_RESPONSE;
     component.onSelectAll();
     expect(component.selection).toEqual([]);
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
   });
 
 
@@ -130,7 +130,7 @@ describe('PendingRequestComponent', () => {
     component.selection = [ MOCK_RESPONSE[1] ];
     component.onAcceptOrReject('accept');
 
-    expect(component.ongoingActivity).toEqual('accepting');
+    expect(component.state).toEqual('accepting');
     expect(requestActionsService.acceptJoinRequest).toHaveBeenCalledWith('50', [ '12' ]);
     expect(requestActionsService.rejectJoinRequest).toHaveBeenCalledTimes(0);
     expect(getRequestsService.getPendingRequests).toHaveBeenCalledTimes(1); // the initial one
@@ -139,7 +139,7 @@ describe('PendingRequestComponent', () => {
     serviceResponder$.next(new Map([ [ '12', 'success' ] ]));
     serviceResponder$.complete();
 
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
     expect(messageService.add).toHaveBeenCalledTimes(1);
     expect(messageService.add).toHaveBeenCalledWith({
       severity: 'success',
@@ -157,7 +157,7 @@ describe('PendingRequestComponent', () => {
     component.selection = [ MOCK_RESPONSE[1] ];
     component.onAcceptOrReject('reject');
 
-    expect(component.ongoingActivity).toEqual('rejecting');
+    expect(component.state).toEqual('rejecting');
     expect(requestActionsService.rejectJoinRequest).toHaveBeenCalledWith('50', [ '12' ]);
     expect(requestActionsService.acceptJoinRequest).toHaveBeenCalledTimes(0);
     expect(getRequestsService.getPendingRequests).toHaveBeenCalledTimes(1); // the initial one
@@ -166,7 +166,7 @@ describe('PendingRequestComponent', () => {
     serviceResponder$.next(new Map([ [ '12', 'success' ] ]));
     serviceResponder$.complete();
 
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
     expect(messageService.add).toHaveBeenCalledTimes(1);
     expect(messageService.add).toHaveBeenCalledWith({
       severity: 'success',
@@ -182,7 +182,7 @@ describe('PendingRequestComponent', () => {
     component.selection = [];
     component.onAcceptOrReject('accept');
 
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
     expect(getRequestsService.getPendingRequests).toHaveBeenCalledTimes(1); // the initial one
   });
 
@@ -208,7 +208,7 @@ describe('PendingRequestComponent', () => {
     serviceResponder$.next(new Map([ [ '11', 'invalid' ], [ '12', 'success' ], [ '10', 'success' ] ]));
     serviceResponder$.complete();
 
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
     expect(messageService.add).toHaveBeenCalledTimes(1);
     expect(messageService.add).toHaveBeenCalledWith({
       severity: 'warn',
@@ -227,7 +227,7 @@ describe('PendingRequestComponent', () => {
     serviceResponder$.next(new Map([ [ '11', 'invalid' ], [ '12', 'cycle' ] ]));
     serviceResponder$.complete();
 
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
     expect(messageService.add).toHaveBeenCalledTimes(1);
     expect(messageService.add).toHaveBeenCalledWith({
       severity: 'error',
@@ -246,7 +246,7 @@ describe('PendingRequestComponent', () => {
     serviceResponder$.next(new Map([ [ '11', 'invalid' ], [ '12', 'cycle' ] ]));
     serviceResponder$.complete();
 
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
     expect(messageService.add).toHaveBeenCalledTimes(1);
     expect(messageService.add).toHaveBeenCalledWith({
       severity: 'error',
@@ -265,7 +265,7 @@ describe('PendingRequestComponent', () => {
     serviceResponder$.error(new Error('...'));
     serviceResponder$.complete();
 
-    expect(component.ongoingActivity).toEqual('none');
+    expect(component.state).toEqual('ready');
     expect(messageService.add).toHaveBeenCalledTimes(1);
     expect(messageService.add).toHaveBeenCalledWith({
       severity: 'error',
