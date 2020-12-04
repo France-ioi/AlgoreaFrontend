afd4eeda1fb77bef2d171fed112b975289ee4f75
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-12-04 17:13:22 +0100

    export userPipe formatUser function to use in code

--- src/app/shared/pipes/userDisplay.ts
+++ src/app/shared/pipes/userDisplay.ts
@@ -6,17 +6,19 @@ export interface FormattableUser {
   lastName: string|null,
 }
 
+export function formatUser<T extends FormattableUser>(userInfos: T) : string {
+  if (userInfos.firstName !== null && userInfos.firstName !== '' && userInfos.lastName !== null && userInfos.lastName !== '') {
+    return `${userInfos.firstName} ${userInfos.lastName} (${userInfos.login})`;
+  } else if (userInfos.firstName !== null && userInfos.firstName !== '') {
+    return `${userInfos.firstName} (${userInfos.login})`;
+  } else if (userInfos.lastName !== null && userInfos.lastName !== '') {
+    return `${userInfos.lastName} (${userInfos.login})`;
+  } else {
+    return userInfos.login;
+  }
+}
+
 @Pipe({ name: 'user' })
 export class UserPipe implements PipeTransform {
-  transform<T extends FormattableUser>(userInfos: T) : string {
-    if (userInfos.firstName !== null && userInfos.firstName !== '' && userInfos.lastName !== null && userInfos.lastName !== '') {
-      return `${userInfos.firstName} ${userInfos.lastName} (${userInfos.login})`;
-    } else if (userInfos.firstName !== null && userInfos.firstName !== '') {
-      return `${userInfos.firstName} (${userInfos.login})`;
-    } else if (userInfos.lastName !== null && userInfos.lastName !== '') {
-      return `${userInfos.lastName} (${userInfos.login})`;
-    } else {
-      return userInfos.login;
-    }
-  }
+  transform = formatUser;
 }
