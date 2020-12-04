a2aef11564e039801d4ca2a32a1437b70e383261
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-12-04 17:20:19 +0100

    handle filter changes

--- src/app/modules/item/components/item-chapter-view/group-situation-chapter-view/group-situation-chapter-view.component.ts
+++ src/app/modules/item/components/item-chapter-view/group-situation-chapter-view/group-situation-chapter-view.component.ts
@@ -2,12 +2,14 @@ import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/
 import { forkJoin, merge, Observable, of, Subject } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { Group } from 'src/app/core/components/group-nav-tree/group';
+import { GetGroupChildrenService } from 'src/app/modules/group/http-services/get-group-children.service';
 import { fetchingState, isReady, readyState } from 'src/app/shared/helpers/state';
 import { GetGroupDescendantsService } from 'src/app/shared/http-services/get-group-descendants.service';
 import { GetGroupProgressService } from 'src/app/shared/http-services/get-group-progress.service';
-import { FormattableUser } from 'src/app/shared/pipes/userDisplay';
+import { formatUser } from 'src/app/shared/pipes/userDisplay';
 import { GetItemChildrenService, ItemChild } from '../../../http-services/get-item-children.service';
 import { ItemData } from '../../../services/item-datasource.service';
+import { TypeFilter } from './composition-filter/composition-filter.component';
 
 export interface Progress {
   groupId: string,
@@ -18,7 +20,7 @@ export interface Progress {
 }
 
 interface Data {
-  users: FormattableUser[],
+  rows: string[],
   items: ItemChild[],
   data: (Progress|undefined)[][],
 }
@@ -33,26 +35,31 @@ export class GroupSituationChapterViewComponent implements OnChanges, OnDestroy 
   @Input() group?: Group;
   @Input() itemData?: ItemData;
 
+  defaultFilter: TypeFilter = 'Users';
+
+  currentFilter = this.defaultFilter;
+
   state: 'error' | 'ready' | 'fetching' = 'fetching';
 
   data: Data = {
-    users: [],
+    rows: [],
     items: [],
     data: [],
   }
 
-  private dataFetching = new Subject<{ groupId: string, itemId: string, attemptId: string }>();
+  private dataFetching = new Subject<{ groupId: string, itemId: string, attemptId: string, filter: TypeFilter }>();
 
   constructor(
     private getItemChildrenService: GetItemChildrenService,
     private getGroupDescendantsService: GetGroupDescendantsService,
     private getGroupUsersProgressService: GetGroupProgressService,
+    private getGroupChildrenService: GetGroupChildrenService,
   ) {
     this.dataFetching.pipe(
       switchMap(params =>
         merge(
           of(fetchingState()),
-          this.getData(params.itemId, params.groupId, params.attemptId).pipe(map(readyState))
+          this.getData(params.itemId, params.groupId, params.attemptId, params.filter).pipe(map(readyState))
         )
       )).subscribe(
       state => {
@@ -75,28 +82,78 @@ export class GroupSituationChapterViewComponent implements OnChanges, OnDestroy 
       return;
     }
 
-    this.dataFetching.next({ groupId: this.group.id, itemId: this.itemData.item.id, attemptId: this.itemData.currentResult.attemptId });
+    this.dataFetching.next({
+      groupId: this.group.id,
+      itemId: this.itemData.item.id,
+      attemptId: this.itemData.currentResult.attemptId,
+      filter: this.currentFilter
+    });
   }
 
-  private getProgress(itemId: string, groupId: string): Observable<Progress[]> {
-    return this.getGroupUsersProgressService.getUsersProgress(groupId, [ itemId ]);
+  private getProgress(itemId: string, groupId: string, filter: TypeFilter): Observable<Progress[]> {
+    switch (filter) {
+      case 'Users':
+        return this.getGroupUsersProgressService.getUsersProgress(groupId, [ itemId ]);
+      case 'Teams':
+        return this.getGroupUsersProgressService.getTeamsProgress(groupId, [ itemId ]);
+      case 'Groups':
+        return this.getGroupUsersProgressService.getGroupsProgress(groupId, [ itemId ])
+          .pipe(map(groupsProgress => groupsProgress.map(m => ({
+            groupId: m.groupId,
+            itemId: m.itemId,
+            validated: m.validationRate === 1,
+            score: m.averageScore,
+            timeSpent: m.avgTimeSpent,
+          }))));
+    }
   }
 
-  private getData(itemId: string, groupId: string, attemptId: string): Observable<Data> {
+  private getRows(groupId: string, filter: TypeFilter): Observable<{id :string, value: string}[]> {
+    switch (filter) {
+      case 'Users':
+        return this.getGroupDescendantsService.getUserDescendants(groupId)
+          .pipe(map(users => users.map(user => ({ id: user.id, value: formatUser(user.user) }))));
+      case 'Teams':
+        return this.getGroupDescendantsService.getTeamDescendants(groupId)
+          .pipe(map(teams => teams.map(team => ({ id: team.id, value: team.name }))));
+      case 'Groups':
+        return this.getGroupChildrenService.getGroupChildren(groupId, [], [], [ 'Team', 'User' ])
+          .pipe(map(groups => groups.map(group => ({ id: group.id, value: group.name }))));
+    }
+  }
+
+  private getData(itemId: string, groupId: string, attemptId: string, filter: TypeFilter): Observable<Data> {
     return forkJoin({
-      users: this.getGroupDescendantsService.getUserDescendants(groupId),
       items: this.getItemChildrenService.get(itemId, attemptId),
-      usersProgress: this.getProgress(itemId, groupId),
+      rows: this.getRows(groupId, filter),
+      usersProgress: this.getProgress(itemId, groupId, filter),
     }).pipe(
       map(data => ({
-        users: data.users.map(user => user.user),
         items: data.items,
-        data: data.users.map(user =>
+        rows: data.rows.map(row => row.value),
+        data: data.rows.map(row =>
           data.items.map(item =>
-            data.usersProgress.find(userProgress => userProgress.itemId === item.id && userProgress.groupId === user.id)
+            data.usersProgress.find(userProgress => userProgress.itemId === item.id && userProgress.groupId === row.id)
           )
-        )
+        ),
       }))
     );
+  }
+
+  onFilterChange(typeFilter: TypeFilter): void {
+    if (!this.itemData || !this.itemData.currentResult || !this.group) {
+      this.state = 'error';
+      return;
+    }
+
+    if (typeFilter !== this.currentFilter) {
+      this.currentFilter = typeFilter;
+      this.dataFetching.next({
+        groupId: this.group.id,
+        itemId: this.itemData.item.id,
+        attemptId: this.itemData.currentResult.attemptId,
+        filter: this.currentFilter
+      });
+    }
   }
 }
