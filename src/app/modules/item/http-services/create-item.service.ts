e94587dcc4682ff4bbe29508e7d0d953aa11af60
Amaury Delprat <amaury.delprat@epita.fr>
2020-12-07 16:27:39 +0100

    implementing as_root_of_group_id in create item service

--- src/app/modules/item/http-services/create-item.service.ts
+++ src/app/modules/item/http-services/create-item.service.ts
@@ -30,7 +30,8 @@ export class CreateItemService {
     title: string | null,
     type: ItemType,
     languageTag: string,
-    parentId?: string
+    parentId?: string,
+    groupId?: string
   ): Observable<string> {
     const body: NewItem = {
       title: title || '',
@@ -38,7 +39,9 @@ export class CreateItemService {
       language_tag: languageTag,
     };
 
+    if (!(parentId || groupId)) return throwError(new Error('Parent id and as_root_of_group_id not given.'));
     if (parentId) body.parent = { item_id: parentId };
+    if (groupId) body.as_root_of_group_id = groupId;
 
     return this.http
       .post<ActionResponse<NewItemData>>(`${appConfig().apiUrl}/items`, body)
