f4a82e1614617a1961de2ba84c9da75f32bdbe3a
Damien Leroy <dleroy@smad.be>
2020-12-07 18:15:18 +0100

    update editor bar to remove 'save'

--- src/app/shared/services/current-content.service.ts
+++ src/app/shared/services/current-content.service.ts
@@ -51,7 +51,7 @@ export function isGroupInfo(info: ContentInfo|null): info is GroupInfo {
 }
 
 export type EditState = 'non-editable'|'editable'|'editing'|'editing-noaction'; // editingNoAction is for temporary disabled actions
-export enum EditAction { StartEditing, Save, StopEditing }
+export enum EditAction { StartEditing, StopEditing }
 
 /**
  * Use this service to track what's the current item display in the content (right) pane.
