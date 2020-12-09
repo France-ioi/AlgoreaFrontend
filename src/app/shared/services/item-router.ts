0a350b93179d4ae401ee550a57d1c2832a67fb11
Damien Leroy <dleroy@smad.be>
2020-12-09 19:01:47 +0100

    Stay of the same subpage when navigating through item pages

--- src/app/shared/services/item-router.ts
+++ src/app/shared/services/item-router.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRoute, Router, UrlTree } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { ItemRoute, isRouteWithAttempt, attemptParamName, parentAttemptParamName, pathParamName } from '../helpers/item-route';
 
 @Injectable({
@@ -9,12 +9,11 @@ export class ItemRouter {
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute,
   ) {}
 
   /**
    * Navigate to given item, on the path page.
-   * [not implemented yet] If page is not given and we are currently on an item page, use the same page. Otherwise, default to 'details'.
+   * If page is not given and we are currently on an item page, use the same page. Otherwise, default to 'details'.
    */
   navigateTo(item: ItemRoute, path?: 'edit'|'details'): void {
     void this.router.navigateByUrl(this.url(item, path));
@@ -29,15 +28,27 @@ export class ItemRouter {
 
   /**
    * Return a url to given item, on the path page.
-   * [not implemented yet] If page is not given and we are currently on an item page, use the same page. Otherwise, default to 'details'.
+   * If page is not given and we are currently on an item page, use the same page. Otherwise, default to 'details'.
    */
   url(item: ItemRoute, path?: 'edit'|'details'): UrlTree {
-    const dest = path || 'details';
+    const dest = path ? [ path ] : (this.currentPage() || [ 'details' ]);
     const params: {[k: string]: any} = {};
     if (isRouteWithAttempt(item)) params[attemptParamName] = item.attemptId;
     else params[parentAttemptParamName] = item.parentAttemptId;
     params[pathParamName] = item.path;
-    return this.router.createUrlTree([ 'items', 'by-id', item.id, params, dest ]);
+    return this.router.createUrlTree([ 'items', 'by-id', item.id, params ].concat(dest));
+  }
+
+  /**
+   * Extract (bit hacky) the item sub-page of the current page.
+   * Return undefined if we are not on an "item" page
+   */
+  private currentPage(): string[]|undefined {
+    const currentPageUrlChildren = this.router.parseUrl(this.router.url).root.children;
+    if (!('primary' in currentPageUrlChildren)) return undefined;
+    const segments = currentPageUrlChildren['primary'].segments;
+    if (segments.length < 3 || segments[0].path !== 'items' || segments[1].path !== 'by-id') return undefined;
+    return segments.slice(3).map(segment => segment.path);
   }
 
 }
