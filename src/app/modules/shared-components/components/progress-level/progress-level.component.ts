243aaa5e67b3679acd2b998309b2158087a751ba
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-12-16 18:46:07 +0100

    fix progress-level component

--- src/app/modules/shared-components/components/progress-level/progress-level.component.ts
+++ src/app/modules/shared-components/components/progress-level/progress-level.component.ts
@@ -1,20 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'alg-progress-level',
   templateUrl: './progress-level.component.html',
   styleUrls: [ './progress-level.component.scss' ]
 })
-export class ProgressLevelComponent {
+export class ProgressLevelComponent implements OnChanges {
 
   @Input() theme : 'default' | 'revert' = 'default';
   @Input() value = 0;
   @Input() maximum = 4;
 
-  totalArray : number[]; // array containing [0, 1, 2, ...] as the template cannot iterate by itself
+  totalArray : number[] = []; // array containing [0, 1, 2, ...] as the template cannot iterate by itself
 
-  constructor() {
+  ngOnChanges(_changes: SimpleChanges): void {
     this.totalArray = [ ...Array(this.maximum).keys() ];
   }
-
 }
