a1f00f682a36008da22ecfaf5b211d8ad1cd9e7d
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2021-01-07 19:27:56 +0100

    add access-code-view to item header component

--- src/app/modules/item/components/item-header/item-header.component.ts
+++ src/app/modules/item/components/item-header/item-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ItemData } from '../../services/item-datasource.service';
 
 @Component({
@@ -6,6 +6,14 @@ import { ItemData } from '../../services/item-datasource.service';
   templateUrl: './item-header.component.html',
   styleUrls: [ './item-header.component.scss' ]
 })
-export class ItemHeaderComponent {
+export class ItemHeaderComponent implements OnChanges {
   @Input() itemData?: ItemData;
+
+  showAccessCodeField = false;
+
+  ngOnChanges(_changes: SimpleChanges): void {
+    if (!this.itemData) return;
+    this.showAccessCodeField = this.itemData.item.prompt_to_join_group_by_code &&
+      this.itemData.item.permissions.can_view !== 'none';
+  }
 }
