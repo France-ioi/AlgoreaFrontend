95eecbb6ed40402c306f35e962aa3ce08be56313
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2021-01-11 11:51:01 +0100

    add fetching of permissions

--- src/app/modules/item/components/item-chapter-view/group-situation-chapter-view/user-progress/user-progress.component.ts
+++ src/app/modules/item/components/item-chapter-view/group-situation-chapter-view/user-progress/user-progress.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Group } from 'src/app/modules/group/http-services/get-group-by-id.service';
 import { Item } from 'src/app/modules/item/http-services/get-item-by-id.service';
+import { GetGroupPermissionsService } from 'src/app/shared/http-services/get-group-permissions.service';
 import { Permissions } from '../../../permissions-edit-dialog/permissions-edit-dialog.component';
 import { TypeFilter } from '../composition-filter/composition-filter.component';
 import { Progress } from '../group-situation-chapter-view.component';
@@ -10,10 +12,11 @@ import { Progress } from '../group-situation-chapter-view.component';
   templateUrl: './user-progress.component.html',
   styleUrls: [ './user-progress.component.scss' ]
 })
-export class UserProgressComponent implements OnChanges {
+export class UserProgressComponent implements OnChanges, OnDestroy {
 
   @Input() title?: string;
 
+  @Input() targetGroupId?: string;
   @Input() watchedGroup?: Group;
   @Input() item?: Item;
   @Input() userProgress?: Progress;
@@ -34,7 +37,15 @@ export class UserProgressComponent implements OnChanges {
     is_owner: true,
   };
 
-  visibleDialog = false;
+  dialog: 'loading'|'opened'|'closed' = 'closed';
+
+  private subscription?: Subscription;
+
+  constructor(private getGroupPermissionsService: GetGroupPermissionsService) {}
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 
   ngOnChanges(_changes: SimpleChanges): void {
     if (!this.userProgress || !this.watchedGroup || !this.item) return;
@@ -48,11 +59,28 @@ export class UserProgressComponent implements OnChanges {
   }
 
   onClickAccess(): void {
-    this.visibleDialog = true;
+    if (!this.watchedGroup || !this.targetGroupId || !this.item) return;
+
+    this.dialog = 'loading';
+    this.subscription?.unsubscribe();
+    this.subscription = this.getGroupPermissionsService.getPermissions(this.watchedGroup.id, this.targetGroupId, this.item.id)
+      .subscribe(permissions => {
+        this.permissions = {
+          can_view: permissions.can_view.granted_only_group,
+          can_grant_view: permissions.can_grant_view.granted_only_group,
+          can_watch: permissions.can_watch.granted_only_group,
+          can_edit: permissions.can_edit.granted_only_group,
+          can_enter_until: new Date(permissions.can_enter_until.granted_only_group),
+          can_enter_from: new Date(permissions.can_enter_from.granted_only_group),
+          is_owner: permissions.is_owner,
+          can_make_session_official: permissions.can_make_session_official,
+        };
+        this.dialog = 'opened';
+      });
   }
 
   onDialogClose(_permissions: Permissions): void {
-    this.visibleDialog = false;
+    this.dialog = 'closed';
     //NOTHING FOR NOW
   }
 }
