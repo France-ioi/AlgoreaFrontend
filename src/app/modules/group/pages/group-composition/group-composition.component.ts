7f3a56f84c71d6b45c896733be4c7f9763ccc367
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2021-01-19 22:03:50 +0100

    fix addGroup service not called

--- src/app/modules/group/pages/group-composition/group-composition.component.ts
+++ src/app/modules/group/pages/group-composition/group-composition.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { forkJoin, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 import { ERROR_MESSAGE } from 'src/app/shared/constants/api';
 import { TOAST_LENGTH } from 'src/app/shared/constants/global';
 import { ManagementAdditions, withManagementAdditions } from '../../helpers/group-management';
@@ -44,7 +44,7 @@ export class GroupCompositionComponent implements OnChanges {
     forkJoin({
       parentGroupId: of(this.group.id),
       childGroupId: group.id ? of(group.id) : this.groupCreationService.create(group.title, group.type),
-    }).pipe(map(ids => this.groupCreationService.addSubgroup(ids.parentGroupId, ids.childGroupId))).subscribe(
+    }).pipe(switchMap(ids => this.groupCreationService.addSubgroup(ids.parentGroupId, ids.childGroupId))).subscribe(
       _ => {
         this.displaySuccess($localize`Group successfully added as child group`);
         this.refreshGroupInfo();
