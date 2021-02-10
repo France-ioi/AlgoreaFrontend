c0bda8f4d0b898ba7327d5f7cf615c6dd2422daf
Gustavo Moreno <gustavo@bookmymusic.fr>
2021-02-10 19:17:43 +0100

    Fix build error

--- src/app/modules/shared-components/components/switch/switch.component.ts
+++ src/app/modules/shared-components/components/switch/switch.component.ts
@@ -1,20 +1,27 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'alg-switch',
   templateUrl: './switch.component.html',
   styleUrls: [ './switch.component.scss' ],
 })
-export class SwitchComponent {
+export class SwitchComponent implements OnInit {
   @Input() checked = false;
   @Input() mode: 'dark' | 'white' | 'circular' | 'dark-circular' = 'dark';
   @Input() type = 'square';
 
   @Input() parentForm?: FormGroup;
   @Input() name = '';
+  formControl?: FormControl;
 
   @Output() change = new EventEmitter<boolean>();
+
+  ngOnInit(): void {
+    if (this.parentForm && this.parentForm.get(this.name)) {
+      this.formControl = this.parentForm.get(this.name) as FormControl;
+    }
+  }
 
   handleChange(checked: boolean): void {
     this.change.emit(checked);
