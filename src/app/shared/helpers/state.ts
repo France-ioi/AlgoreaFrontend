714903fe65d76a3ea172c08e232eac37c619473f
Damien Leroy <dleroy@smad.be>
2021-03-30 17:28:42 +0200

    improve state for easier use in templates

--- src/app/shared/helpers/state.ts
+++ src/app/shared/helpers/state.ts
@@ -1,21 +1,21 @@
 import { of, OperatorFunction } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-export interface Ready<T> { readonly tag: 'ready', readonly data: T }
-export interface Fetching { readonly tag: 'fetching' }
-export interface FetchError { readonly tag: 'error', readonly error?: any }
+export type FetchState<T> = Ready<T>|Fetching|FetchError;
+export type Ready<T> = Readonly<{ tag: 'ready', isReady: true, data: T, isFetching: false, isError: false, error?: undefined }>
+export type Fetching = Readonly<{ tag: 'fetching', isReady: false, data?: undefined, isFetching: true, isError: false, error?: undefined }>
+export type FetchError = Readonly<{ tag: 'error', isReady: false, data?: undefined, isFetching: false, isError: true, error: Error }>
 
 export function readyState<T>(data: T): Ready<T> {
-  return { tag: 'ready', data: data };
+  return { tag: 'ready', isReady: true, data: data, isFetching: false, isError: false };
 }
 
 export function fetchingState(): Fetching {
-  return { tag: 'fetching' };
+  return { tag: 'fetching', isReady: false, isFetching: true, isError: false };
 }
 
-export function errorState(error?: any): FetchError {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  return { tag: 'error', error: error };
+export function errorState(error: Error): FetchError {
+  return { tag: 'error', isReady: false, isFetching: false, isError: true, error: error };
 }
 
 /* Do not use the following 3 functions in templates (!), only for type assertion in TS */
