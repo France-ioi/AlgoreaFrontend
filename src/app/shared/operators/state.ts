31b4376c6e8cc291f5f6562301294b3af2d19803
Damien Leroy <dleroy@smad.be>
2021-04-01 13:11:11 +0200

    add doc to state operators

--- src/app/shared/operators/state.ts
+++ src/app/shared/operators/state.ts
@@ -2,6 +2,9 @@ import { of, OperatorFunction, pipe } from 'rxjs';
 import { catchError, filter, map, startWith } from 'rxjs/operators';
 import { errorState, fetchingState, FetchState, Ready, readyState } from 'src/app/shared/helpers/state';
 
+/**
+ * Rx operator which first emits a loading state and then emit a ready or error state depending on the source. Never fails.
+ */
 export function mapToFetchState<T>(): OperatorFunction<T,FetchState<T>> {
   return pipe(
     map(val => readyState(val)),
@@ -10,6 +13,9 @@ export function mapToFetchState<T>(): OperatorFunction<T,FetchState<T>> {
   );
 }
 
+/**
+ * Rx operator which filters only ready states
+ */
 export function readyOnly<T>(): OperatorFunction<FetchState<T>,Ready<T>> {
   return pipe(
     filter((s): s is Ready<T> => s.isReady)
