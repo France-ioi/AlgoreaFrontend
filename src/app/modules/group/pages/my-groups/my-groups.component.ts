612c73296455d7c94077669248e85d7c6e301d5d
Damien Leroy <dleroy@smad.be>
2021-04-06 11:45:11 +0200

    let 'my groups' page advertise its content so that the left menu can load properly

--- src/app/modules/group/pages/my-groups/my-groups.component.ts
+++ src/app/modules/group/pages/my-groups/my-groups.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { contentInfo } from 'src/app/shared/models/content/content-info';
+import { CurrentContentService } from 'src/app/shared/services/current-content.service';
 
 @Component({
   selector: 'alg-my-groups',
   templateUrl: './my-groups.component.html',
   styleUrls: [ './my-groups.component.scss' ]
 })
-export class MyGroupsComponent {
+export class MyGroupsComponent implements OnDestroy {
+
+  constructor(
+    private currentContent: CurrentContentService,
+  ) {
+    this.currentContent.current.next(contentInfo({ breadcrumbs: { category: $localize`My groups`, path: [], currentPageIdx: -1 } }));
+  }
+
+  ngOnDestroy(): void {
+    this.currentContent.current.next(null);
+  }
 
 }
