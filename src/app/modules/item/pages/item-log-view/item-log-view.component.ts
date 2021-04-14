f168c643c08e464e2817c31b3653a219b6d5872b
Aleksandr Semenov <semenov.information@gmail.com>
2021-04-14 20:45:11 +0300

    Adds table with data for log view component

--- src/app/modules/item/pages/item-log-view/item-log-view.component.ts
+++ src/app/modules/item/pages/item-log-view/item-log-view.component.ts
@@ -1,9 +1,38 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, ViewChild } from '@angular/core';
 import { ItemData } from '../../services/item-datasource.service';
-import { ActivityLogService } from 'src/app/shared/http-services/activity-log.service';
-import { ReplaySubject } from 'rxjs';
-import { distinct, switchMap } from 'rxjs/operators';
+import { ActivityLog, ActivityLogService } from 'src/app/shared/http-services/activity-log.service';
+import { Observable, ReplaySubject } from 'rxjs';
+import { distinct, switchMap, map } from 'rxjs/operators';
 import { mapToFetchState } from 'src/app/shared/operators/state';
+import { Table } from 'primeng/table';
+import { Router } from '@angular/router';
+import { incompleteItemStringUrl } from 'src/app/shared/routing/item-route';
+
+interface Column {
+  sortable?: boolean,
+  field: string,
+  header: string
+}
+
+const logColumns: Column[] = [
+  {
+    field: 'activity_type',
+    header: 'Action'
+  },
+  {
+    field: 'item.string.title',
+    header: 'Content'
+  },
+  {
+    field: 'at',
+    header: 'Time'
+  }
+];
+
+interface Data {
+  columns: Column[],
+  rowData: any | ActivityLog[]
+}
 
 @Component({
   selector: 'alg-item-log-view',
@@ -14,19 +43,35 @@ export class ItemLogViewComponent implements OnChanges {
 
   @Input() itemData?: ItemData;
 
+  @ViewChild('table') private table?: Table;
+
   private readonly id$ = new ReplaySubject<string>(1);
   readonly state$ = this.id$.pipe(
     distinct(),
-    switchMap(id => this.activityLogService.getActivityLog(id)),
+    switchMap(id => this.getData$(id)),
     mapToFetchState(),
   );
 
   constructor(
-    private activityLogService: ActivityLogService
+    private activityLogService: ActivityLogService,
+    private router: Router,
   ) {}
 
   ngOnChanges(): void {
     if (this.itemData) this.id$.next(this.itemData.item.id);
+  }
+
+  getData$(id: string): Observable<Data> {
+    return this.activityLogService.getActivityLog(id).pipe(
+      map((data: ActivityLog[]) => ({
+        columns: logColumns,
+        rowData: data
+      }))
+    );
+  }
+
+  navigateToItem(log: ActivityLog): void {
+    void this.router.navigateByUrl(incompleteItemStringUrl(log.item.id));
   }
 
 }
