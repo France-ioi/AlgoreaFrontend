fbd955bb07faddddcb0da177241b976ff762374b
Aleksandr Semenov <semenov.information@gmail.com>
2021-04-19 12:24:06 +0300

    Fixes indents, item link pipe

--- src/app/shared/pipes/itemLink.ts
+++ src/app/shared/pipes/itemLink.ts
@@ -2,11 +2,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { ItemType, typeCategoryOfItem } from '../helpers/item-type';
 import { incompleteItemStringUrl } from '../routing/item-route';
 
-function transform({ id, type }: {id: string, type: ItemType}): any {
-  return incompleteItemStringUrl(id, typeCategoryOfItem({ type }));
-}
-
 @Pipe({ name: 'itemLink', pure: true })
 export class ItemLinkPipe implements PipeTransform {
-  transform = transform;
+  transform({ id, type }: {id: string, type: ItemType}): string {
+    return incompleteItemStringUrl(id, typeCategoryOfItem({ type }));
+  }
 }
