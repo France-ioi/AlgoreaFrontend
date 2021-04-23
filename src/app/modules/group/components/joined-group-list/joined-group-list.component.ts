ffd08ff4c185ae60ffb6b181cb3591730c213746
Damien Leroy <dleroy@smad.be>
2021-04-23 17:54:40 +0200

    rewrite joined-group-list fetching with mapToFetchState and sorting

--- src/app/modules/group/components/joined-group-list/joined-group-list.component.ts
+++ src/app/modules/group/components/joined-group-list/joined-group-list.component.ts
@@ -1,56 +1,35 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { SortEvent } from 'primeng/api';
-import { merge, of, Subject } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
-import { JoinedGroup, JoinedGroupsService } from 'src/app/core/http-services/joined-groups.service';
-import { fetchingState, readyState } from 'src/app/shared/helpers/state';
+import { ReplaySubject } from 'rxjs';
+import { distinctUntilChanged, startWith, switchMap } from 'rxjs/operators';
+import { JoinedGroupsService } from 'src/app/core/http-services/joined-groups.service';
+import { NO_SORT, sortEquals, multisortEventToOptions, SortOptions } from 'src/app/shared/helpers/sort-options';
+import { mapToFetchState } from 'src/app/shared/operators/state';
 
 @Component({
   selector: 'alg-joined-group-list',
   templateUrl: './joined-group-list.component.html',
   styleUrls: [ './joined-group-list.component.scss' ]
 })
-export class JoinedGroupListComponent implements OnDestroy, OnInit {
+export class JoinedGroupListComponent implements OnDestroy {
 
-  state: 'error' | 'ready' | 'fetching' = 'fetching';
-  currentSort: string[] = [];
+  private readonly sort$ = new ReplaySubject<SortOptions>(1);
+  readonly state$ = this.sort$.pipe(
+    startWith(NO_SORT),
+    distinctUntilChanged(sortEquals),
+    switchMap(sort => this.joinedGroupsService.getJoinedGroups(sort)),
+    mapToFetchState(),
+  );
 
-  data: JoinedGroup[] = [];
-
-  private dataFetching = new Subject<{ sort: string[] }>();
-
-  constructor(private joinedGroupsService:JoinedGroupsService) {
-    this.dataFetching.pipe(
-      switchMap(params =>
-        merge(
-          of(fetchingState()),
-          this.joinedGroupsService.getJoinedGroups(params.sort).pipe(map(readyState)),
-        )
-      )
-    ).subscribe(
-      state => {
-        this.state = state.tag;
-        if (state.isReady) this.data = state.data;
-      },
-      _err => this.state = 'error'
-    );
-  }
-
-  ngOnInit(): void {
-    this.dataFetching.next({ sort: this.currentSort });
-  }
+  constructor(private joinedGroupsService:JoinedGroupsService) {}
 
   ngOnDestroy(): void {
-    this.dataFetching.complete();
+    this.sort$.complete();
   }
 
   onCustomSort(event: SortEvent): void {
-    const sortMeta = event.multiSortMeta?.map(meta => (meta.order === -1 ? `-${meta.field}` : meta.field));
-
-    if (sortMeta && JSON.stringify(sortMeta) !== JSON.stringify(this.currentSort)) {
-      this.currentSort = sortMeta;
-      this.dataFetching.next({ sort: this.currentSort });
-    }
+    const sort = multisortEventToOptions(event);
+    if (sort) this.sort$.next(sort);
   }
 
 }
