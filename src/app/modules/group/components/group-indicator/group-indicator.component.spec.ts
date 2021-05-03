b23d5fffd72610a38ffc05f54c7d028386b22d85
Aleksandr Semenov <semenov.information@gmail.com>
2021-05-03 20:38:06 +0300

    Fixes indent

--- src/app/modules/group/components/group-indicator/group-indicator.component.spec.ts
+++ src/app/modules/group/components/group-indicator/group-indicator.component.spec.ts
@@ -10,7 +10,7 @@ describe('GroupIndicatorComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ GroupIndicatorComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
