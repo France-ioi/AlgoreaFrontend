80f9b952b8ab2225ead5ff83873380dcf84cb2c3
Damien Leroy <dleroy@smad.be>
2021-05-05 11:43:57 +0200

    new dummy observation bar

---
+++ src/app/core/components/observation-bar/observation-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'alg-observation-bar',
+  templateUrl: './observation-bar.component.html',
+  styleUrls: ['./observation-bar.component.scss']
+})
+export class ObservationBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
