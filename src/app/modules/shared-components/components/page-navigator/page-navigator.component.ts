77ac29a744d51ec68173585df53e87ee9b869119
Damien Leroy <dleroy@smad.be>
2021-05-05 11:07:26 +0200

    add watch icon to page navigator

--- src/app/modules/shared-components/components/page-navigator/page-navigator.component.ts
+++ src/app/modules/shared-components/components/page-navigator/page-navigator.component.ts
@@ -6,10 +6,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: [ './page-navigator.component.scss' ],
 })
 export class PageNavigatorComponent {
+  @Input() allowWatching = false;
   @Input() allowEditing = false;
   @Input() allowFullScreen = false;
   @Input() navigationMode = 'nextAndPrev';
+
   @Output() edit = new EventEmitter<void>();
+  @Output() watch = new EventEmitter<void>();
 
   editPage(): void {
     this.edit.emit();
