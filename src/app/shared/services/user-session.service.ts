83cdb8e39b326806ad093d9bc4bffc3cf1363c5d
Damien Leroy <dleroy@smad.be>
2021-05-13 12:36:41 +0200

    new more-convenient observable for user session

--- src/app/shared/services/user-session.service.ts
+++ src/app/shared/services/user-session.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { of, EMPTY, BehaviorSubject, Subscription } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
-import { switchMap, catchError, distinctUntilChanged } from 'rxjs/operators';
+import { switchMap, catchError, distinctUntilChanged, map, filter } from 'rxjs/operators';
 import { CurrentUserHttpService, UserProfile } from '../http-services/current-user.service';
 import { Group } from 'src/app/modules/group/http-services/get-group-by-id.service';
+import { isNotUndefined } from '../helpers/null-undefined-predicates';
 
 export interface UserSession {
   user: UserProfile,
@@ -16,6 +17,13 @@ export interface UserSession {
 export class UserSessionService implements OnDestroy {
 
   session$ = new BehaviorSubject<UserSession|undefined>(undefined)
+
+  /** currently-connected user profile, temporary or not, excluding transient (undefined) states */
+  user$ = this.session$.pipe(
+    filter(isNotUndefined),
+    map(session => session.user),
+    distinctUntilChanged((u1, u2) => u1.groupId === u2.groupId)
+  );
 
   private subscription?: Subscription;
 
