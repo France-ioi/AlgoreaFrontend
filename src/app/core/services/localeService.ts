3ad01a01b1fc60ad2613c69bd1da4254272cebae
Thomas Juster <juster.thomas@gmail.com>
2021-05-18 09:35:49 +0200

    create language mismatch subject in locale service

--- src/app/core/services/localeService.ts
+++ src/app/core/services/localeService.ts
@@ -1,17 +1,46 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { appConfig } from 'src/app/shared/helpers/config';
+import { UserSessionService } from 'src/app/shared/services/user-session.service';
+
+export interface LanguageMismatch {
+  platformLanguage: string | undefined,
+  userDefaultLanguage: string,
+  userDefaultLanguageIsSupported: boolean,
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class LocaleService {
+export class LocaleService implements OnDestroy {
 
   readonly languages?: { tag: string, path: string }[];
   readonly currentTag?: string;
+  readonly languageMismatch$ = new BehaviorSubject<LanguageMismatch | undefined>(undefined)
 
-  constructor() {
+  private userSessionSubscription: Subscription
+
+  constructor(private userSession: UserSessionService) {
     this.languages = appConfig().languages;
     this.currentTag = this.languages?.find(l => window.location.pathname.endsWith(l.path))?.tag;
+
+    this.userSessionSubscription = this.userSession.session$.subscribe(session => {
+      if (!session) return;
+
+      const userDefaultLanguage = session.user.defaultLanguage;
+      const hasLanguageMismatch = !!this.currentTag && session.user.defaultLanguage !== this.currentTag;
+      if (hasLanguageMismatch) {
+        this.languageMismatch$.next({
+          platformLanguage: this.currentTag,
+          userDefaultLanguage,
+          userDefaultLanguageIsSupported: this.languages?.some(({ tag }) => tag === userDefaultLanguage) ?? false,
+        });
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSessionSubscription.unsubscribe();
   }
 
   navigateTo(langTag: string): void {
