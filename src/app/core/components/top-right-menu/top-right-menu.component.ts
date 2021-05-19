2afb9cc3d842b843415d09f348d06772010a0667
Thomas Juster <juster.thomas@gmail.com>
2021-05-19 11:44:53 +0200

    add dev-only invalidate token action in top right menu

--- src/app/core/components/top-right-menu/top-right-menu.component.ts
+++ src/app/core/components/top-right-menu/top-right-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { appConfig } from 'src/app/shared/helpers/config';
 import { UserSessionService } from 'src/app/shared/services/user-session.service';
 
 @Component({
@@ -10,13 +12,22 @@ export class TopRightMenuComponent {
 
   readonly menuItems = [
     { label: 'Profile', icon: 'pi pi-user', routerLink: [ 'groups', 'me' ] },
+    ...(!appConfig.production
+      ? [{ label: 'Invalidate token', icon: 'pi pi-refresh', command: (): void => this.invalidateToken() }]
+      : []
+    ),
     { label: 'Log out', icon: 'pi pi-power-off', command: ():void => this.sessionService.logout() },
   ]
 
   sessions$ = this.sessionService.session$;
 
   constructor(
-    private sessionService: UserSessionService
+    private sessionService: UserSessionService,
+    private authService: AuthService,
   ) { }
 
+  private invalidateToken(): void {
+    const authStatus = this.authService.status$.value;
+    if (authStatus.authenticated) this.authService.invalidToken(authStatus);
+  }
 }
