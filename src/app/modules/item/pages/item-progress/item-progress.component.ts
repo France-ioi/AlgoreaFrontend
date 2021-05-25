e969ef8e03daffdf99437ffb949bfa1a7ef30510
Thomas Juster <juster.thomas@gmail.com>
2021-05-25 15:15:05 +0200

    restore item progress view

--- src/app/modules/item/pages/item-progress/item-progress.component.ts
+++ src/app/modules/item/pages/item-progress/item-progress.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { delay } from 'rxjs/operators';
 import { UserSessionService } from 'src/app/shared/services/user-session.service';
 import { ItemData } from '../../services/item-datasource.service';
 
@@ -11,7 +12,7 @@ export class ItemProgressComponent {
 
   @Input() itemData?: ItemData;
 
-  watchedGroup$ = this.sessionService.watchedGroup$;
+  session$ = this.sessionService.session$.pipe(delay(0));
 
   constructor(private sessionService: UserSessionService) {}
 }
