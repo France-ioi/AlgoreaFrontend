388c00c1304f5920bd16c9936dfb060f1e9bce2a
Thomas Juster <juster.thomas@gmail.com>
2021-05-26 15:33:18 +0200

    mark languages as required in configuration

--- src/app/shared/helpers/config.ts
+++ src/app/shared/helpers/config.ts
@@ -4,6 +4,11 @@ interface TokenConfig { authType: 'tokens' }
 type CookieConfig = { authType: 'cookies' } & ({ secure: true, sameSite: boolean } | { secure: boolean, sameSite: true })
 export type AuthTypeConfig = TokenConfig | CookieConfig;
 
+export interface Language {
+  tag: string,
+  path: string,
+}
+
 export type Environment = {
   production: boolean;
 
@@ -17,7 +22,7 @@ export type Environment = {
   defaultActivityId: string;
   defaultSkillId: string;
 
-  languages?: { tag: string, path: string }[];
+  languages: Language[];
 
   allowForcedToken: boolean, // for dev: allow devs to define 'forced_token' in storage so that this token is used in any case
   // The authType used with API is either 'tokens' or 'cookies'.
