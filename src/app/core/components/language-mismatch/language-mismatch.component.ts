5b972fa41f54a24ebd333c8a7f71fe22cae258d6
Thomas Juster <juster.thomas@gmail.com>
2021-05-31 15:40:33 +0200

    create language mismatch dialog component

---
+++ src/app/core/components/language-mismatch/language-mismatch.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { mapPending } from 'src/app/shared/operators/map-pending';
+import { UserSessionService } from 'src/app/shared/services/user-session.service';
+import { LocaleService } from '../../services/localeService';
+
+@Component({
+  selector: 'alg-language-mismatch',
+  templateUrl: './language-mismatch.component.html',
+  styleUrls: [ './language-mismatch.component.scss' ],
+})
+export class LanguageMismatchComponent {
+
+  readonly currentLanguage = this.localeService.currentLang?.tag;
+  readonly languages = this.localeService.languages;
+
+  languageMismatch$ = this.sessionService.userProfile$.pipe(
+    map(profile => {
+      const hasLanguageMismatch = !!this.currentLanguage && profile.defaultLanguage !== this.currentLanguage;
+      return hasLanguageMismatch ? {
+        userDefaultLanguage: profile.defaultLanguage,
+        userDefaultLanguageIsSupported: this.languages.some(({ tag }) => tag === profile.defaultLanguage),
+      } : undefined;
+    }),
+  )
+  updating$?: Observable<boolean>
+
+  constructor(
+    private localeService: LocaleService,
+    private sessionService: UserSessionService,
+  ) { }
+
+  onUpdateUserLanguage(language: string | undefined): void {
+    if (!language) return;
+    this.updating$ = this.sessionService.updateCurrentUser({ default_language: language }).pipe(mapPending());
+  }
+
+  onVisitPlatformInUserLanguage(language: string): void {
+    this.localeService.navigateTo(language);
+  }
+
+}
