ee639cc13b779987896f9736405bd76d5abc8113
Aleksandr Semenov <semenov.information@gmail.com>
2021-06-02 15:03:15 +0300

    Improves itemProgressLabel pipe

--- src/app/modules/item/pages/item-progress/itemProgressLabel.ts
+++ src/app/modules/item/pages/item-progress/itemProgressLabel.ts
@@ -5,7 +5,7 @@ import { Group } from '../../../group/http-services/get-group-by-id.service';
 export class ItemProgressLabelPipe implements PipeTransform {
   transform(type: string, watchedGroup?: Group): any {
     if (!!watchedGroup && [ 'Chapter', 'Task', 'Course' ].includes(type)) {
-      return $localize`Situation of ${ watchedGroup?.name }`;
+      return $localize`Situation of ${ watchedGroup.name }`;
     }
 
     return $localize`Your situation`;
