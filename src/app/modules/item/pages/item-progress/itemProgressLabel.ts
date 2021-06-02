e3745b0109fbb1487d368b05cb373d2fbd3d399c
Aleksandr Semenov <semenov.information@gmail.com>
2021-06-02 21:32:13 +0300

    Improves type for ItemProgressLabelPipe

--- src/app/modules/item/pages/item-progress/itemProgressLabel.ts
+++ src/app/modules/item/pages/item-progress/itemProgressLabel.ts
@@ -1,9 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Group } from '../../../group/http-services/get-group-by-id.service';
+import { ItemType } from '../../../../shared/helpers/item-type';
 
 @Pipe({ name: 'itemProgressLabel' })
 export class ItemProgressLabelPipe implements PipeTransform {
-  transform(type: string, watchedGroup?: Group): any {
+  transform(type: ItemType, watchedGroup?: Group): any {
     if (!!watchedGroup && [ 'Chapter', 'Task', 'Course' ].includes(type)) {
       return $localize`Situation of ${ watchedGroup.name }`;
     }
