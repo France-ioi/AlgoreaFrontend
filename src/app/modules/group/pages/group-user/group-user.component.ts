7dcfe6a831a2a3349cd28931cc723fcdb4659284
Aleksandr Semenov <semenov.information@gmail.com>
2021-06-11 16:34:05 +0300

    Adds update breadcrumbs logic

--- src/app/modules/group/pages/group-user/group-user.component.ts
+++ src/app/modules/group/pages/group-user/group-user.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { User, GetUserService } from '../../http-services/get-user.service';
 import { mapToFetchState } from '../../../../shared/operators/state';
-import { Observable } from 'rxjs';
-import { ActivatedRoute, Router } from '@angular/router';
-import { delay, switchMap, map } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { delay, switchMap, map, filter, startWith } from 'rxjs/operators';
 import { contentInfo } from '../../../../shared/models/content/content-info';
 import { CurrentContentService } from '../../../../shared/services/current-content.service';
 import { UserSessionService } from '../../../../shared/services/user-session.service';
@@ -13,7 +13,7 @@ import { UserSessionService } from '../../../../shared/services/user-session.ser
   templateUrl: './group-user.component.html',
   styleUrls: [ './group-user.component.scss' ]
 })
-export class GroupUserComponent implements OnInit {
+export class GroupUserComponent implements OnInit, OnDestroy {
   readonly state$ = this.route.params.pipe(
     switchMap(({ id }) => this.getUser$(id)),
     mapToFetchState(),
@@ -24,6 +24,8 @@ export class GroupUserComponent implements OnInit {
     map(userProfile => userProfile.groupId),
   );
 
+  private subscription?: Subscription;
+
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -33,26 +35,39 @@ export class GroupUserComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(({ id }) => {
-      this.currentContent.current.next(contentInfo({
-        breadcrumbs: {
-          category: $localize`Users`,
-          path: [
-            {
-              title: 'Login',
-              navigateTo: this.router.createUrlTree([ 'groups', 'users', id ]),
-            },
-            {
-              title: $localize`Progress`,
-            }
-          ],
-          currentPageIdx: -1,
-        }
-      }));
-    });
+    this.subscription = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        startWith(null),
+      )
+      .subscribe(() => {
+        this.updateBreadcrumbs();
+      });
   }
 
-  getUser$(id: string): Observable<User> {
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  private getUser$(id: string): Observable<User> {
     return this.getUserService.getForId(id);
+  }
+
+  private updateBreadcrumbs(): void {
+    this.currentContent.current.next(contentInfo({
+      breadcrumbs: {
+        category: $localize`Users`,
+        path: [
+          {
+            title: 'Login',
+            navigateTo: this.router.createUrlTree([ 'groups', 'users', this.route.snapshot.params.id ]),
+          },
+          {
+            title: this.router.url.includes('personal-data') ? $localize`Personal info` : $localize`Progress`,
+          }
+        ],
+        currentPageIdx: -1,
+      }
+    }));
   }
 }
