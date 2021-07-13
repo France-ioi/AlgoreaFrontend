562979b84b6aa4b4872a7a34444ceb7060df74f0
Thomas Juster <juster.thomas@gmail.com>
2021-07-13 18:51:09 +0200

    remove dead code

--- src/app/shared/helpers/domain.ts
+++
@@ -1,6 +0,0 @@
-export function getDomain(href: string): string {
-  const url = new URL(href);
-  const origin = url.hostname;
-  const hasSubdomain = origin.indexOf('.') !== origin.lastIndexOf('.');
-  return hasSubdomain ? origin.slice(origin.indexOf('.') + 1) : origin;
-}
