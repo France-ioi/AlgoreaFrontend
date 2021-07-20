0a44124f5cce3abe6aa7b28dbb21fc1dae5c4b37
Thomas Juster <juster.thomas@gmail.com>
2021-07-20 10:11:12 +0200

    change marble diagrams format in spec

--- src/app/shared/operators/set-list-by-batch.spec.ts
+++ src/app/shared/operators/set-list-by-batch.spec.ts
@@ -13,12 +13,12 @@ describe('setListByBatch operator', () => {
 
   it('should append all list items 2 by 2', () => {
     testScheduler.run(({ cold, hot, expectObservable }) => {
-      const interval = hot(' -f--f--f-- f--f--f--f--f');
-      const result = cold('  -(x|', { x: [ 1, 2, 3, 4, 5 ] });
-      const expected = cold('----a--b--(c|', {
-        a: [ 1, 2 ],
-        b: [ 1, 2, 3, 4 ],
-        c: [ 1, 2, 3, 4, 5 ],
+      const interval = hot(' -x--x--x-- x--x-');
+      const result = cold('  -(1|            ', { 1: [ 1, 2, 3, 4, 5 ] });
+      const expected = cold('----1--2--(3|   ', {
+        1: [ 1, 2 ],
+        2: [ 1, 2, 3, 4 ],
+        3: [ 1, 2, 3, 4, 5 ],
       });
       const list = result.pipe(setListByBatch({ size: 2, interval: () => interval }));
       expectObservable(list).toEqual(expected);
@@ -27,9 +27,9 @@ describe('setListByBatch operator', () => {
 
   it('should append all list items at once', () => {
     testScheduler.run(({ cold, hot, expectObservable }) => {
-      const interval= hot('  -f -- f--f--f--f--f--f--f');
-      const result = cold('  --(x|', { x: [ 1, 2, 3, 4, 5 ] });
-      const expected = cold('-- --(a|', { a: [ 1, 2, 3, 4, 5 ] });
+      const interval= hot('  -x - - x ');
+      const result = cold('  --(1|    ', { 1: [ 1, 2, 3, 4, 5 ] });
+      const expected = cold('-- - -(1|', { 1: [ 1, 2, 3, 4, 5 ] });
       const list = result.pipe(setListByBatch({ size: 6, interval: () => interval }));
       expectObservable(list).toEqual(expected);
     });
@@ -37,11 +37,11 @@ describe('setListByBatch operator', () => {
 
   it('should set only new list items when switching before next interval', () => {
     testScheduler.run(({ cold, hot, expectObservable }) => {
-      const interval = hot(' --f--f--f--f--f--f--f--f');
-      const result = cold('  ---xy--------|', { x: [ 1, 2, 3, 4, 5 ], y: [ 3, 2, 1 ] });
-      const expected = cold('-----a--b----|', {
-        a: [ 3, 2 ],
-        b: [ 3, 2, 1 ],
+      const interval = hot(' --x--x--x--x--');
+      const result = cold('  ---12--------|', { 1: [ 1, 2, 3, 4, 5 ], 2: [ 3, 2, 1 ] });
+      const expected = cold('-----1--2----|', {
+        1: [ 3, 2 ],
+        2: [ 3, 2, 1 ],
       });
       const list = result.pipe(setListByBatch({ size: 2, interval: () => interval }));
       expectObservable(list).toEqual(expected);
@@ -50,13 +50,13 @@ describe('setListByBatch operator', () => {
 
   it('should replace existing old list items by new and set last items of new list when switching between intervals', () => {
     testScheduler.run(({ cold, hot, expectObservable }) => {
-      const interval = hot(' -f--f--f--f--f--f--f--f');
-      const result = cold('  --x-----y---------|', { x: [ 1, 2, 3, 4, 5 ], y: [ 5, 4, 3, 2, 1 ] });
-      const expected = cold('----a--b--c--d----|', {
-        a: [ 1, 2 ],
-        b: [ 1, 2, 3, 4 ],
-        c: [ 5, 4, 3, 2 ],
-        d: [ 5, 4, 3, 2, 1 ],
+      const interval = hot(' -x--x--x--x--x--x--');
+      const result = cold('  --1-----2---------|', { 1: [ 1, 2, 3, 4, 5 ], 2: [ 5, 4, 3, 2, 1 ] });
+      const expected = cold('----1--2--3--4----|', {
+        1: [ 1, 2 ],
+        2: [ 1, 2, 3, 4 ],
+        3: [ 5, 4, 3, 2 ],
+        4: [ 5, 4, 3, 2, 1 ],
       });
       const list = result.pipe(setListByBatch({ size: 2, interval: () => interval }));
       expectObservable(list).toEqual(expected);
@@ -65,13 +65,13 @@ describe('setListByBatch operator', () => {
 
   it('should replace old list items by the new ones and remove exceeding items', () => {
     testScheduler.run(({ cold, hot, expectObservable }) => {
-      const interval = hot(' -f--f--f--f -- f--f--f--f');
-      const result = cold('  -x---------(y|', { x: [ 1, 2, 3, 4, 5, 6 ], y: [ 2, 1 ] });
-      const expected = cold('----a--b--c --(d|', {
-        a: [ 1, 2 ],
-        b: [ 1, 2, 3, 4 ],
-        c: [ 1, 2, 3, 4, 5, 6 ],
-        d: [ 2, 1 ],
+      const interval = hot(' -x--x--x--x - - x ');
+      const result = cold('  -1---------(2|    ', { 1: [ 1, 2, 3, 4, 5, 6 ], 2: [ 2, 1 ] });
+      const expected = cold('----1--2--3 - -(4|', {
+        1: [ 1, 2 ],
+        2: [ 1, 2, 3, 4 ],
+        3: [ 1, 2, 3, 4, 5, 6 ],
+        4: [ 2, 1 ],
       });
       const list = result.pipe(setListByBatch({ size: 2, interval: () => interval }));
       expectObservable(list).toEqual(expected);
@@ -80,13 +80,13 @@ describe('setListByBatch operator', () => {
 
   it('should remove all items', () => {
     testScheduler.run(({ cold, hot, expectObservable }) => {
-      const interval = hot(' -f--f--f--f -- f--f--f--f');
-      const result = cold('  -x---------(y|', { x: [ 1, 2, 3, 4, 5, 6 ], y: [] as number[] });
-      const expected = cold('----a--b--c --(d|', {
-        a: [ 1, 2 ],
-        b: [ 1, 2, 3, 4 ],
-        c: [ 1, 2, 3, 4, 5, 6 ],
-        d: [],
+      const interval = hot(' -x--x--x--x - - x ');
+      const result = cold('  -1---------(2|   ', { 1: [ 1, 2, 3, 4, 5, 6 ], 2: [] as number[] });
+      const expected = cold('----1--2--3 - -(4|', {
+        1: [ 1, 2 ],
+        2: [ 1, 2, 3, 4 ],
+        3: [ 1, 2, 3, 4, 5, 6 ],
+        4: [],
       });
       const list = result.pipe(setListByBatch({ size: 2, interval: () => interval }));
       expectObservable(list).toEqual(expected);
@@ -95,12 +95,12 @@ describe('setListByBatch operator', () => {
 
   it('should replace old list items by the new list items', () => {
     testScheduler.run(({ cold, hot, expectObservable }) => {
-      const interval = hot(' -f--f--f--f-- f--f--f--f');
-      const result = cold('  --x--------y|', { x: [ 1, 2, 3, 4 ], y: [ 4, 3, 2, 1 ] });
-      const expected = cold('----a--b-----(c|', {
-        a: [ 1, 2 ],
-        b: [ 1, 2, 3, 4 ],
-        c: [ 4, 3, 2, 1 ],
+      const interval = hot(' -x--x--x--x-- x ');
+      const result = cold('  --1--------2|   ', { 1: [ 1, 2, 3, 4 ], 2: [ 4, 3, 2, 1 ] });
+      const expected = cold('----1--2-----(3|', {
+        1: [ 1, 2 ],
+        2: [ 1, 2, 3, 4 ],
+        3: [ 4, 3, 2, 1 ],
       });
       const list = result.pipe(setListByBatch({ size: 2, interval: () => interval }));
       expectObservable(list).toEqual(expected);
@@ -109,10 +109,10 @@ describe('setListByBatch operator', () => {
 
   it('should stop appending list items', () => {
     testScheduler.run(({ cold, hot, expectObservable }) => {
-      const interval = hot(' -f --f--f--f--f--f--f--f');
-      const result = cold('  --(x|', { x: [ 1, 2, 3, 4, 5, 6, 7, 8 ] });
-      const cancel = hot('   -- ----1---');
-      const expected = cold('-- --a-|', { a: [ 1, 2 ] });
+      const interval = hot(' -x - -x--x-');
+      const result = cold('  --(1|      ', { 1: [ 1, 2, 3, 4, 5, 6, 7, 8 ] });
+      const cancel = hot('   -- - ---1--');
+      const expected = cold('-- - -1-|  ', { 1: [ 1, 2 ] });
       const list = result.pipe(setListByBatch({ size: 2, interval: () => interval, until: cancel }));
       expectObservable(list).toEqual(expected);
     });
