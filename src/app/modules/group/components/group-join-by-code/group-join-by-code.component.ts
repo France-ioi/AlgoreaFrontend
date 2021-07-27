2c029f005cd99881c195b9950f0cf44b6f3448e4
Thomas Juster <juster.thomas@gmail.com>
2021-07-27 18:32:35 +0200

    reset code expiration at creation and removal too

--- src/app/modules/group/components/group-join-by-code/group-join-by-code.component.ts
+++ src/app/modules/group/components/group-join-by-code/group-join-by-code.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
 import { Duration } from '../../../../shared/helpers/duration';
 import { Group } from '../../http-services/get-group-by-id.service';
 import { CodeLifetime, codeAdditions, CodeAdditions, isSameCodeLifetime } from '../../helpers/group-code';
 import { GroupActionsService } from '../../http-services/group-actions.service';
 import { CodeActionsService } from '../../http-services/code-actions.service';
 import { ActionFeedbackService } from 'src/app/shared/services/action-feedback.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'alg-group-join-by-code',
@@ -61,18 +63,26 @@ export class GroupJoinByCodeComponent implements OnChanges {
     // disable UI
     this.processing = true;
 
+    const groupId = this.group.id;
+    const expiresAt = this.group.codeExpiresAt;
     // call code refresh service, then group refresh data
-    this.codeActionsService.createNewCode(this.group.id).subscribe({
-      next: () => {
-        this.actionFeedbackService.success($localize`A new code has been generated`);
-        this.processing = false;
-        this.refreshRequired.emit();
-      },
-      error: () => {
-        this.actionFeedbackService.unexpectedError();
-        this.processing = false;
-      },
-    });
+    this.codeActionsService.createNewCode(groupId)
+      .pipe(
+        switchMap(() =>
+          (expiresAt === null ? of(undefined) : this.groupActionsService.updateGroup(groupId, { code_expires_at: null }))
+        ),
+      )
+      .subscribe({
+        next: () => {
+          this.actionFeedbackService.success($localize`A new code has been generated`);
+          this.processing = false;
+          this.refreshRequired.emit();
+        },
+        error: () => {
+          this.actionFeedbackService.unexpectedError();
+          this.processing = false;
+        },
+      });
   }
 
   submitCodeLifetime(newCodeLifetime: CodeLifetime): void {
@@ -105,18 +115,26 @@ export class GroupJoinByCodeComponent implements OnChanges {
     // disable UI
     this.processing = true;
 
+    const groupId = this.group.id;
+    const expiresAt = this.group.codeExpiresAt;
     // call code refresh service, then group refresh data
-    this.codeActionsService.removeCode(this.group.id).subscribe({
-      next: () => {
-        this.actionFeedbackService.success($localize`Users will not be able to join with the former code.`);
-        this.processing = false;
-        this.refreshRequired.emit();
-      },
-      error: () => {
-        this.actionFeedbackService.unexpectedError();
-        this.processing = false;
-      },
-    });
+    this.codeActionsService.removeCode(groupId)
+      .pipe(
+        switchMap(() =>
+          (expiresAt === null ? of(undefined) : this.groupActionsService.updateGroup(groupId, { code_expires_at: null }))
+        ),
+      )
+      .subscribe({
+        next: () => {
+          this.actionFeedbackService.success($localize`Users will not be able to join with the former code.`);
+          this.processing = false;
+          this.refreshRequired.emit();
+        },
+        error: () => {
+          this.actionFeedbackService.unexpectedError();
+          this.processing = false;
+        },
+      });
   }
 
   changeCodeLifetime(selected: number): void {
