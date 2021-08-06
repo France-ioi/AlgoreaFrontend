65125f325f6648d94796da151ea6f5002a7c5159
Gabriel Vidal <gvidalayrinhac@gmail.com>
2021-08-06 19:21:18 +0200

    link item page-navigator to itemNavigationService

--- src/app/modules/item/components/item-header/item-header.component.ts
+++ src/app/modules/item/components/item-header/item-header.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ItemNavigationService } from 'src/app/core/http-services/item-navigation.service';
+import { ItemRoute } from 'src/app/shared/routing/item-route';
+import { ItemRouter } from 'src/app/shared/routing/item-router';
 import { ModeAction, ModeService } from 'src/app/shared/services/mode.service';
 import { ItemData } from '../../services/item-datasource.service';
 
@@ -7,14 +10,38 @@ import { ItemData } from '../../services/item-datasource.service';
   templateUrl: './item-header.component.html',
   styleUrls: [ './item-header.component.scss' ]
 })
-export class ItemHeaderComponent {
+export class ItemHeaderComponent implements OnChanges {
   @Input() itemData?: ItemData;
+
+  navigationNeighbors: {parent: ItemRoute|null, left: ItemRoute|null, right: ItemRoute|null}|null = null;
 
   constructor(
     private modeService: ModeService,
+    private itemNavigationService :ItemNavigationService,
+    private itemRouter: ItemRouter,
   ) {}
+
+  ngOnChanges(_changes: SimpleChanges): void {
+    if (!this.itemData) return;
+
+    this.itemNavigationService.getNavigationNeighbors(this.itemData.route).subscribe(data => {
+      this.navigationNeighbors = data;
+    });
+  }
 
   onEditButtonClicked(): void {
     this.modeService.modeActions$.next(ModeAction.StartEditing);
+  }
+
+  onNavigationClicked(mode: 'parent'|'left'|'right'): void {
+    if (!this.itemData || !this.navigationNeighbors) return;
+
+    if (mode === 'parent' && this.navigationNeighbors.parent) {
+      this.itemRouter.navigateTo(this.navigationNeighbors.parent);
+    } else if (mode === 'left' && this.navigationNeighbors.left) {
+      this.itemRouter.navigateTo(this.navigationNeighbors.left);
+    } else if (mode === 'right' && this.navigationNeighbors.right) {
+      this.itemRouter.navigateTo(this.navigationNeighbors.right);
+    }
   }
 }
