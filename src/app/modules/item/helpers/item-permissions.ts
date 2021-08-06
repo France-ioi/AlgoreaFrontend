c04cec5d8502fe8600a247ba267c6e7ada852077
Aleksandr Semenov <semenov.information@gmail.com>
2021-08-06 14:05:13 +0300

    Changes canView permission to canWatch for progress tab in observation mode

--- src/app/modules/item/helpers/item-permissions.ts
+++ src/app/modules/item/helpers/item-permissions.ts
@@ -2,6 +2,7 @@ import * as D from 'io-ts/Decoder';
 
 export const permissionsDecoder = D.struct({
   canView: D.literal('none','info','content','content_with_descendants','solution'),
+  canWatch: D.literal('none','result','answer','answer_with_grant'),
   canEdit: D.literal('none','children','all','all_with_grant'),
   canGrantView: D.literal('none','enter','content','content_with_descendants','solution','solution_with_grant'),
 });
