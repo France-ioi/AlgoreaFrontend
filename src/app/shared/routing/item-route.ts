7a3a830a78786ab060ca0421f7e511eb0b284965
Damien Leroy <dleroy@smad.be>
2021-08-09 15:31:39 +0200

    reorganization/cleanification of the item route file

--- src/app/shared/routing/item-route.ts
+++ src/app/shared/routing/item-route.ts
@@ -11,10 +11,15 @@ const activityPrefix = 'activities';
 const skillPrefix = 'skills';
 const parentAttemptParamName = 'parentAttempId';
 const attemptParamName = 'attempId';
+export const itemRoutePrefixes = [ activityPrefix, skillPrefix ];
 
 // alias for better readibility
 type ItemId = string;
 type AttemptId = string;
+
+/* **********************************************************************************************************
+ * Item Route: Object storing information required to navigate to an item without need for fetching a path
+ * ********************************************************************************************************** */
 
 interface ItemRouteBase extends ContentRoute {
   contentType: ItemTypeCategory;
@@ -26,8 +31,6 @@ export type ItemRoute = ItemRouteWithAttempt | ItemRouteWithParentAttempt;
 export function isRouteWithAttempt(item: ItemRoute): item is ItemRouteWithAttempt {
   return 'attemptId' in item;
 }
-
-export const itemRoutePrefixes = [ activityPrefix, skillPrefix ];
 
 /**
  * The route to the app default (see config) item
@@ -41,30 +44,12 @@ export function appDefaultItemRoute(cat: ItemTypeCategory = 'activity'): ItemRou
   };
 }
 
-/**
- * Url (as string) of the item route without attemptId or path (only item id)
-*/
-export function urlArrayForRawItem(id: ItemId, cat: ItemTypeCategory, page: string|string[] = 'details'): UrlCommand {
-  return urlArrayForItem(id, cat, {}, page);
-}
 
-/**
- * Return a url array (`commands` array) to the given item, on the given page.
- */
-export function urlArrayForItemRoute(route: ItemRoute, page: string|string[] = 'details'): UrlCommand {
-  const params: UrlCommandParameters = {};
-  if (isRouteWithAttempt(route)) params[attemptParamName] = route.attemptId;
-  else params[parentAttemptParamName] = route.parentAttemptId;
-  params[pathParamName] = route.path;
-  return urlArrayForItem(route.id, route.contentType, params, page);
-}
+/* **********************************************************************************************************
+ * Utility functions for finding the item route from router information
+ * ********************************************************************************************************** */
 
-function urlArrayForItem(id: ItemId, cat: ItemTypeCategory, params: UrlCommandParameters, page: string|string[]):
-  UrlCommand {
-  return [ '/', itemTypeCategoryPrefix(cat), 'by-id', id, params, ...pageToPagePath(page) ];
-}
-
-interface ItemRouteError {
+ interface ItemRouteError {
   tag: 'error';
   contentType: ItemTypeCategory;
   id?: ItemId;
@@ -91,10 +76,32 @@ export function isItemRouteError(route: ItemRoute|ItemRouteError): route is Item
   return 'tag' in route && route.tag === 'error';
 }
 
-function itemTypeCategoryPrefix(cat: ItemTypeCategory): string {
-  return cat === 'activity' ? activityPrefix : skillPrefix;
+
+/* **********************************************************************************************************
+ * Utility functions for converting item info to a navigable url command
+ * ********************************************************************************************************** */
+
+/**
+ * Url (as string) of the item route without attemptId or path (only item id)
+*/
+export function urlArrayForRawItem(id: ItemId, cat: ItemTypeCategory, page: string|string[] = 'details'): UrlCommand {
+  return urlArrayForItem(id, cat, {}, page);
 }
 
-function pageToPagePath(page: string|string[]): string[] {
-  return isString(page) ? [ page ] : page;
+/**
+ * Return a url array (`commands` array) to the given item, on the given page.
+ */
+export function urlArrayForItemRoute(route: ItemRoute, page: string|string[] = 'details'): UrlCommand {
+  const params: UrlCommandParameters = {};
+  if (isRouteWithAttempt(route)) params[attemptParamName] = route.attemptId;
+  else params[parentAttemptParamName] = route.parentAttemptId;
+  params[pathParamName] = route.path;
+  return urlArrayForItem(route.id, route.contentType, params, page);
+}
+
+function urlArrayForItem(id: ItemId, cat: ItemTypeCategory, params: UrlCommandParameters, page: string|string[]):
+  UrlCommand {
+  const prefix = cat === 'activity' ? activityPrefix : skillPrefix;
+  const pagePath = isString(page) ? [ page ] : page;
+  return [ '/', prefix, 'by-id', id, params, ...pagePath ];
 }
