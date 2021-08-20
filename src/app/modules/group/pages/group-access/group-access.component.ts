40ae8afa7cebf3ec86378f6f11f4f7af2bab0d71
Aleksandr Semenov <semenov.information@gmail.com>
2021-08-20 13:50:14 +0300

    Adds group access tab and component with root activity

--- src/app/modules/group/pages/group-access/group-access.component.ts
+++ src/app/modules/group/pages/group-access/group-access.component.ts
@@ -1,11 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Group } from '../../http-services/get-group-by-id.service';
+import { GetItemByIdService } from '../../../item/http-services/get-item-by-id.service';
+import { ReplaySubject } from 'rxjs';
+import { switchMap, filter } from 'rxjs/operators';
+import { isNotNull } from '../../../../shared/helpers/null-undefined-predicates';
+import { mapToFetchState } from '../../../../shared/operators/state';
 
 @Component({
   selector: 'alg-group-access',
   templateUrl: './group-access.component.html',
   styleUrls: [ './group-access.component.scss' ],
 })
-export class GroupAccessComponent {
+export class GroupAccessComponent implements OnChanges {
   @Input() group?: Group;
+
+  private readonly rootActivityId$ = new ReplaySubject<string | null>(1);
+
+  itemState$ = this.rootActivityId$.pipe(
+    filter(isNotNull),
+    switchMap(rootActivityId =>
+      this.getItemByIdService.get(rootActivityId).pipe(
+        mapToFetchState(),
+      )
+    ),
+  );
+
+  constructor(private getItemByIdService: GetItemByIdService) {
+  }
+
+  ngOnChanges(): void {
+    if (this.group) {
+      this.rootActivityId$.next(this.group.rootActivityId);
+    }
+  }
+
 }
