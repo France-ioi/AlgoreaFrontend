70591a7d1794b89964dfcaeabd396b4548c1b631
Aleksandr Semenov <semenov.information@gmail.com>
2021-08-20 19:50:51 +0300

    Adds get participant progress service

--- src/app/modules/item/pages/chapter-user-progress/chapter-user-progress.component.ts
+++ src/app/modules/item/pages/chapter-user-progress/chapter-user-progress.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { GetParticipantProgressService } from '../../http-services/get-participant-progress.service';
+import { ReplaySubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { mapToFetchState } from '../../../../shared/operators/state';
+
+interface Column {
+  field: string,
+  header: string
+}
 
 @Component({
   selector: 'alg-chapter-user-progress',
   templateUrl: './chapter-user-progress.component.html',
   styleUrls: [ './chapter-user-progress.component.scss' ]
 })
-export class ChapterUserProgressComponent {
+export class ChapterUserProgressComponent implements OnChanges {
+  @Input() id?: string;
 
-  constructor() { }
+  private readonly id$ = new ReplaySubject<string>(1);
+  state$ = this.id$.pipe(
+    switchMap(id => this.getParticipantProgressService.get(id)),
+    mapToFetchState(),
+  );
+
+  columns: Column[] = [
+    {
+      field: 'score',
+      header: 'Content',
+    },
+    {
+      field: 'score',
+      header: 'Latest activity',
+    },
+    {
+      field: 'score',
+      header: 'Time spent',
+    },
+    {
+      field: 'score',
+      header: '# subm.',
+    },
+    {
+      field: 'score',
+      header: 'Score',
+    }
+  ];
+
+  constructor(private getParticipantProgressService: GetParticipantProgressService) { }
+
+  ngOnChanges(): void {
+    if (this.id) {
+      this.id$.next(this.id);
+    }
+  }
 
 }
