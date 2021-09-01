fa3824093e3fe0659f14fda15876634ef834b119
Thomas Juster <juster.thomas@gmail.com>
2021-09-01 10:13:32 +0200

    create user link pipe

---
+++ src/app/shared/pipes/userLink.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { UrlCommand } from '../helpers/url';
+import { RawGroupRoute } from '../routing/group-route';
+import { urlArrayForGroupRoute } from '../routing/group-route';
+
+/**
+ * Functions using item route should always be preferred to raw item.
+ * Using raw item means further requests will be required to fetch path and attempt information.
+ */
+@Pipe({ name: 'userLink', pure: true })
+export class UserLinkPipe implements PipeTransform {
+  transform(route: RawGroupRoute): UrlCommand {
+    return urlArrayForGroupRoute(route, 'details', true);
+  }
+}
