2446ea9d5dabf42d92fdfc2d7ad21ef88db063fb
Thomas Juster <juster.thomas@gmail.com>
2021-09-02 10:32:14 +0200

    fetch group nav data in group datasource

--- src/app/modules/group/services/group-datasource.service.ts
+++ src/app/modules/group/services/group-datasource.service.ts
@@ -1,11 +1,16 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { ReplaySubject, Subject } from 'rxjs';
+import { forkJoin, ReplaySubject, Subject } from 'rxjs';
 import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { GroupNavigation, GroupNavigationService } from 'src/app/core/http-services/group-navigation.service';
 import { mapToFetchState } from 'src/app/shared/operators/state';
 import { GroupRoute } from 'src/app/shared/routing/group-route';
 import { GetGroupByIdService, Group } from '../http-services/get-group-by-id.service';
 
-export interface GroupData { route: GroupRoute, group: Group }
+export interface GroupData {
+  route: GroupRoute,
+  group: Group,
+  navigation: GroupNavigation;
+}
 
 /**
  * A datasource which allows fetching a group using a proper state and sharing it among several components.
@@ -22,8 +27,11 @@ export class GroupDataSource implements OnDestroy {
   state$ = this.fetchOperation.pipe(
     // switchMap does cancel the previous ongoing processing if a new one comes
     // on new fetch operation to be done: set "fetching" state and fetch the data which will result in a ready or error state
-    switchMap(route => this.getGroupByIdService.get(route.id).pipe(
-      map(group => ({ route, group })),
+    switchMap(route => forkJoin({
+      group: this.getGroupByIdService.get(route.id),
+      navigation: this.groupNavigationService.getGroupNavigation(route.id),
+    }).pipe(
+      map(({ group, navigation }) => ({ route, group, navigation })),
       mapToFetchState({ resetter: this.refresh$ }),
     )),
     shareReplay(1),
@@ -31,6 +39,7 @@ export class GroupDataSource implements OnDestroy {
 
   constructor(
     private getGroupByIdService: GetGroupByIdService,
+    private groupNavigationService: GroupNavigationService,
   ) {}
 
   fetchGroup(route: GroupRoute): void {
