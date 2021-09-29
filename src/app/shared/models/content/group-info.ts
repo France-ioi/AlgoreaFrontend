ad497977caea792f0b12eb28f84b3fce8199f429
Damien Leroy <dleroy@smad.be>
2021-09-29 17:14:05 +0200

    fix style

--- src/app/shared/models/content/group-info.ts
+++ src/app/shared/models/content/group-info.ts
@@ -4,8 +4,8 @@ import { ContentInfo, RoutedContentInfo } from './content-info';
 
 export interface GroupInfo extends RoutedContentInfo {
   type: 'group',
-  route: GroupRoute;
-  navData?: GroupNavigationData
+  route: GroupRoute,
+  navData?: GroupNavigationData,
 }
 
 export function isGroupInfo(info: ContentInfo|null): info is GroupInfo {
