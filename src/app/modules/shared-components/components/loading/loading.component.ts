00fb760f5e716903129dc8148c4ce18c49e9b5e2
Aleksandr Semenov <semenov.information@gmail.com>
2021-09-29 17:33:48 +0300

    Stage commit: Loading component

--- src/app/modules/shared-components/components/loading/loading.component.ts
+++ src/app/modules/shared-components/components/loading/loading.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'alg-loading',
   templateUrl: './loading.component.html',
   styleUrls: [ './loading.component.scss' ],
 })
-export class LoadingComponent {}
+export class LoadingComponent {
+  @Input() size: 'small' | 'medium' | 'large' = 'large';
+
+  styles = {
+    'small': {
+      width: '24px',
+      height: '24px',
+    },
+    'medium': {
+      width: '3.3333rem',
+      height: '3.3333rem',
+    },
+    'large': {
+      width: '6rem',
+      height: '6rem',
+    }
+  };
+}
