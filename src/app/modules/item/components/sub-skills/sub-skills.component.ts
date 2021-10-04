fa4de185b4ba6488ff564992d7bbf41015adf87d
Aleksandr Semenov <semenov.information@gmail.com>
2021-10-04 20:31:53 +0300

    Improves state for sub skills component

--- src/app/modules/item/components/sub-skills/sub-skills.component.ts
+++ src/app/modules/item/components/sub-skills/sub-skills.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { ReplaySubject } from 'rxjs';
+import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { bestAttemptFromResults } from 'src/app/shared/helpers/attempts';
 import { isASkill, typeCategoryOfItem } from 'src/app/shared/helpers/item-type';
 import { ItemRouter } from 'src/app/shared/routing/item-router';
 import { canCurrentUserViewItemContent } from '../../helpers/item-permissions';
 import { GetItemChildrenService, ItemChild } from '../../http-services/get-item-children.service';
 import { ItemData } from '../../services/item-datasource.service';
+import { mapToFetchState } from '../../../../shared/operators/state';
 
 interface SubSkillAdditions {
   isLocked: boolean,
@@ -24,38 +25,11 @@ interface SubSkillAdditions {
 export class SubSkillsComponent implements OnChanges, OnDestroy {
   @Input() itemData?: ItemData;
 
-  state: 'loading' | 'error' | 'ready' = 'loading';
-  children: (ItemChild&SubSkillAdditions)[] = [];
-
-  private subscription?: Subscription;
-
-  constructor(
-    private getItemChildrenService: GetItemChildrenService,
-    private itemRouter: ItemRouter,
-  ) {}
-
-  ngOnChanges(_changes: SimpleChanges): void {
-    this.reloadData();
-  }
-
-  click(child: ItemChild&SubSkillAdditions): void {
-    if (!this.itemData) return;
-    const attemptId = child.result?.attemptId;
-    const parentAttemptId = this.itemData.currentResult?.attemptId;
-    if (!parentAttemptId) return; // unexpected: children have been loaded, so we are sure this item has an attempt
-    this.itemRouter.navigateTo({
-      contentType: typeCategoryOfItem(child),
-      id: child.id,
-      path: this.itemData.route.path.concat([ this.itemData.item.id ]),
-      ...attemptId ? { attemptId: attemptId } : { parentAttemptId: parentAttemptId }
-    });
-  }
-
-  private reloadData(): void {
-    if (this.itemData?.currentResult) {
-      this.state = 'loading';
-      this.subscription?.unsubscribe();
-      this.subscription = this.getItemChildrenService.get(this.itemData.item.id, this.itemData.currentResult.attemptId).pipe(
+  private readonly params$ = new ReplaySubject<{ id: string, attemptId: string }>(1);
+  readonly state$ = this.params$.pipe(
+    distinctUntilChanged((a, b) => a.id === b.id && a.attemptId === b.attemptId),
+    switchMap(({ id, attemptId }) =>
+      this.getItemChildrenService.get(id, attemptId).pipe(
         map(children =>
           children
             .filter(child => isASkill(child))
@@ -71,21 +45,39 @@ export class SubSkillsComponent implements OnChanges, OnDestroy {
               };
             })
         )
-      ).subscribe({
-        next: children => {
-          this.children = children;
-          this.state = 'ready';
-        },
-        error: _err => {
-          this.state = 'error';
-        }
+      )
+    ),
+    mapToFetchState(),
+  );
+
+  constructor(
+    private getItemChildrenService: GetItemChildrenService,
+    private itemRouter: ItemRouter,
+  ) {}
+
+  ngOnChanges(_changes: SimpleChanges): void {
+    if (this.itemData?.currentResult) {
+      this.params$.next({
+        id: this.itemData.item.id,
+        attemptId: this.itemData.currentResult.attemptId,
       });
-    } else {
-      this.state = 'error';
     }
   }
 
+  click(child: ItemChild&SubSkillAdditions): void {
+    if (!this.itemData) return;
+    const attemptId = child.result?.attemptId;
+    const parentAttemptId = this.itemData.currentResult?.attemptId;
+    if (!parentAttemptId) return; // unexpected: children have been loaded, so we are sure this item has an attempt
+    this.itemRouter.navigateTo({
+      contentType: typeCategoryOfItem(child),
+      id: child.id,
+      path: this.itemData.route.path.concat([ this.itemData.item.id ]),
+      ...attemptId ? { attemptId: attemptId } : { parentAttemptId: parentAttemptId }
+    });
+  }
+
   ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
+    this.params$.complete();
   }
 }
