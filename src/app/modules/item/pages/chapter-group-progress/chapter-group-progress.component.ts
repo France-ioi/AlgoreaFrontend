3fe78f6fb075edd16899d3969ff181c15570eb7c
Aleksandr Semenov <semenov.information@gmail.com>
2021-10-05 19:13:25 +0300

    Adds refresh button for chapter group progress component

--- src/app/modules/item/pages/chapter-group-progress/chapter-group-progress.component.ts
+++ src/app/modules/item/pages/chapter-group-progress/chapter-group-progress.component.ts
@@ -5,6 +5,7 @@ import { UserSessionService } from 'src/app/shared/services/user-session.service
 import { ItemData } from '../../services/item-datasource.service';
 import { mapToFetchState } from '../../../../shared/operators/state';
 import { isNotUndefined } from '../../../../shared/helpers/null-undefined-predicates';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'alg-chapter-group-progress',
@@ -15,14 +16,19 @@ export class ChapterGroupProgressComponent {
 
   @Input() itemData?: ItemData;
 
+  private refresh$ = new Subject<void>();
   state$ = this.sessionService.watchedGroup$.pipe(
     filter(isNotUndefined),
     switchMap(watchedGroup => this.getGroupByIdService.get(watchedGroup.route.id)),
-    mapToFetchState(),
+    mapToFetchState({ resetter: this.refresh$ }),
   );
 
   constructor(
     private sessionService: UserSessionService,
     private getGroupByIdService: GetGroupByIdService,
   ) {}
+
+  refresh(): void {
+    this.refresh$.next();
+  }
 }
