883444e8c472704fa2e85ad1b3173ca51edee1c3
Aleksandr Semenov <semenov.information@gmail.com>
2021-10-26 18:30:17 +0300

    Improves removeBatch for remove group manager service

--- src/app/modules/group/http-services/remove-group-manager.service.ts
+++ src/app/modules/group/http-services/remove-group-manager.service.ts
@@ -2,18 +2,16 @@ import { Injectable } from '@angular/core';
 import { merge, Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { appConfig } from '../../../shared/helpers/config';
-import { SimpleActionResponse } from '../../../shared/http-services/action-response';
+import { assertSuccess, SimpleActionResponse } from '../../../shared/http-services/action-response';
 import { reduce, map, switchMap, catchError } from 'rxjs/operators';
-import { Result } from '../components/member-list/group-removal-response-handling';
+import { Result } from '../components/group-manager-list/group-manager-removal-response-handling';
 
-export function parseResults(data: SimpleActionResponse[]): Result {
+export function parseResults(data: boolean[]): Result {
   return {
     countRequests: data.length,
-    countSuccess: data.filter(state => state.success).length,
+    countSuccess: data.filter(success => success).length,
   };
 }
-
-const failedState = { success: false, message: $localize`Failed to delete manager` };
 
 @Injectable({
   providedIn: 'root',
@@ -22,23 +20,31 @@ export class RemoveGroupManagerService {
   constructor(private http: HttpClient) {
   }
 
-  remove(groupId: string, managerId: string): Observable<SimpleActionResponse> {
-    return this.http.delete<SimpleActionResponse>(`${appConfig.apiUrl}/groups/${groupId}/managers/${managerId}`);
+  remove(groupId: string, managerId: string): Observable<void> {
+    return this.http
+      .delete<SimpleActionResponse>(`${appConfig.apiUrl}/groups/${groupId}/managers/${managerId}`)
+      .pipe(
+        map(assertSuccess),
+      );
   }
 
   removeBatch(parentGroupId: string, ids: string[], ownManagerId?: string): Observable<Result> {
     return merge(
-      ...ids.map(id => this.remove(parentGroupId, id).pipe(catchError(() => of(failedState)))),
+      ...ids.map(id => this.remove(parentGroupId, id).pipe(
+        map(() => true),
+        catchError(() => of(false))),
+      ),
     ).pipe(
-      reduce<SimpleActionResponse, SimpleActionResponse[]>((removedManagers, removedManager) =>
-        [ ...removedManagers, removedManager ], []
+      reduce<boolean, boolean[]>((removedManagers, success) =>
+        [ ...removedManagers, success ], []
       ),
       switchMap(removedManagers =>
         (ownManagerId ?
-          removedManagers.some(removedManager => !removedManager.success) ?
-            of([ ...removedManagers, failedState ]) : this.remove(parentGroupId, ownManagerId).pipe(
-              catchError(() => of(failedState)),
-              map(removedManager => [ ...removedManagers, removedManager ]),
+          removedManagers.some(removedManager => !removedManager) ?
+            of([ ...removedManagers, false ]) : this.remove(parentGroupId, ownManagerId).pipe(
+              map(() => true),
+              catchError(() => of(false)),
+              map(success => [ ...removedManagers, success ]),
             )
           : of(removedManagers))
       ),
