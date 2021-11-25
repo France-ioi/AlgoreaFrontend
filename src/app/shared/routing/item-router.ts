32d3b55c15ee8ba6b4f0abfa184021333d6678b0
Thomas Juster <juster.thomas@gmail.com>
2021-11-25 17:24:06 +0100

    fix home error link

--- src/app/shared/routing/item-router.ts
+++ src/app/shared/routing/item-router.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NavigationExtras, Router, UrlTree } from '@angular/router';
 import { ensureDefined } from '../helpers/null-undefined-predicates';
+import { UrlCommand } from '../helpers/url'
 import { itemCategoryFromPrefix, RawItemRoute, urlArrayForItemRoute } from './item-route';
 
 interface NavigateOptions {
@@ -42,6 +43,10 @@ export class ItemRouter {
    */
   url(item: RawItemRoute, page?: string|string[]): UrlTree {
     return this.router.createUrlTree(urlArrayForItemRoute(item, page ?? this.currentItemPage()));
+  }
+
+  urlArray(route: RawItemRoute, page?: string[]): UrlCommand {
+    return urlArrayForItemRoute(route, page ?? this.currentItemPage());
   }
 
   /**
