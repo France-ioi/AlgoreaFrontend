6926e10a705b9d95e7dfc57d7bae276cda883802
Thomas Juster <juster.thomas@gmail.com>
2021-12-15 15:44:27 +0100

    feat: implement task launching in LTI

--- src/app/modules/lti/pages/lti/lti.component.ts
+++ src/app/modules/lti/pages/lti/lti.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
-import { GetItemChildrenService } from 'src/app/modules/item/http-services/get-item-children.service';
-import { GetResultsService } from 'src/app/modules/item/http-services/get-results.service';
-import { rawItemRoute } from 'src/app/shared/routing/item-route';
+import { forkJoin, Observable, of } from 'rxjs';
+import { catchError, map, shareReplay, switchMap, switchMapTo } from 'rxjs/operators';
+import { GetItemByIdService, Item } from 'src/app/modules/item/http-services/get-item-by-id.service';
+import { GetItemChildrenService, ItemChild } from 'src/app/modules/item/http-services/get-item-children.service';
+import { GetItemPathService } from 'src/app/modules/item/http-services/get-item-path.service';
+import { GetResultsService, Result } from 'src/app/modules/item/http-services/get-results.service';
+import { bestAttemptFromResults, implicitResultStart } from 'src/app/shared/helpers/attempts';
+import { ensureDefined } from 'src/app/shared/helpers/null-undefined-predicates';
+import { ResultActionsService } from 'src/app/shared/http-services/result-actions.service';
+import { fullItemRoute } from 'src/app/shared/routing/item-route';
 import { ItemRouter } from 'src/app/shared/routing/item-router';
 import { LayoutService } from 'src/app/shared/services/layout.service';
+
+const explicitEntryWithNoResultError = new Error('no result');
+const noChildError = new Error('no first child');
+const noContentIdError = new Error('no content id');
+const fetchError = new Error('fetch error');
+type LTIError = 'no content id' | 'fetch error' | 'explicit entry with no result' | 'no child';
 
 @Component({
   selector: 'alg-lti',
@@ -14,37 +26,76 @@ import { LayoutService } from 'src/app/shared/services/layout.service';
 })
 export class LTIComponent {
 
-  error?: 'no content id' | 'fetch error' | 'no result' | 'no child';
+  error?: LTIError;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private itemRouter: ItemRouter,
-    private getResultsService: GetResultsService,
-    private getItemChildrenService: GetItemChildrenService,
     private layoutService: LayoutService,
+    private getItemByIdService: GetItemByIdService,
+    private getResultsService: GetResultsService,
+    private getItemPathService: GetItemPathService,
+    private resultActionsService: ResultActionsService,
+    private getItemChildrenService: GetItemChildrenService,
   ) {
     this.layoutService.toggleTopRightControls(false);
     this.layoutService.toggleFullFrameContent(true, false);
 
-    const contentId = this.activatedRoute.snapshot.queryParamMap.get('content_id');
-    if (!contentId) {
-      this.error = 'no content id';
-      return;
-    }
-
-    const noResultError = new Error('no result');
-    this.getResultsService.get(contentId, { attempt_id: '0', limit: 1, sort: [ '-id' ] }).pipe(
-      switchMap(([ result ]) => {
-        if (!result) throw noResultError;
-        return this.getItemChildrenService.get(contentId, result.attemptId);
-      })
+    this.activatedRoute.queryParamMap.pipe(
+      map(queryParams => {
+        const contentId = queryParams.get('content_id');
+        if (!contentId) throw noContentIdError;
+        return contentId;
+      }),
+      switchMap(contentId => this.getNavigationData(contentId).pipe(
+        catchError(err => of(err instanceof Error ? err : fetchError)),
+      )),
     ).subscribe({
-      next: ([ firstChild ]) => {
-        if (firstChild) this.itemRouter.navigateTo(rawItemRoute('activity', firstChild.id), { navExtras: { replaceUrl: true } });
-        else this.error = 'no child';
+      next: dataOrError => {
+        if (dataOrError instanceof Error) {
+          if (dataOrError === noChildError) this.error = 'no child';
+          else if (dataOrError === explicitEntryWithNoResultError) this.error = 'explicit entry with no result';
+          else this.error = 'fetch error';
+          return;
+        }
+        const { firstChild , path, result } = dataOrError;
+        const itemRoute = fullItemRoute('activity', firstChild.id, path, { attemptId: result.attemptId });
+        this.itemRouter.navigateTo(itemRoute, { navExtras: { replaceUrl: true } });
       },
-      error: error => this.error = error === noResultError ? 'no result' : 'fetch error',
     });
+  }
+
+  private getNavigationData(itemId: string): Observable<{ firstChild: ItemChild, path: string[], result: Result }> {
+    const item$ = this.getItemByIdService.get(itemId).pipe(shareReplay(1));
+    const path$ = this.getItemPathService.getItemPath(itemId).pipe(shareReplay(1));
+    const results$ = this.getResultsService.get(itemId, { attempt_id: '0' });
+
+    const bestResult$ = forkJoin([ item$, results$ ]).pipe(
+      switchMap(([ item, results ]) => {
+        if (results.length === 0 && !implicitResultStart(item)) throw explicitEntryWithNoResultError;
+        return path$.pipe(switchMap(path => this.getBestResultOrStartOne(item, results, path)));
+      }),
+      shareReplay(1),
+    );
+
+    const children$ = bestResult$.pipe(switchMapTo(this.getItemChildrenService.get(itemId, '0')));
+
+    return children$.pipe(
+      switchMap(([ firstChild ]) => {
+        if (!firstChild) throw noChildError;
+        return forkJoin([ path$, bestResult$ ]).pipe(map(([ path, result ]) => ({ firstChild, path, result })));
+      }),
+    );
+  }
+
+  private getBestResultOrStartOne(item: Item, results: Result[], path: string[]): Observable<Result> {
+    const bestResult = bestAttemptFromResults(results);
+    if (bestResult) return of(bestResult);
+
+    return this.resultActionsService.startWithoutAttempt([ ...path, item.id ]).pipe(
+      switchMap(() => this.getResultsService.get(item.id, { attempt_id: '0' })),
+      map(([ first ]) => ensureDefined(first, 'unexpected: there should be a result after its creation')),
+    );
   }
 
 }
