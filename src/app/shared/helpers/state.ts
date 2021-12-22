97de8a0d684422935aba9433c454cce1608e4a77
Aleksandr Semenov <semenov.information@gmail.com>
2021-12-22 18:39:27 +0200

    Adds clear message for forbidden groups

--- src/app/shared/helpers/state.ts
+++ src/app/shared/helpers/state.ts
@@ -2,7 +2,7 @@
 export type FetchState<T> = Ready<T>|Fetching<T>|FetchError;
 export type Ready<T> = Readonly<{ tag: 'ready', isReady: true, data: T, isFetching: false, isError: false, error?: undefined }>;
 export type Fetching<T> = Readonly<{ tag: 'fetching', isReady: false, data?: T, isFetching: true, isError: false, error?: undefined }>;
-export type FetchError = Readonly<{ tag: 'error', isReady: false, data?: undefined, isFetching: false, isError: true, error: Error }>;
+export type FetchError = Readonly<{ tag: 'error', isReady: false, data?: undefined, isFetching: false, isError: true, error: Error & { status?: number } }>;
 
 export function readyState<T>(data: T): Ready<T> {
   return { tag: 'ready', isReady: true, data: data, isFetching: false, isError: false };
@@ -12,6 +12,6 @@ export function fetchingState<T = undefined>(data?: T): Fetching<T> {
   return { tag: 'fetching', isReady: false, data: data, isFetching: true, isError: false };
 }
 
-export function errorState(error: Error): FetchError {
+export function errorState(error: Error & { status?: number }): FetchError {
   return { tag: 'error', isReady: false, isFetching: false, isError: true, error: error };
 }
