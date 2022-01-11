06d3830fb9f895fe513bbd694cf5117eda9a8065
Thomas Juster <juster.thomas@gmail.com>
2022-01-11 10:23:23 +0100

    revert test content

--- src/app/shared/http-services/check-login.service.ts
+++ src/app/shared/http-services/check-login.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { appConfig } from '../helpers/config';
 import * as D from 'io-ts/Decoder';
 import { decodeSnakeCase } from 'src/app/shared/operators/decode';
-import { delay, map } from 'rxjs/operators';
-
-const fakePassingId = 42;
+import { map } from 'rxjs/operators';
 
 const dataDecoder = D.struct({
   loginIdMatched: D.boolean,
@@ -20,8 +18,6 @@ export class CheckLoginService {
   constructor(private http: HttpClient) {}
 
   check(loginId: number): Observable<boolean> {
-    if (loginId === fakePassingId) return of(true).pipe(delay(300));
-
     const params = new HttpParams({ fromObject: { login_id: loginId } });
 
     return this.http
