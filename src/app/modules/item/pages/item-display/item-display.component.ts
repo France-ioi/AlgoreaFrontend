644efcbfb89f62349cdf84d88b79eaefe0bf190c
Aleksandr Semenov <semenov.information@gmail.com>
2022-02-03 16:18:04 +0200

    Removes height calculation for item display component

--- src/app/modules/item/pages/item-display/item-display.component.ts
+++ src/app/modules/item/pages/item-display/item-display.component.ts
@@ -11,10 +11,9 @@ import {
   SimpleChanges,
   ViewChild,
 } from '@angular/core';
-import { EMPTY, fromEvent, merge, Observable, ReplaySubject } from 'rxjs';
-import { delay, distinctUntilChanged, filter, map, pairwise, startWith, switchMap } from 'rxjs/operators';
+import { EMPTY, Observable } from 'rxjs';
+import { map, pairwise, startWith, switchMap } from 'rxjs/operators';
 import { HOURS, SECONDS } from 'src/app/shared/helpers/duration';
-import { isNotNull } from 'src/app/shared/helpers/null-undefined-predicates';
 import { TaskConfig, ItemTaskService } from '../../services/item-task.service';
 import { mapToFetchState } from 'src/app/shared/operators/state';
 import { capitalize } from 'src/app/shared/helpers/case_conversion';
@@ -25,7 +24,6 @@ import { FullItemRoute } from 'src/app/shared/routing/item-route';
 import { DomSanitizer } from '@angular/platform-browser';
 import { PermissionsInfo } from '../../helpers/item-permissions';
 import { ActionFeedbackService } from 'src/app/shared/services/action-feedback.service';
-import { LayoutService } from 'src/app/shared/services/layout.service';
 import { LTIDataSource } from 'src/app/modules/lti/services/lti-datasource.service';
 import { PublishResultsService } from '../../http-services/publish-result.service';
 import { errorIsHTTPForbidden } from 'src/app/shared/helpers/errors';
@@ -66,23 +64,6 @@ export class ItemDisplayComponent implements OnInit, AfterViewChecked, OnChanges
     map(views => views.map(view => ({ view, name: this.getTabNameByView(view) }))),
   );
 
-
-  private computeIframeOffsetTop$ = new ReplaySubject<void>(1);
-  private iframeOffsetTop$ = merge(
-    fromEvent(globalThis, 'resize'),
-    this.layoutService.fullFrameContent$.pipe(delay(1000)), // time for the animation be complete
-    this.computeIframeOffsetTop$,
-  ).pipe(
-    map(() => (this.iframe ? this.iframe.nativeElement.getBoundingClientRect().top + globalThis.scrollY : null)),
-    filter(isNotNull),
-    distinctUntilChanged(),
-  );
-  // Start updating the iframe height to match the task's height
-  iframeHeight$ = this.iframeOffsetTop$.pipe(
-    map(top => `calc(100vh - ${top}px)`),
-    distinctUntilChanged(),
-  );
-
   private subscriptions = [
     this.taskService.saveAnswerAndStateInterval$
       .pipe(startWith({ success: true }), pairwise())
@@ -118,7 +99,6 @@ export class ItemDisplayComponent implements OnInit, AfterViewChecked, OnChanges
     private taskService: ItemTaskService,
     private sanitizer: DomSanitizer,
     private actionFeedbackService: ActionFeedbackService,
-    private layoutService: LayoutService,
     private publishResultService: PublishResultsService,
     private ltiDataSource: LTIDataSource,
   ) {}
@@ -130,7 +110,6 @@ export class ItemDisplayComponent implements OnInit, AfterViewChecked, OnChanges
 
   ngAfterViewChecked(): void {
     if (!this.iframe || this.taskService.initialized) return;
-    this.computeIframeOffsetTop$.next();
     this.taskService.initTask(this.iframe.nativeElement);
   }
 
@@ -151,7 +130,6 @@ export class ItemDisplayComponent implements OnInit, AfterViewChecked, OnChanges
 
   ngOnDestroy(): void {
     if (this.actionFeedbackService.hasFeedback) this.actionFeedbackService.clear();
-    this.computeIframeOffsetTop$.complete();
     this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
