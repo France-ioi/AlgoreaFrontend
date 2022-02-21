a11380d3c782629c10b5a9d34346185ad1b86fd6
Aleksandr Semenov <semenov.information@gmail.com>
2022-02-21 21:06:24 +0200

    Fixes lint errors in group removal response

--- src/app/modules/group/components/member-list/group-removal-response-handling.ts
+++ src/app/modules/group/components/member-list/group-removal-response-handling.ts
@@ -10,11 +10,11 @@ export function displayGroupRemovalResponseToast(feedbackService: ActionFeedback
   if (result.countSuccess === result.countRequests) {
     feedbackService.success($localize`${result.countSuccess} group(s) have been removed`);
   } else if (result.countSuccess === 0) {
-    feedbackService.error($localize`Unable to remove the selected group(s). ` + result.errorText || '');
+    feedbackService.error($localize`Unable to remove the selected group(s). ` + `${result.errorText || ''}`);
   } else {
     feedbackService.partial(
       $localize`${result.countSuccess} group(s) have been removed, ${result.countRequests - result.countSuccess} could
-       not be removed. ` + result.errorText || ''
+       not be removed. ` + `${result.errorText || ''}`
     );
   }
 }
