b4630b8ed7c0d683e74851d1a942dd76db625458
Thomas Juster <juster.thomas@gmail.com>
2022-04-04 15:10:48 +0200

    make test expectation more explicit

--- e2e/src/group-page.e2e-spec.ts
+++ e2e/src/group-page.e2e-spec.ts
@@ -49,7 +49,7 @@ describe('groups/users page', () => {
       await page.logout();
     });
 
-    it('should fail gracefully with an error message and retry cta', async () => {
+    it('should have an empty left nav with an error message and retry cta', async () => {
       // the lines below assert the state is correct and will fail otherwise, no "expect()" is required here.
       await Promise.all([
         page.waitUntilVisible(page.getLeftNavErrorMessage()),
