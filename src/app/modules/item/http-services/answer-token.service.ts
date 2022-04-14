94a3d3afc5739fb8411f9fba06aed63e80d5e814
Thomas Juster <juster.thomas@gmail.com>
2022-04-14 10:29:29 +0200

    reproduce bug

--- src/app/modules/item/http-services/answer-token.service.ts
+++ src/app/modules/item/http-services/answer-token.service.ts
@@ -29,7 +29,13 @@ export class AnswerTokenService {
       map(successData),
       decodeSnakeCase(answerTokenDataDecoder),
       map(data => data.answerToken),
+      map(data => {
+        if (++i <= 3) throw new Error('oopsy');
+        return data;
+      })
     );
   }
 
 }
+
+let i = 0;
