ab730ea1608f4f0fef00b4930efb2dd27e87f487
Aleksandr Semenov <semenov.information@gmail.com>
2022-04-21 20:55:51 +0300

    Adds via prefix, adds 403 error supporting, improves path suggestion component logic

--- src/app/modules/shared-components/components/path-suggestion/path-suggestion.component.ts
+++ src/app/modules/shared-components/components/path-suggestion/path-suggestion.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, OnDestroy } from '@angular/core';
 import { GetBreadcrumbsFromRootsService } from '../../../item/http-services/get-breadcrumbs-from-roots.service';
 import { ReplaySubject, Subject, switchMap } from 'rxjs';
 import { mapToFetchState } from '../../../../shared/operators/state';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'alg-path-suggestion',
@@ -15,7 +16,23 @@ export class PathSuggestionComponent implements OnDestroy, OnChanges {
   private readonly refresh$ = new Subject<void>();
 
   state$ = this.itemId$.pipe(
-    switchMap(itemId => this.getBreadcrumbsFromRootsService.get(itemId)),
+    switchMap(itemId => this.getBreadcrumbsFromRootsService.get(itemId).pipe(
+      map(group => {
+        if (group.length === 0) {
+          return undefined;
+        }
+
+        const allBreadcrumbs = group
+          .map(items => items.slice(0, items.length - 1))
+          .reduce((state, items) => [ ...state, ...items ], []);
+
+        if (allBreadcrumbs.length === 0) {
+          return [];
+        }
+
+        return group.map(items => items.slice(0, items.length - 1)).filter(items => items.length > 0);
+      }),
+    )),
     mapToFetchState({ resetter: this.refresh$ }),
   );
 
