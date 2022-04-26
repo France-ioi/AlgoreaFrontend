e6264937ac6f75ca267a75d02884e9a33530db2f
Thomas Juster <juster.thomas@gmail.com>
2022-04-26 15:49:13 +0200

    add text when task loading is too long

--- src/app/modules/item/components/task-loader/task-loader.component.ts
+++ src/app/modules/item/components/task-loader/task-loader.component.ts
@@ -1,14 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import { SECONDS } from 'src/app/shared/helpers/duration';
 
 @Component({
   selector: 'alg-task-loader',
   templateUrl: './task-loader.component.html',
   styleUrls: [ './task-loader.component.scss' ]
 })
-export class TaskLoaderComponent {
+export class TaskLoaderComponent implements OnInit, OnDestroy {
 
   @Input() label = '';
+  @Input() delayedLabel = '';
+  @Input() delay = 5;
+
+  showDelayedLabel = false;
+  subscription?: Subscription;
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.subscription = timer(this.delay*SECONDS).subscribe(() => {
+      this.showDelayedLabel = true;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 
 }
