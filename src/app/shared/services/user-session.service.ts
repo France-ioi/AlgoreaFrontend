987e9aeeb5c8e298873c1cd0bec350f66ddb263c
Aleksandr Semenov <semenov.information@gmail.com>
2022-06-02 13:08:55 +0300

    Fixes lint error

--- src/app/shared/services/user-session.service.ts
+++ src/app/shared/services/user-session.service.ts
@@ -63,7 +63,7 @@ export class UserSessionService implements OnDestroy {
     const refresh$ = this.currentUserService.refresh().pipe(shareReplay(1));
     refresh$.subscribe({
       next: () => this.userProfileUpdated$.next(),
-      // error has to be handled in the caller of the `refresh()` function 
+      // error has to be handled in the caller of the `refresh()` function
       error: () => {},
     });
     return refresh$;
