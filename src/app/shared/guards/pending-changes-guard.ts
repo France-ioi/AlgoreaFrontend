09860063dcfb8818d1dd8c348153c644e86ab45d
Damien Leroy <dleroy@smad.be>
2022-09-07 11:10:45 +0200

    fix the crash report which is triggered when leaving a component which has not been shown because of loading or error

--- src/app/shared/guards/pending-changes-guard.ts
+++ src/app/shared/guards/pending-changes-guard.ts
@@ -27,14 +27,17 @@ export class PendingChangesGuard implements CanDeactivate<PendingChangesComponen
   ): Observable<boolean> {
     const dialogResponse = new Subject<boolean>();
 
+    // If a component is not defined in router, need to use the PendingChangesService as alternative approach
     const pendingChangesComponent = component || this.pendingChangesService.component;
 
-    // If a component is not defined in router, need to use the PendingChangesService as alternative approach
     if (!pendingChangesComponent) {
-      throw new Error('Unexpected: Component is not defined in router');
+      // The component may still be not set in some specific scenarios.
+      // Example: The routing leads to a "detail" component but its parent decides not to show it (for instance because there is an error
+      //   in the parent). In such a case, when we leave the page, the "detail" component is being deactivated while not existing.
+      return of(true);
     }
 
-    if (!pendingChangesComponent.isDirty()) return of(true) ;
+    if (!pendingChangesComponent.isDirty()) return of(true);
     this.confirmationService.confirm({
       message: $localize`This page has unsaved changes. Do you want to leave this page and lose its changes?`,
       header: $localize`Confirm Navigation`,
