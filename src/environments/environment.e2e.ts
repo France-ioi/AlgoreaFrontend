8e685bc225541a9cdfa1de8fe551f55b1188d81f
Thomas Juster <juster.thomas@gmail.com>
2022-09-21 19:36:11 +0200

    add e2e environment file

---
+++ src/environments/environment.e2e.ts
@@ -0,0 +1,59 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+import { Environment, PartialDeep } from 'src/app/shared/helpers/config';
+
+export const environment: Environment = {
+  production: false,
+  apiUrl: 'https://dev.algorea.org/api',
+  oauthServerUrl: 'https://login.france-ioi.org',
+  forumServerUrl: 'ws://localhost:3001',
+  forumApiUrl: 'http://localhost:3000',
+  oauthClientId: '43',
+
+  defaultActivityId: '4702',
+
+  languages: [
+    { tag: 'fr', path: '/fr/' },
+    { tag: 'en', path: '/' },
+  ],
+  defaultTitle: 'Algorea Platform',
+  languageSpecificTitles: { fr: 'Plateforme Algoréa' },
+  allowForcedToken: true,
+  authType: 'tokens',
+
+  itemPlatformId: 'algorea_backend',
+
+  theme: 'default',
+  featureFlags: {
+    hideTaskTabs: [],
+    skillsDisabled: false,
+  },
+};
+
+type Preset = 'demo';
+export const presets: Record<Preset, PartialDeep<Environment>> = {
+  demo: {
+    defaultActivityId: '1352246428241737349', // SNT
+    defaultTitle: 'Demo app',
+    authType: 'cookies',
+    theme: 'coursera-pt',
+  },
+};
+
+export function getPresetNameByOrigin(origin: string): Preset | null {
+  switch (origin) {
+    case 'http://demo.localhost': return 'demo';
+    default: return null;
+  }
+}
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
