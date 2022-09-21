7ef29b9ab297104c9bf2306fab7673ab4702b8a1
Damien Leroy <dleroy@smad.be>
2022-09-21 12:13:53 +0200

    inject Auth while using the injector to prevent dependency loop

--- src/app/shared/interceptors/authentication.interceptor.ts
+++ src/app/shared/interceptors/authentication.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
@@ -19,27 +19,33 @@ import { errorIsHTTPUnauthenticated } from '../helpers/errors';
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
-  constructor(public auth: AuthService) {}
+  constructor(private injector: Injector) {}
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // skip interception if not req to API or the context skips it
     if (!isRequestToApi(req) || !req.context.get(useAuthInterceptor)) return next.handle(req);
 
-    return this.auth.status$.pipe(
+    // Inject the auth service at runtime injecting in the constructor blocks the whole process because of the dependancy loop
+    // (as auth service call http services)
+    const authService = this.injector.get(AuthService);
+
+    return authService.status$.pipe(
       // take the latest token (and wait for one if the current one is null
       filter<AuthStatus, AuthResult>((auth):auth is AuthResult => auth.authenticated),
       take(1), // prevent auth changes to reset ongoing request
       switchMap(auth => this.callNextWithAuth(req, next, auth).pipe(
         catchError(err => {
-          const currentAuth = this.auth.status$.value;
+          const currentAuth = authService.status$.value;
           // on 401 -> retry once if the auth has changed
           if (errorIsHTTPUnauthenticated(err)) {
-            this.auth.invalidToken(auth); // inform that "auth" is invalid (if still in use)
+            authService.invalidToken(auth); // inform that "auth" is invalid (if still in use)
             // auth has changed in the meantime -> retry
             if (req.context.get(retryOnceOn401) && auth !== currentAuth && currentAuth.authenticated) {
               return this.callNextWithAuth(req, next, currentAuth).pipe(
                 catchError(err => {
-                  if (errorIsHTTPUnauthenticated(err)) this.auth.invalidToken(currentAuth); // inform the token is invalid (if still in use)
+                  if (errorIsHTTPUnauthenticated(err)) {
+                    authService.invalidToken(currentAuth); // inform the token is invalid (if still in use)
+                  }
                   throw err; // rethrow the error anyway (401 or not)
                 })
               );
