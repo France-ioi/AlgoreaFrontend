808e47f81d6b815743ac4571341a8404ce2bd980
Damien Leroy <dleroy@smad.be>
2022-10-20 17:14:07 +0200

    add an heartbeat to prevent WS connection closing by the server

--- src/app/modules/item/services/forum.service.ts
+++ src/app/modules/item/services/forum.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { appConfig } from 'src/app/shared/helpers/config';
-import { map, merge, retry, shareReplay, startWith, Subject } from 'rxjs';
+import { EMPTY, map, merge, retry, shareReplay, startWith, Subject, switchMap, timer } from 'rxjs';
 import { webSocket } from 'rxjs/webSocket';
-import { SECONDS } from 'src/app/shared/helpers/duration';
+import { MINUTES, SECONDS } from 'src/app/shared/helpers/duration';
 
 const wsRetryDelay = 5*SECONDS;
+const heartbeatStartDelay = 1*MINUTES;
+const heartbeatStartPeriod = 4*MINUTES; // API gatetway closes the connection after 10min without activity
 
 @Injectable({
   providedIn: 'root',
@@ -28,9 +30,17 @@ export class ForumService implements OnDestroy {
   // subscribe to the ws so that it stays open until 'destroy'
   private subscription = this.inputMessages$.subscribe();
 
+  private heartbeatSubscription = this.isWsOpen$.pipe(
+    switchMap(open => {
+      if (!open) return EMPTY;
+      return timer(heartbeatStartDelay, heartbeatStartPeriod);
+    })
+  ).subscribe(() => this.send({ action: 'heartbeat' })); // the action is not necessarily recognized, it is used to keep the connection up
+
   constructor() {}
 
   ngOnDestroy(): void {
+    this.heartbeatSubscription.unsubscribe();
     this.subscription.unsubscribe();
     this.ws$.complete();
     this.openEvents$.complete();
