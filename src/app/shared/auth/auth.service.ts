44b059a9a60e8ae33884802a34865c5adb0d5d80
Damien Leroy <dleroy@smad.be>
2022-11-08 16:16:41 +0100

    unsubscribe authservice subscription on destroy

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -40,13 +40,9 @@ export class AuthService implements OnDestroy {
 
   private countInvalidToken = 0;
 
-  constructor(
-    private oauthService: OAuthService,
-    private authHttp: AuthHttpService,
-    private localeService: LocaleService
-  ) {
+  authSubscription = this.oauthService
     // (1) check if a code/state is given in URL (i.e., we are back from a oauth login redirect) and try to get a token from it.
-    oauthService.tryCompletingCodeFlowLogin().pipe(
+    .tryCompletingCodeFlowLogin().pipe(
       catchError(_e => {
         // (2) use the ongoing authentication if any
         if (appConfig.allowForcedToken && hasForcedToken()) return of(forcedTokenAuthFromStorage());
@@ -71,38 +67,45 @@ export class AuthService implements OnDestroy {
       }
     });
 
-    // setup auto-refresh after delay
-    this.status$.pipe(
-      switchMap(auth => {
-        // Max delay for Rx.timer. Otherwise, it triggers immediately (see bug https://github.com/ReactiveX/rxjs/issues/3015)
-        const maxDelay = 2147483647; // 2^31-1
-        if (!auth.authenticated) return EMPTY;
-        // Refresh if the token is valid < `minTokenLifetime` or when it will have reached 50% of its lifetime. Retry every minute.
-        const refreshIn = auth.expiration.getTime() - Date.now() <= minTokenLifetime ? 0 :
-          Math.max((auth.expiration.getTime() + auth.creation.getTime())/2 - Date.now(), 0);
-        return timer(Math.min(refreshIn, maxDelay), 1*MINUTES).pipe(map(() => auth));
-      }),
-      switchMap(auth => {
-        const isExpired = auth.expiration.valueOf() < Date.now();
-        if (isExpired) return of({ auth, tokenIsExpired: true }); // don't even try to refresh the token if it is expired
-        return this.authHttp.refreshAuth().pipe(
-          catchError(err => {
-            // For any http error, ignore it since the token is valid. Another try will occur the next minute.
-            if (err instanceof HttpErrorResponse || err instanceof TimeoutError) return EMPTY;
-            else throw err; // rethrow any JS error to let our error monitor catch it.
-          }),
-          map(auth => ({ auth, tokenIsExpired: false })),
-        );
-      })
-    ).subscribe(result => {
-      if (result.tokenIsExpired) this.invalidToken(result.auth);
-      else this.status$.next(result.auth);
-    });
-  }
+  // auto-refresh after delay
+  autoRefreshSubscription = this.status$.pipe(
+    switchMap(auth => {
+      // Max delay for Rx.timer. Otherwise, it triggers immediately (see bug https://github.com/ReactiveX/rxjs/issues/3015)
+      const maxDelay = 2147483647; // 2^31-1
+      if (!auth.authenticated) return EMPTY;
+      // Refresh if the token is valid < `minTokenLifetime` or when it will have reached 50% of its lifetime. Retry every minute.
+      const refreshIn = auth.expiration.getTime() - Date.now() <= minTokenLifetime ? 0 :
+        Math.max((auth.expiration.getTime() + auth.creation.getTime())/2 - Date.now(), 0);
+      return timer(Math.min(refreshIn, maxDelay), 1*MINUTES).pipe(map(() => auth));
+    }),
+    switchMap(auth => {
+      const isExpired = auth.expiration.valueOf() < Date.now();
+      if (isExpired) return of({ auth, tokenIsExpired: true }); // don't even try to refresh the token if it is expired
+      return this.authHttp.refreshAuth().pipe(
+        catchError(err => {
+          // For any http error, ignore it since the token is valid. Another try will occur the next minute.
+          if (err instanceof HttpErrorResponse || err instanceof TimeoutError) return EMPTY;
+          else throw err; // rethrow any JS error to let our error monitor catch it.
+        }),
+        map(auth => ({ auth, tokenIsExpired: false })),
+      );
+    })
+  ).subscribe(result => {
+    if (result.tokenIsExpired) this.invalidToken(result.auth);
+    else this.status$.next(result.auth);
+  });
+
+  constructor(
+    private oauthService: OAuthService,
+    private authHttp: AuthHttpService,
+    private localeService: LocaleService
+  ) {}
 
   ngOnDestroy(): void {
     this.status$.complete();
     this.failure$.complete();
+    this.authSubscription.unsubscribe();
+    this.autoRefreshSubscription.unsubscribe();
   }
 
   /**
