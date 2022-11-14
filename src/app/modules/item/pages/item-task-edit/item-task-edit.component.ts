0b8ea367fc7f0421b154109fe101f154f7e48b6f
Damien Leroy <dleroy@smad.be>
2022-11-14 15:14:42 +0100

    loading editing url into the task edit tab

--- src/app/modules/item/pages/item-task-edit/item-task-edit.component.ts
+++ src/app/modules/item/pages/item-task-edit/item-task-edit.component.ts
@@ -1,13 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'alg-item-task-edit',
   templateUrl: './item-task-edit.component.html',
   styleUrls: [ './item-task-edit.component.scss' ]
 })
-export class ItemTaskEditComponent {
+export class ItemTaskEditComponent implements OnChanges {
   @Input() editorUrl?: string;
+  sanitizedUrl?: SafeResourceUrl;
 
-  constructor() {}
+  constructor(
+    private sanitizer: DomSanitizer,
+  ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.editorUrl) {
+      this.sanitizedUrl = this.editorUrl ? this.sanitizer.bypassSecurityTrustResourceUrl(this.editorUrl) : undefined;
+    }
+  }
 
 }
