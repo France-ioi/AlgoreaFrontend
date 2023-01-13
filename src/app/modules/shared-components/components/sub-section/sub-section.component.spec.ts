2f4d5dff8f5f322cd804c4ab2550d7d89df9240a
Aleksandr Semenov <semenov.information@gmail.com>
2023-01-13 19:49:48 +0200

    Fixes deprecated async

--- src/app/modules/shared-components/components/sub-section/sub-section.component.spec.ts
+++ src/app/modules/shared-components/components/sub-section/sub-section.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubSectionComponent } from './sub-section.component';
 
@@ -6,7 +6,7 @@ describe('SubSectionComponent', () => {
   let component: SubSectionComponent;
   let fixture: ComponentFixture<SubSectionComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SubSectionComponent ]
     })
