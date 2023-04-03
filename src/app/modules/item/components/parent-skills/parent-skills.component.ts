e2c8fcddabb8dbf781dde2e2a5a1bddc62b9f7ef
Aleksandr Semenov <semenov.information@gmail.com>
2023-04-03 17:25:57 +0300

    Changes click function with router link in parent skills component

--- src/app/modules/item/components/parent-skills/parent-skills.component.ts
+++ src/app/modules/item/components/parent-skills/parent-skills.component.ts
@@ -1,16 +1,10 @@
 import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { ReplaySubject, Subject } from 'rxjs';
 import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
-import { typeCategoryOfItem } from 'src/app/shared/helpers/item-type';
-import { ItemRouter } from 'src/app/shared/routing/item-router';
-import { GetItemParentsService, ItemParent } from '../../http-services/get-item-parents.service';
+import { GetItemParentsService } from '../../http-services/get-item-parents.service';
 import { ItemData } from '../../services/item-datasource.service';
 import { mapToFetchState } from '../../../../shared/operators/state';
 import { canCurrentUserViewContent } from 'src/app/shared/models/domain/item-view-permission';
-
-interface ParentSkillAdditions {
-  isLocked: boolean,
-}
 
 @Component({
   selector: 'alg-parent-skills',
@@ -31,7 +25,6 @@ export class ParentSkillsComponent implements OnChanges, OnDestroy {
 
   constructor(
     private getItemParentsService: GetItemParentsService,
-    private itemRouter: ItemRouter,
   ) {}
 
   ngOnChanges(_changes: SimpleChanges): void {
@@ -41,17 +34,6 @@ export class ParentSkillsComponent implements OnChanges, OnDestroy {
         attemptId: this.itemData.currentResult.attemptId,
       });
     }
-  }
-
-  click(parent: ItemParent&ParentSkillAdditions): void {
-    if (!this.itemData) return;
-
-    this.itemRouter.navigateTo({
-      contentType: typeCategoryOfItem(parent),
-      id: parent.id,
-      path: this.itemData.route.path.slice(0, -1),
-      attemptId: parent.result.attemptId,
-    });
   }
 
   ngOnDestroy(): void {
