3a1e474a38513415144f6936c01ce5fce0da42e9
Aleksandr Semenov <semenov.information@gmail.com>
2023-08-09 16:11:27 +0300

    Improves text style in thread messages and open thread container code

--- src/app/core/app.component.ts
+++ src/app/core/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, NgZone, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { UserSessionService } from '../shared/services/user-session.service';
 import { delay, switchMap, tap } from 'rxjs/operators';
-import { filter, map, merge, Subscription } from 'rxjs';
+import { merge, Subscription } from 'rxjs';
 import { AuthService } from '../shared/auth/auth.service';
 import { Router } from '@angular/router';
 import { LocaleService } from './services/localeService';
@@ -15,7 +15,6 @@ import { CrashReportingService } from './services/crash-reporting.service';
 import { Location } from '@angular/common';
 import { ChunkErrorService } from './services/chunk-error.service';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
-import { isNotUndefined } from '../shared/helpers/null-undefined-predicates';
 import { DiscussionService } from '../modules/item/services/discussion.service';
 
 @Component({
@@ -41,12 +40,11 @@ export class AppComponent implements OnInit, OnDestroy {
   canShowLeftMenu$ = this.layoutService.canShowLeftMenu$;
   showTopRightControls$ = this.layoutService.showTopRightControls$.pipe(delay(0));
   isNarrowScreen$ = this.layoutService.isNarrowScreen$;
-  threadOpened$ = this.discussionService.state$.pipe(filter(isNotUndefined), map(({ visible }) => visible));
+  threadState$ = this.discussionService.state$;
   scrolled = false;
   isWatching$ = this.groupWatchingService.isWatching$;
   watchedGroupError$ = this.groupWatchingService.watchedGroupError$;
   showWatchedGroupErrorDialog = true;
-  showItemThreadWidget = !!appConfig.forumServerUrl;
 
   private subscription?: Subscription;
 
