bddefccac00b6552c837e625b7b799ffa050b1c1
Aleksandr Semenov <semenov.information@gmail.com>
2023-08-10 20:29:25 +0300

    Fixes participant progress decoder

--- src/app/modules/item/http-services/get-participant-progress.service.ts
+++ src/app/modules/item/http-services/get-participant-progress.service.ts
@@ -6,34 +6,41 @@ import { decodeSnakeCase } from '../../../shared/operators/decode';
 import * as D from 'io-ts/Decoder';
 import { dateDecoder } from 'src/app/shared/helpers/decoders';
 import { itemCorePermDecoder } from 'src/app/shared/models/domain/item-permissions';
+import { pipe } from 'fp-ts/function';
 
-const participantProgressDecoder = D.struct({
-  children: D.array(D.struct({
-    currentUserPermissions: itemCorePermDecoder,
-    hintsRequested: D.number,
-    itemId: D.string,
-    latestActivityAt: D.nullable(dateDecoder),
-    noScore: D.boolean,
-    score: D.number,
-    string: D.struct({
-      languageTag: D.string,
-      title: D.nullable(D.string),
-    }),
-    submissions: D.number,
-    timeSpent: D.number,
-    type: D.literal('Chapter', 'Task', 'Skill'),
-    validated: D.boolean,
-  })),
-  item: D.struct({
-    hintsRequested: D.number,
-    itemId: D.string,
-    latestActivityAt: D.nullable(dateDecoder),
-    score: D.number,
-    submissions: D.number,
-    timeSpent: D.number,
-    validated: D.boolean,
-  })
-});
+const participantProgressDecoder = pipe(
+  D.struct({
+    item: D.struct({
+      hintsRequested: D.number,
+      itemId: D.string,
+      latestActivityAt: D.nullable(dateDecoder),
+      score: D.number,
+      submissions: D.number,
+      timeSpent: D.number,
+      validated: D.boolean,
+    })
+  }),
+  D.intersect(
+    D.partial({
+      children: D.array(D.struct({
+        currentUserPermissions: itemCorePermDecoder,
+        hintsRequested: D.number,
+        itemId: D.string,
+        latestActivityAt: D.nullable(dateDecoder),
+        noScore: D.boolean,
+        score: D.number,
+        string: D.struct({
+          languageTag: D.string,
+          title: D.nullable(D.string),
+        }),
+        submissions: D.number,
+        timeSpent: D.number,
+        type: D.literal('Chapter', 'Task', 'Skill'),
+        validated: D.boolean,
+      })),
+    })
+  ),
+);
 
 export type ParticipantProgress = D.TypeOf<typeof participantProgressDecoder>;
 
