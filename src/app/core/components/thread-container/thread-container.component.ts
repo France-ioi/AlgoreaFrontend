2b803d77b34dbd15b4ad0daa24acf54c7c2145ef
Aleksandr Semenov <semenov.information@gmail.com>
2023-10-02 13:24:50 +0300

    Adds item title in thread header

--- src/app/core/components/thread-container/thread-container.component.ts
+++ src/app/core/components/thread-container/thread-container.component.ts
@@ -1,7 +1,11 @@
 import { AfterViewInit, Component, Input, OnDestroy, ViewChild } from '@angular/core';
 import { DiscussionService } from 'src/app/modules/item/services/discussion.service';
-import { filter, Subscription } from 'rxjs';
+import { combineLatest, distinctUntilChanged, filter, Subscription, switchMap } from 'rxjs';
 import { ThreadComponent } from '../thread/thread.component';
+import { ThreadService } from '../../../modules/item/services/threads.service';
+import { UserSessionService } from '../../../shared/services/user-session.service';
+import { GetItemByIdService } from '../../../modules/item/http-services/get-item-by-id.service';
+import { isNotNull } from '../../../shared/helpers/null-undefined-predicates';
 
 @Component({
   selector: 'alg-thread-container',
@@ -14,10 +18,27 @@ export class ThreadContainerComponent implements AfterViewInit, OnDestroy {
   @Input() topCompensation = 0;
 
   visible$ = this.discussionService.visible$;
+  readonly threadItem$ = combineLatest([
+    this.userSessionService.userProfile$,
+    this.threadService.threadId$.pipe(
+      filter(isNotNull),
+      distinctUntilChanged((x,y) => x?.participantId === y?.participantId && x?.itemId === y?.itemId),
+    ),
+    this.discussionService.visible$.pipe(filter(visible => visible), distinctUntilChanged()),
+  ]).pipe(
+    switchMap(([ user, threadId ]) =>
+      this.getItemByIdService.get(threadId.itemId, user.groupId === threadId.participantId ? undefined : threadId.participantId)
+    ),
+  );
 
   private subscription?: Subscription;
 
-  constructor(private discussionService: DiscussionService) {
+  constructor(
+    private discussionService: DiscussionService,
+    private threadService: ThreadService,
+    private userSessionService: UserSessionService,
+    private getItemByIdService: GetItemByIdService,
+  ) {
   }
 
   ngAfterViewInit(): void {
