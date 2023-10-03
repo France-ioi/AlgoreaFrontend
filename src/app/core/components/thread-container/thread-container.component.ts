3f9554426826fb21c00dbe5419a30d6d953d5bc2
Aleksandr Semenov <semenov.information@gmail.com>
2023-10-03 12:02:08 +0300

    Adds error handling for get item title in thread

--- src/app/core/components/thread-container/thread-container.component.ts
+++ src/app/core/components/thread-container/thread-container.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component, Input, OnDestroy, ViewChild } from '@angular/core';
 import { DiscussionService } from 'src/app/modules/item/services/discussion.service';
-import { combineLatest, distinctUntilChanged, filter, Subscription, switchMap } from 'rxjs';
+import { combineLatest, distinctUntilChanged, filter, of, Subscription, switchMap } from 'rxjs';
 import { ThreadComponent } from '../thread/thread.component';
 import { ThreadService } from '../../../modules/item/services/threads.service';
 import { UserSessionService } from '../../../shared/services/user-session.service';
 import { GetItemByIdService } from '../../../modules/item/http-services/get-item-by-id.service';
 import { isNotNull } from '../../../shared/helpers/null-undefined-predicates';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'alg-thread-container',
@@ -19,15 +20,14 @@ export class ThreadContainerComponent implements AfterViewInit, OnDestroy {
 
   visible$ = this.discussionService.visible$;
   readonly threadItem$ = combineLatest([
-    this.userSessionService.userProfile$,
     this.threadService.threadId$.pipe(
       filter(isNotNull),
       distinctUntilChanged((x,y) => x?.participantId === y?.participantId && x?.itemId === y?.itemId),
     ),
     this.discussionService.visible$.pipe(filter(visible => visible), distinctUntilChanged()),
   ]).pipe(
-    switchMap(([ user, threadId ]) =>
-      this.getItemByIdService.get(threadId.itemId, user.groupId === threadId.participantId ? undefined : threadId.participantId)
+    switchMap(([ threadId ]) =>
+      this.getItemByIdService.get(threadId.itemId).pipe(catchError(() => of(null)))
     ),
   );
 
