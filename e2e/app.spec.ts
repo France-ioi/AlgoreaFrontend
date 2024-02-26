2df3392cd433c87c834f120760806b1b0c6d1081
Damien Leroy <dleroy@smad.be>
2024-02-26 15:30:55 +0100

    improve test structure (showcase)

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -5,10 +5,14 @@ test('home page loaded', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByRole('heading', { name: 'Parcours officiels' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'Algorea Platform' })).toBeVisible();
-  // children list
-  await expect(page.locator('alg-item-children-list')).toContainText('ALGOREA SERIOUS GAME');
-  // left menu
-  await expect(page.locator('#nav-1471479157476024035')).toContainText('ALGOREA SERIOUS GAME');
+
+  await test.step('check children list', async () => {
+    await expect(page.locator('alg-item-children-list')).toContainText('ALGOREA SERIOUS GAME');
+  });
+
+  await test.step('check left menu', async () => {
+    await expect(page.locator('#nav-1471479157476024035')).toContainText('ALGOREA SERIOUS GAME');
+  });
 });
 
 test('home page loaded as usual user', async ({ page }) => {
