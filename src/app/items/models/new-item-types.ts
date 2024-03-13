cf1f4c5c899b3231b8314f58626046107d7f5226
Aleksandr Semenov <semenov.information@gmail.com>
2024-03-13 16:04:07 +0200

    Replaces icons for new item

--- src/app/items/models/new-item-types.ts
+++ src/app/items/models/new-item-types.ts
@@ -4,19 +4,19 @@ import { ActivityType, ItemType } from './item-type';
 export const allowedNewActivityTypes: NewContentType<ActivityType>[] = [
   {
     type: 'Chapter',
-    icon: 'fa fa-book',
+    icon: 'ph-duotone ph-book',
     title: $localize`Chapter`,
     description: $localize`A new folder which can contain any activities.`,
   },
   {
     type: 'Task',
-    icon: 'fa fa-code',
+    icon: 'ph-duotone ph-code',
     title: $localize`Empty task`,
     description: $localize`A new task which will be configured later.`,
   },
   {
     type: 'Task',
-    icon: 'fa fa-code',
+    icon: 'ph-duotone ph-code',
     title: $localize`Task`,
     description: $localize`A new task configured with a given url.`,
     allowToAddUrl: true,
@@ -25,7 +25,7 @@ export const allowedNewActivityTypes: NewContentType<ActivityType>[] = [
 
 export const skillNewType: NewContentType<ItemType> = {
   type: 'Skill',
-  icon: 'fa fa-graduation-cap',
+  icon: 'ph-duotone ph-graduation-cap',
   title: $localize`Skill`,
   description: $localize`A new skill.`,
 };
