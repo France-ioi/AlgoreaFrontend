4e0223f931e3686f51efe8afe446c64900c72b99
Aleksandr Semenov <semenov.information@gmail.com>
2024-04-09 14:24:43 +0300

    Removes unused code

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -2,8 +2,6 @@ import { test, expect } from '@playwright/test';
 import { initAsUsualUser } from './helpers/e2e_auth';
 import { apiUrl } from './helpers/e2e_http';
 
-test.describe.configure({ mode: 'parallel' });
-
 test('home page loaded', async ({ page }) => {
   await page.goto('/');
   await expect.soft(page.getByRole('heading', { name: 'Parcours officiels' })).toBeVisible();
