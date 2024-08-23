0b56336b40adf166e2b467a3a37f04972230fbd7
Aleksandr Semenov <semenov.information@gmail.com>
2024-08-23 16:38:35 +0300

    Adds e2e tests for item edit parameters

---
+++ e2e/items/item-parameters.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+import { initAsUsualUser } from '../helpers/e2e_auth';
+
+test('checks no access message', async ({ page }) => {
+  await initAsUsualUser(page);
+  await page.goto('a/1625159049301502151;p=4702;a=0/parameters');
+  await expect.soft(page.getByText('You do not have the permissions to edit this content.')).toBeVisible();
+});
+
+test('checks edit parameters components are visible', async ({ page }) => {
+  await initAsUsualUser(page);
+  await page.goto('a/6707691810849260111;p=;a=0/parameters');
+  await expect.soft(page.getByText('Information')).toBeVisible();
+  await expect.soft(page.getByRole('heading', { name: 'Information' })).toBeVisible();
+  await expect.soft(page.getByRole('heading', { name: 'Description' })).toBeVisible();
+  await expect.soft(page.getByRole('heading', { name: 'Score & Validation' })).toBeVisible();
+});
