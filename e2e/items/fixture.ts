919bad6e0a6e4423960d019c9dfe9dfa355603d3
Aleksandr Semenov <semenov.information@gmail.com>
2024-09-12 17:46:57 +0300

    Adds more permissions for display

--- e2e/items/fixture.ts
+++ e2e/items/fixture.ts
@@ -1,10 +1,12 @@
 import { test as base } from '@playwright/test';
 import { ItemContentPage } from 'e2e/items/pages/item-content-page';
 import { EditPermissionsModal } from 'e2e/items/pages/edit-permissions-modal';
+import { ItemPermissionsComponent } from 'e2e/items/pages/item-permissions-component';
 
 interface ItemFixtures {
   itemContentPage: ItemContentPage,
   editPermissionsModal: EditPermissionsModal,
+  itemPermissionsComponent: ItemPermissionsComponent,
 }
 
 export const test = base.extend<ItemFixtures>({
@@ -13,6 +15,9 @@ export const test = base.extend<ItemFixtures>({
   },
   editPermissionsModal: async ({ page }, use) => {
     await use(new EditPermissionsModal(page));
+  },
+  itemPermissionsComponent: async ({ page }, use) => {
+    await use(new ItemPermissionsComponent(page));
   },
 });
 
