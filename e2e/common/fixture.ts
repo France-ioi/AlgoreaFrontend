baff037156565f3b72c95cb2238e71efa17e5cf4
Aleksandr Semenov <semenov.information@gmail.com>
2024-10-15 14:42:40 +0300

    Changes approach for create e2e group, adds clean old e2e groups

--- e2e/common/fixture.ts
+++ e2e/common/fixture.ts
@@ -1,13 +1,18 @@
-import { test as base } from '@playwright/test';
 import { LostChangesConfirmationModal } from 'e2e/common/pages/lost-changes-confirmation-modal';
+import { Toast } from 'e2e/common/pages/toast';
+import { test as groupFixtures } from '../groups/create-group-fixture';
 
 interface CommonFixtures {
   lostChangesConfirmationModal: LostChangesConfirmationModal,
+  toast: Toast,
 }
 
-export const test = base.extend<CommonFixtures>({
+export const test = groupFixtures.extend<CommonFixtures>({
   lostChangesConfirmationModal: async ({ page }, use) => {
     await use(new LostChangesConfirmationModal(page));
+  },
+  toast: async ({ page }, use) => {
+    await use(new Toast(page));
   },
 });
 
