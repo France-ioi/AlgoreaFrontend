bea8d62836286acbfa73ec7d8da902ec54deb3af
Aleksandr Semenov <semenov.information@gmail.com>
2024-10-25 16:25:20 +0300

    Adds checking tests for select i18 for associated item info message

--- e2e/fr-translation.spec.ts
+++ e2e/fr-translation.spec.ts
@@ -121,3 +121,68 @@ test('checks select for no content in left menu', async ({ page }) => {
   await expect.soft(page.getByText('Vous n\'êtes membre ou gestionnaire d\'aucun groupe')).toBeVisible();
 });
 
+// To be translated
+test('checks select for associated item can view info message', async ({ page, groupSettingsPage }) => {
+  await initAsTesterUser(page);
+  await groupSettingsPage.goto('/groups/by-id/1769157720761729219;p=7002637463896228384/settings?watchedGroupId=1769157720761729219');
+  const associatedItemLocator = page.locator('alg-associated-item').filter({ hasText: 'Activité associée' });
+  await expect.soft(associatedItemLocator).toBeVisible();
+  await expect.soft(associatedItemLocator.getByText('E2E: Associated Item')).toBeVisible();
+  await expect.soft(
+    page.locator('alg-message-info').getByText('The activité is visible to the members of this group. They will see it in the left menu, in the "activité" tab.')
+  ).toBeVisible();
+});
+
+test('checks select for associated item no access message', async ({ page, groupSettingsPage }) => {
+  await initAsTesterUser(page);
+  await groupSettingsPage.goto('groups/by-id/9024243890537335167;p=7002637463896228384/settings?watchedGroupId=9024243890537335167');
+  const associatedItemLocator = page.locator('alg-associated-item').filter({ hasText: 'Activité associée' });
+  await expect.soft(associatedItemLocator).toBeVisible();
+  await expect.soft(associatedItemLocator.getByText('E2E: Associated Item')).toBeVisible();
+  await expect.soft(
+    page.locator('alg-message-info').getByText('This activité may not be visible to the members of this group. You can grant them access by "observing" the group')
+  ).toBeVisible();
+});
+
+test('checks select for associated item with can\'t view and can\'t grant perm message', async ({ page, groupSettingsPage }) => {
+  await initAsTesterUser(page);
+  await groupSettingsPage.goto('/groups/by-id/9189508152532044730;p=7002637463896228384/settings?watchedGroupId=9189508152532044730');
+  const associatedItemLocator = page.locator('alg-associated-item').filter({ hasText: 'Activité associée' });
+  await expect.soft(associatedItemLocator).toBeVisible();
+  await expect.soft(associatedItemLocator.getByText('E2E: Associated Item')).toBeVisible();
+  await expect.soft(
+    page.locator('alg-message-info').getByText('You are not allowed to give access to this group, ask another manager to do so.')
+  ).toBeVisible();
+});
+
+test('checks select for associated item with current user can\'t grant and group can\'t view message', async ({ page, groupSettingsPage }) => {
+  await initAsTesterUser(page);
+  await groupSettingsPage.goto('/groups/by-id/752055703863185594;p=7002637463896228384/settings?watchedGroupId=752055703863185594');
+  const associatedItemLocator = page.locator('alg-associated-item').filter({ hasText: 'Activité associée' });
+  await expect.soft(associatedItemLocator).toBeVisible();
+  await expect.soft(associatedItemLocator.getByText('E2E: Associated Item')).toBeVisible();
+  await expect.soft(page.locator('alg-message-info').getByText('This activité may not be visible to the members of this group. You are not allowed to grant access to this content, ask someone who has the permission to grant access to this content or make sure all your users can view the activité by other means (e.g. if it is public).')).toBeVisible();
+});
+
+test('checks select for associated item current user cannot grant permission to the group', async ({ page, groupSettingsPage }) => {
+  await initAsTesterUser(page);
+  await groupSettingsPage.goto('/groups/by-id/3434783988769832015;p=7002637463896228384/settings?watchedGroupId=3434783988769832015');
+  const associatedItemLocator = page.locator('alg-associated-item').filter({ hasText: 'Activité associée' });
+  await expect.soft(associatedItemLocator).toBeVisible();
+  await expect.soft(associatedItemLocator.getByText('E2E: Associated Item no perm')).toBeVisible();
+  await expect.soft(
+    page.locator('alg-message-info').getByText('You cannot view the permissions given to this group, so we cannot determine if it has access to this activité.')
+  ).toBeVisible();
+});
+
+test('checks select for associated item current user either cannot watch or grant perm to the item', async ({ page, groupSettingsPage }) => {
+  await initAsTesterUser(page);
+  await groupSettingsPage.goto('/groups/by-id/4113242269106225458;p=7002637463896228384/settings?watchedGroupId=4113242269106225458');
+  const associatedItemLocator = page.locator('alg-associated-item').filter({ hasText: 'Activité associée' });
+  await expect.soft(associatedItemLocator).toBeVisible();
+  await expect.soft(associatedItemLocator.getByText('E2E: Associated Item no perm')).toBeVisible();
+  await expect.soft(
+    page.locator('alg-message-info').getByText('You cannot view the permissions given to this activité, so we cannot determine if the group has access to it.')
+  ).toBeVisible();
+});
+
