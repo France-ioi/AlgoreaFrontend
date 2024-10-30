bc34a8ff5bef55338c2fc7461eccec01c747a6db
Aleksandr Semenov <semenov.information@gmail.com>
2024-10-30 18:33:29 +0200

    Replaces native scrollbar with custom for group indicator

--- src/app/groups/containers/group-indicator/group-indicator.component.ts
+++ src/app/groups/containers/group-indicator/group-indicator.component.ts
@@ -2,13 +2,14 @@ import { Component, Input } from '@angular/core';
 import { Group } from '../../data-access/get-group-by-id.service';
 import { GroupLinksComponent } from '../group-links/group-links.component';
 import { NgSwitch, NgSwitchCase } from '@angular/common';
+import { NgScrollbar } from 'ngx-scrollbar';
 
 @Component({
   selector: 'alg-group-indicator',
   templateUrl: './group-indicator.component.html',
   styleUrls: [ './group-indicator.component.scss' ],
   standalone: true,
-  imports: [ NgSwitch, NgSwitchCase, GroupLinksComponent ]
+  imports: [ NgSwitch, NgSwitchCase, GroupLinksComponent, NgScrollbar ]
 })
 export class GroupIndicatorComponent {
   @Input() group?: Group;
