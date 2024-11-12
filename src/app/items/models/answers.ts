7836a9b9c0ea5099f9a0e05f2c9d7dc16c3cdb6c
Damien Leroy <dleroy@smad.be>
2024-11-12 10:28:38 +0100

    fix mistake

--- src/app/items/models/answers.ts
+++ src/app/items/models/answers.ts
@@ -3,5 +3,5 @@ import { Answer } from '../data-access/get-answer.service';
 type StateAnswer = Pick<Answer, 'answer'|'state'>;
 
 export function areStateAnswerEqual(x: StateAnswer, y: StateAnswer): boolean {
-  return (x.answer ?? '') === (y.answer ?? '') && (y.state ?? '') === (y.state ?? '');
+  return (x.answer ?? '') === (y.answer ?? '') && (x.state ?? '') === (y.state ?? '');
 }
