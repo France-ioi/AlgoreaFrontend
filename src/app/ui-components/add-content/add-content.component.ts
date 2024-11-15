30b498b13d392e65f406c2fdc8f4a06b32cc311e
Aleksandr Semenov <semenov.information@gmail.com>
2024-11-15 11:44:00 +0200

    Removes diacritical marks from search content

--- src/app/ui-components/add-content/add-content.component.ts
+++ src/app/ui-components/add-content/add-content.component.ts
@@ -110,7 +110,7 @@ export class AddContentComponent<Type> implements OnInit, OnDestroy {
     const existingTitleControl: Observable<string> | undefined = this.addContentForm.get('searchExisting')?.valueChanges;
     if (existingTitleControl) this.subscriptions.push(
       existingTitleControl.pipe(
-        map(value => value.trim()),
+        map(value => value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').trim()),
         filter(value => this.checkLength(value)),
         debounceTime(300),
         switchMap(value => searchFunction(value).pipe(mapToFetchState())),
