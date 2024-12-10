fb0f1dc6b5e0fb20a073b90374f0d292eb6f33e3
Aleksandr Semenov <semenov.information@gmail.com>
2024-12-10 18:35:14 +0200

    Converts io decoder into zod in get group by id service

--- src/app/groups/models/group-management.ts
+++ src/app/groups/models/group-management.ts
@@ -1,5 +1,6 @@
 import { pipe } from 'fp-ts/function';
 import * as D from 'io-ts/Decoder';
+import { z } from 'zod';
 
 const managershipOpts = {
   none: 'none',
@@ -26,6 +27,22 @@ export const groupManagershipDecoder = pipe(
     })
   )
 );
+export const groupManagershipSchema = z.object({
+  currentUserManagership: z.union([
+    z.literal(managershipOpts.none),
+    z.literal(managershipOpts.descendant),
+    z.literal(managershipOpts.direct),
+    z.literal(managershipOpts.ancestor),
+  ]),
+}).and(z.object({
+  currentUserCanGrantGroupAccess: z.boolean(),
+  currentUserCanManage: z.union([
+    z.literal(managementLevelOpts.none),
+    z.literal(managementLevelOpts.memberships),
+    z.literal(managementLevelOpts.membershipsAndGroup),
+  ]),
+  currentUserCanWatchMembers: z.boolean(),
+}).partial());
 
 type GroupManagership = D.TypeOf<typeof groupManagershipDecoder>;
 
