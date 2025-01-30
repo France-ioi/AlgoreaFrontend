672752159257b545ba0febd64fe43c317acb64ca
Aleksandr Semenov <semenov.information@gmail.com>
2025-01-30 12:53:19 +0200

    Replaces new button in top right controls

--- src/app/containers/top-right-controls/top-right-controls.component.ts
+++ src/app/containers/top-right-controls/top-right-controls.component.ts
@@ -2,18 +2,18 @@ import { Component, Input } from '@angular/core';
 import { delay } from 'rxjs/operators';
 import { UserSessionService } from '../../services/user-session.service';
 import { LocaleService } from '../../services/localeService';
-import { ButtonModule } from 'primeng/button';
 import { LanguagePickerComponent } from '../language-picker/language-picker.component';
 import { TopRightMenuComponent } from '../top-right-menu/top-right-menu.component';
 import { NgClass, NgIf } from '@angular/common';
 import { LetDirective } from '@ngrx/component';
+import { ButtonIconComponent } from 'src/app/ui-components/button-icon/button-icon.component';
 
 @Component({
   selector: 'alg-top-right-controls',
   templateUrl: './top-right-controls.component.html',
   styleUrls: [ './top-right-controls.component.scss' ],
   standalone: true,
-  imports: [ LetDirective, NgClass, NgIf, TopRightMenuComponent, LanguagePickerComponent, ButtonModule ]
+  imports: [ LetDirective, NgClass, NgIf, TopRightMenuComponent, LanguagePickerComponent, ButtonIconComponent ]
 })
 export class TopRightControlsComponent{
 
