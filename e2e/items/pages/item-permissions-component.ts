ecd95ef467497249fdefe10bff31e939ee80cb59
Damien Leroy <dleroy@smad.be>
2025-02-19 10:49:19 +0100

    fix the e2e as text change was causing an issue for the check

--- e2e/items/pages/item-permissions-component.ts
+++ e2e/items/pages/item-permissions-component.ts
@@ -3,6 +3,7 @@ import { expect, Page } from '@playwright/test';
 export class ItemPermissionsComponent {
   itemPermissionsLocator = this.page.locator('alg-item-permissions');
   sectionHeaderLocator = this.itemPermissionsLocator.locator('alg-section-header');
+  itemPermissionListLocator = this.itemPermissionsLocator.locator('.permission-indicator-section');
 
   constructor(private page: Page) {
   }
@@ -12,11 +13,11 @@ export class ItemPermissionsComponent {
   }
 
   async checksIsPermissionRowWithValueVisible(name: string, value: string): Promise<void> {
-    await expect.soft(this.itemPermissionsLocator.getByText(name)).toBeVisible();
+    await expect.soft(this.itemPermissionListLocator.getByText(name)).toBeVisible();
     await expect.soft(this.sectionHeaderLocator.filter({ hasText: name }).getByText(value)).toBeVisible();
   }
 
   async checksIsPermissionRowNotVisible(name: string): Promise<void> {
-    await expect.soft(this.itemPermissionsLocator.getByText(name)).not.toBeVisible();
+    await expect.soft(this.itemPermissionListLocator.getByText(name)).not.toBeVisible();
   }
 }
