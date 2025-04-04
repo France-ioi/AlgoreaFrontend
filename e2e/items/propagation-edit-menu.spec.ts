2ee664e6347201c65ac0e378a0ec9e661bc5e2d9
Aleksandr Semenov <semenov.information@gmail.com>
2025-04-04 12:55:37 +0300

    Fixes e2e test case

--- e2e/items/propagation-edit-menu.spec.ts
+++ e2e/items/propagation-edit-menu.spec.ts
@@ -3,9 +3,9 @@ import { initAsUsualUser } from 'e2e/helpers/e2e_auth';
 
 test('checks propagation edit menu', async ({ page }) => {
   await initAsUsualUser(page);
-  await page.goto('a/899800937596940136;p=;pa=0/edit-children');
-  await expect.soft(page.getByTestId('item-title')).toHaveText('4LA 1ère année UE Informatique');
-  const targetTdLocator = page.getByRole('row').filter({ has: page.getByText('CC1') });
+  await page.goto('a/433058822737374194;p=39530140456452546,504065524219241180,1053382493721676;a=0;pa=0/edit-children');
+  await expect.soft(page.getByTestId('item-title')).toHaveText('PAPL');
+  const targetTdLocator = page.getByRole('row').filter({ has: page.getByText('Séance 6') });
   await expect.soft(targetTdLocator).toBeVisible();
   const menuButtonLocator = targetTdLocator.getByRole('button');
   await expect.soft(menuButtonLocator).toBeVisible();
