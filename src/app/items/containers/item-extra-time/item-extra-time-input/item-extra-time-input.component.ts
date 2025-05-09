a90b44fe115cde13d151e70a39a75483cf40bfaa
Aleksandr Semenov <semenov.information@gmail.com>
2025-05-09 12:43:18 +0300

    Replaces model signal with signal

--- src/app/items/containers/item-extra-time/item-extra-time-input/item-extra-time-input.component.ts
+++ src/app/items/containers/item-extra-time/item-extra-time-input/item-extra-time-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, input, model, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonIconComponent } from 'src/app/ui-components/button-icon/button-icon.component';
@@ -16,7 +16,7 @@ import { ButtonIconComponent } from 'src/app/ui-components/button-icon/button-ic
 })
 export class ItemExtraTimeInputComponent {
   initialValue = input.required<number>();
-  value = model<number | null>(null); // The p-inputNumber value is nullable
+  value = signal<number | null>(null); // The p-inputNumber value is nullable
   disabled = input(false);
   saveEvent = output<number>();
 
