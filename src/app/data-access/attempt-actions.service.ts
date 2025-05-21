a7a23c558163597533142eaade0c270a34d6dd94
Damien Leroy <dleroy@smad.be>
2025-05-21 09:30:28 +0200

    more service adapatation to config change (3)

--- src/app/data-access/attempt-actions.service.ts
+++ src/app/data-access/attempt-actions.service.ts
@@ -3,19 +3,21 @@ import { HttpClient } from '@angular/common/http';
 import { CreateResponse, createdId } from './action-response';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { appConfig } from '../utils/config';
+import { APPCONFIG } from '../app.config';
+import { inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AttemptActionsService {
+  private config = inject(APPCONFIG);
 
   constructor(private http: HttpClient) {}
 
   create(itemIdPath: string[], parentAttemptId: string): Observable<string> {
     const path = itemIdPath.join('/');
     return this.http
-      .post<CreateResponse>(`${appConfig.apiUrl}/items/${path}/attempts`, null, {
+      .post<CreateResponse>(`${this.config.apiUrl}/items/${path}/attempts`, null, {
         params: {
           parent_attempt_id: parentAttemptId
         }
