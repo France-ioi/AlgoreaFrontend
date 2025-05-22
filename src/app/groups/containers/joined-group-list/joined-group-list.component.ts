dc475897cb9fbb73a4be8ab3717b925727fe0e6f
Aleksandr Semenov <semenov.information@gmail.com>
2025-05-22 12:23:44 +0300

    Adds confirmation modal component, replaces it in joined group list

--- src/app/groups/containers/joined-group-list/joined-group-list.component.ts
+++ src/app/groups/containers/joined-group-list/joined-group-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
-import { ConfirmationService, SortEvent, SharedModule } from 'primeng/api';
+import { SortEvent, SharedModule } from 'primeng/api';
 import { ReplaySubject, Subject } from 'rxjs';
-import { distinctUntilChanged, startWith, switchMap } from 'rxjs/operators';
+import { distinctUntilChanged, filter, startWith, switchMap } from 'rxjs/operators';
 import { GroupMembership, JoinedGroupsService } from 'src/app/data-access/joined-groups.service';
 import { NO_SORT, sortEquals, multisortEventToOptions, SortOptions } from 'src/app/data-access/sort-options';
 import { mapToFetchState } from 'src/app/utils/operators/state';
@@ -13,8 +13,9 @@ import { RouterLink } from '@angular/router';
 import { TableModule } from 'primeng/table';
 import { ErrorComponent } from 'src/app/ui-components/error/error.component';
 import { NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, AsyncPipe, DatePipe } from '@angular/common';
-import { ButtonComponent } from 'src/app/ui-components/button/button.component';
 import { ButtonIconComponent } from 'src/app/ui-components/button-icon/button-icon.component';
+import { Dialog } from '@angular/cdk/dialog';
+import { ConfirmationModalComponent, ConfirmationModalData } from 'src/app/ui-components/confirmation-modal/confirmation-modal.component';
 
 @Component({
   selector: 'alg-joined-group-list',
@@ -34,7 +35,6 @@ import { ButtonIconComponent } from 'src/app/ui-components/button-icon/button-ic
     RippleModule,
     AsyncPipe,
     DatePipe,
-    ButtonComponent,
     ButtonIconComponent
   ],
 })
@@ -48,10 +48,12 @@ export class JoinedGroupListComponent implements OnDestroy {
     mapToFetchState({ resetter: this.refresh$.asObservable() }),
   );
 
-  constructor(private joinedGroupsService: JoinedGroupsService,
-              private groupLeaveService: GroupLeaveService,
-              private confirmationService: ConfirmationService,
-              private actionFeedbackService: ActionFeedbackService) {}
+  constructor(
+    private joinedGroupsService: JoinedGroupsService,
+    private groupLeaveService: GroupLeaveService,
+    private actionFeedbackService: ActionFeedbackService,
+    private dialogService: Dialog,
+  ) {}
 
   ngOnDestroy(): void {
     this.sort$.complete();
@@ -62,34 +64,27 @@ export class JoinedGroupListComponent implements OnDestroy {
     if (sort) this.sort$.next(sort);
   }
 
-  onGroupLeaveClick(event: Event, membership: GroupMembership): void {
-    this.confirmationService.confirm({
-      target: event.target || undefined,
-      key: 'commonPopup',
-      message: $localize`Are you sure you want to leave this group?`,
-      header: $localize`Confirm Action`,
-      icon: 'ph-duotone ph-warning-circle',
-      acceptLabel: $localize`Yes, leave group`,
-      accept: () => {
-        this.leaveGroup(membership);
-      },
-      rejectLabel: $localize`No`,
-    });
-  }
-
-  leaveGroup(membership: GroupMembership): void {
-    const groupId = membership.group.id;
+  onGroupLeaveClick(membership: GroupMembership): void {
     const groupName = membership.group.name;
-    this.groupLeaveService.leave(groupId)
-      .subscribe({
-        next: () => {
-          this.refresh();
-          this.actionFeedbackService.success($localize`You have left "${groupName}"`);
-        },
-        error: _err => {
-          this.actionFeedbackService.error($localize`Failed to leave "${groupName}"`);
-        }
-      });
+    this.dialogService.open<boolean, ConfirmationModalData>(ConfirmationModalComponent, {
+      data: {
+        message: $localize`Are you sure you want to leave this group?`,
+        messageIcon: 'ph-duotone ph-warning-circle',
+        acceptButtonCaption: $localize`Yes, leave group`,
+        rejectButtonCaption: $localize`No`,
+      },
+    }).closed.pipe(
+      filter(accepted => !!accepted),
+      switchMap(() => this.groupLeaveService.leave(membership.group.id)),
+    ).subscribe({
+      next: () => {
+        this.refresh();
+        this.actionFeedbackService.success($localize`You have left "${groupName}"`);
+      },
+      error: _err => {
+        this.actionFeedbackService.error($localize`Failed to leave "${groupName}"`);
+      },
+    });
   }
 
   refresh(): void {
