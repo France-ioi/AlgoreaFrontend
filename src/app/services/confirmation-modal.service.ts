d3232891a6edca4fd3c794c74ae289975a27aa3b
Aleksandr Semenov <semenov.information@gmail.com>
2025-05-28 19:12:07 +0300

    Moves custom confirmation dialog into service

---
+++ src/app/services/confirmation-modal.service.ts
@@ -0,0 +1,30 @@
+import { inject, Injectable } from '@angular/core';
+import { Dialog, DialogConfig, DialogRef } from '@angular/cdk/dialog';
+import { ConfirmationModalComponent } from 'src/app/ui-components/confirmation-modal/confirmation-modal.component';
+import { Observable } from 'rxjs';
+
+export interface ConfirmationModalData {
+  message: string,
+  messageIconStyleClass?: string,
+  acceptButtonCaption?: string,
+  acceptButtonStyleClass?: string,
+  rejectButtonCaption?: string,
+  rejectButtonStyleClass?: string,
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfirmationModalService {
+  private dialogService = inject(Dialog);
+
+  open<D = ConfirmationModalData, R = boolean>(
+    data: D,
+    config?: Omit<DialogConfig<D, DialogRef<R>>, 'data'>,
+  ): Observable<R | undefined> {
+    return this.dialogService.open<R, D>(ConfirmationModalComponent, {
+      data,
+      ...config
+    }).closed;
+  }
+}
