0b51fd7b072e2a94a6f43e0c330d36a390f843e1
Damien Leroy <dleroy@smad.be>
2025-06-03 16:57:42 +0200

    convert thread & forum ws to zod

--- src/app/data-access/thread.service.ts
+++ src/app/data-access/thread.service.ts
@@ -3,30 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, map, of, switchMap } from 'rxjs';
 import { APPCONFIG } from '../app.config';
 import { inject } from '@angular/core';
-import { z } from 'zod';
 import { decodeSnakeCaseZod } from 'src/app/utils/operators/decode';
 import jwtDecode from 'jwt-decode';
-
-const threadSchema = z.object({
-  itemId: z.string(),
-  participantId: z.string(),
-  status: z.enum([ 'not_started', 'waiting_for_participant', 'waiting_for_trainer', 'closed' ]),
-  token: z.string(),
-});
-
-type Thread = z.infer<typeof threadSchema>;
-
-const threadTokenSchema = z.object({
-  itemId: z.string(),
-  participantId: z.string(),
-  userId: z.string(),
-  isMine: z.boolean(),
-  canWatch: z.boolean(),
-  canWrite: z.boolean(),
-});
-type ThreadToken = z.infer<typeof threadTokenSchema>;
-
-export type ThreadInfo = Thread & ThreadToken;
+import { Thread, threadSchema, threadTokenSchema } from '../forum/models/threads';
 
 @Injectable({
   providedIn: 'root',
@@ -36,7 +15,7 @@ export class ThreadService {
 
   constructor(private http: HttpClient) {}
 
-  get(itemId: string, participantId: string): Observable<ThreadInfo> {
+  get(itemId: string, participantId: string): Observable<Thread> {
     return this.http.get<unknown>(`${this.config.apiUrl}/items/${itemId}/participant/${participantId}/thread`).pipe(
       decodeSnakeCaseZod(threadSchema),
       switchMap(thread => of(jwtDecode(thread.token, { header: false })).pipe(
