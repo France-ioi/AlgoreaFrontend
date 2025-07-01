f91c67e83899d825e4275264fad482f7995fee94
Aleksandr Semenov <semenov.information@gmail.com>
2025-07-01 16:38:42 +0300

    Adds e2e test for select control

---
+++ e2e/items/item-parameters-validation-criteria.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '../common/fixture';
+import { initAsTesterUser } from '../helpers/e2e_auth';
+import { rootItemId } from 'e2e/items/create-item-fixture';
+
+test.beforeEach(async ({ page }) => {
+  await initAsTesterUser(page);
+});
+
+test.afterEach(({ deleteItem }) => {});
+
+test('checks edit parameters - validation criteria', async ({ page, createItem, itemContentPage }, use) => {
+  if (!createItem) throw new Error('The item is not created');
+  await page.goto(`/a/${createItem.itemId};p=${rootItemId};pa=0/parameters`);
+  const selectLocator = page.locator('alg-select').filter({ has: page.getByText('All children validated') });
+  await expect.soft(selectLocator).toBeVisible();
+  await selectLocator.click();
+  const targetOptionLocator = page.locator('alg-select-option').getByText('Never');
+  await expect.soft(targetOptionLocator).toBeVisible();
+  await targetOptionLocator.click();
+  await itemContentPage.saveChangesAndCheckNotification();
+  await expect.soft(page.locator('alg-select').getByText('Never')).toBeVisible();
+});
