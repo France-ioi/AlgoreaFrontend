a966a3307e5ba8789b0e2d427fc788fd57343fb1
Damien Leroy <dleroy@smad.be>
2025-09-04 17:57:48 +0200

    'url' pipe: use routers directly so that page can be implitly deduced

--- src/app/pipes/routeUrl.ts
+++ src/app/pipes/routeUrl.ts
@@ -1,17 +1,19 @@
 import { inject, Pipe, PipeTransform } from '@angular/core';
-import { APPCONFIG } from '../config';
-import { UrlCommand } from '../utils/url';
-import { isRawGroupRoute, RawGroupRoute, urlArrayForGroupRoute } from '../models/routing/group-route';
+import { isRawGroupRoute, RawGroupRoute } from '../models/routing/group-route';
 import { RawItemRoute } from '../models/routing/item-route';
-import { itemRouteAsUrlCommand } from '../models/routing/item-route-serialization';
+import { ItemRouter } from '../models/routing/item-router';
+import { GroupRouter } from '../models/routing/group-router';
+import { UrlTree } from '@angular/router';
 
 @Pipe({
   name: 'url', pure: true,
   standalone: true
 })
 export class RouteUrlPipe implements PipeTransform {
-  private config = inject(APPCONFIG);
-  transform(route: RawItemRoute|RawGroupRoute, page?: string[]): UrlCommand {
-    return isRawGroupRoute(route) ? urlArrayForGroupRoute(route, page) : itemRouteAsUrlCommand(route, this.config.redirects, page);
+  private itemRouter = inject(ItemRouter);
+  private groupRouter = inject(GroupRouter);
+
+  transform(route: RawItemRoute|RawGroupRoute, page?: string[]): UrlTree {
+    return isRawGroupRoute(route) ? this.groupRouter.url(route, page) : this.itemRouter.url(route, page);
   }
 }
