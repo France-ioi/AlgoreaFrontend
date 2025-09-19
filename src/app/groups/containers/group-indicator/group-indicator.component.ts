79fa6d317b2b85d20761f27e2b2b5976abedfa72
Damien Leroy <dleroy@smad.be>
2025-09-19 17:06:19 +0200

    convert group indicator to new control flows

--- src/app/groups/containers/group-indicator/group-indicator.component.ts
+++ src/app/groups/containers/group-indicator/group-indicator.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Group } from '../../models/group';
 import { GroupLinksComponent } from '../group-links/group-links.component';
-import { NgSwitch, NgSwitchCase } from '@angular/common';
 import { NgScrollbar } from 'ngx-scrollbar';
 
 @Component({
@@ -9,10 +8,11 @@ import { NgScrollbar } from 'ngx-scrollbar';
   templateUrl: './group-indicator.component.html',
   styleUrls: [ './group-indicator.component.scss' ],
   standalone: true,
-  imports: [ NgSwitch, NgSwitchCase, GroupLinksComponent, NgScrollbar ]
+  imports: [
+    GroupLinksComponent,
+    NgScrollbar,
+  ]
 })
 export class GroupIndicatorComponent {
-  @Input() group?: Group;
-
-  constructor() { }
+  group = input.required<Group>();
 }
