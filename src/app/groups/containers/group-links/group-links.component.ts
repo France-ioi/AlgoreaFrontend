07177dcc397f38384bcf51924dc7dbbac2ae42e7
Damien Leroy <dleroy@smad.be>
2025-09-19 14:57:35 +0200

    convert group links to control flows

--- src/app/groups/containers/group-links/group-links.component.ts
+++ src/app/groups/containers/group-links/group-links.component.ts
@@ -1,8 +1,8 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { GroupShortInfo } from '../../models/group';
 import { GroupLinkPipe } from 'src/app/pipes/groupLink';
 import { RouterLink } from '@angular/router';
-import { NgFor, NgIf, SlicePipe } from '@angular/common';
+import { SlicePipe } from '@angular/common';
 
 const defaultMaxItemsDisplay = 4;
 
@@ -11,12 +11,15 @@ const defaultMaxItemsDisplay = 4;
   templateUrl: './group-links.component.html',
   styleUrls: [ './group-links.component.scss' ],
   standalone: true,
-  imports: [ NgFor, RouterLink, NgIf, SlicePipe, GroupLinkPipe ]
+  imports: [
+    RouterLink,
+    SlicePipe,
+    GroupLinkPipe
+  ]
 })
 export class GroupLinksComponent {
-  @Input() items?: GroupShortInfo[];
-  maxItemsDisplay = input(defaultMaxItemsDisplay);
 
-  constructor() { }
+  items = input.required<GroupShortInfo[]>();
+  maxItemsDisplay = input(defaultMaxItemsDisplay);
 
 }
