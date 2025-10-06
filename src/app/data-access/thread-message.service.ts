4a6722d341800155cdfe247de47bd594330c217b
Damien Leroy <dleroy@smad.be>
2025-10-06 11:14:12 +0200

    implement sending message through the new rest API

--- src/app/data-access/thread-message.service.ts
+++ src/app/data-access/thread-message.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { APPCONFIG } from '../config';
 import { inject } from '@angular/core';
-import { ThreadId } from '../forum/models/threads';
 import { z } from 'zod';
 import { decodeSnakeCase } from '../utils/operators/decode';
+import { assertSuccess, SimpleActionResponse } from './action-response';
 
 const messageSchema = z.object({
   time: z.date(),
@@ -21,6 +21,18 @@ export class ThreadMessageService {
   private config = inject(APPCONFIG);
 
   constructor(private http: HttpClient) {}
+
+  create(message: string, options: { authToken: string }): Observable<void> {
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const headers = { Authorization: `Bearer ${options.authToken}` };
+
+    return this.http
+      .post<SimpleActionResponse>(`${this.config.slsApiUrl}/forum/message`, { message }, { headers })
+      .pipe(
+        map(assertSuccess),
+      );
+  }
 
   getAll(options: { authToken: string, limit?: number, from?: Date }): Observable<ThreadMessage[]> {
     // thread id is deduce from the token
