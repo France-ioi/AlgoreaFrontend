68361abc539cd5f05f5ba1bc94544e76095c84a7
Damien Leroy <dleroy@smad.be>
2025-10-06 13:14:26 +0200

    convert sls msg to thread events

--- src/app/forum/store/current-thread/current-thread.store.ts
+++ src/app/forum/store/current-thread/current-thread.store.ts
@@ -2,14 +2,13 @@ import { Thread, ThreadId } from '../../models/threads';
 import { FetchState, fetchingState } from 'src/app/utils/state';
 import { IncomingThreadEvent } from '../../data-access/websocket-messages/threads-inbound-events';
 import { ThreadEvent } from '../../models/thread-events';
-import { ThreadMessage } from 'src/app/data-access/thread-message.service';
 
 export interface State {
   visible: boolean, // must not be `true` if `id` is not defined
   id: ThreadId | null,
   info: FetchState<Thread>,
   logEvents: FetchState<ThreadEvent[]>,
-  slsEvents: FetchState<ThreadMessage[]>,
+  slsEvents: FetchState<ThreadEvent[]>,
   wsEvents: IncomingThreadEvent[],
 }
 
