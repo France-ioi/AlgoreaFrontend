c933c314feb00b2570b6204eca80edae0ea72e8f
Damien Leroy <dleroy@smad.be>
2025-11-12 19:03:46 +0100

    use the new backend service for logs in threads

--- src/app/forum/store/current-thread/event-fetching.effects.ts
+++ src/app/forum/store/current-thread/event-fetching.effects.ts
@@ -1,31 +1,25 @@
 import { createEffect } from '@ngrx/effects';
 import { inject } from '@angular/core';
-import { EMPTY, map, switchMap } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs';
 import { ActivityLogService } from 'src/app/data-access/activity-log.service';
 import { Store } from '@ngrx/store';
-import { UserSessionService } from 'src/app/services/user-session.service';
-import { combineLatest } from 'rxjs';
 import { fromForum } from '..';
 import { mapToFetchState, readyData } from 'src/app/utils/operators/state';
 import { eventFetchingActions } from './event-fetching.actions';
 import { convertActivityLogsToThreadEvents, convertThreadMessageToThreadEvents } from '../../models/thread-events-conversions';
 import { ThreadMessageService } from 'src/app/data-access/thread-message.service';
+import { isNotNull } from 'src/app/utils/null-undefined-predicates';
 
 export const logEventFetchingEffect = createEffect(
   (
     store$ = inject(Store),
-    userSessionService = inject(UserSessionService),
     activityLogService = inject(ActivityLogService),
-  ) => combineLatest([ store$.select(fromForum.selectThreadId), userSessionService.userProfile$ ]).pipe(
-    switchMap(([ threadId, profile ]) => {
-      if (!threadId) return EMPTY;
-      const isMine = threadId.participantId === profile.groupId;
-      const watchedGroupId = isMine ? undefined : threadId.participantId; // FIXME: will not work for teams
-      return activityLogService.getActivityLog(threadId.itemId, { watchedGroupId, limit: 11 }).pipe(
-        map(convertActivityLogsToThreadEvents),
-        mapToFetchState(),
-      );
-    }),
+  ) => store$.select(fromForum.selectThreadId).pipe(
+    filter(isNotNull),
+    switchMap(threadId => activityLogService.getThreadActivityLog(threadId, { limit: 11 }).pipe(
+      map(convertActivityLogsToThreadEvents),
+      mapToFetchState(),
+    )),
     map(fetchState => eventFetchingActions.logEventsFetchStateChanged({ fetchState })),
   ),
   { functional: true }
