f41c51de87028e52e677f5bda76971343d39554f
Aleksandr Semenov <semenov.information@gmail.com>
2025-11-16 19:59:13 +0200

    Replaces primeng dialog with custom one in group obs error

---
+++ src/app/containers/group-observation-error-modal/group-observation-error-modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { NotificationModalComponent } from 'src/app/ui-components/notification-modal/notification-modal.component';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+
+@Component({
+  selector: 'alg-group-observation-error-modal',
+  templateUrl: './group-observation-error-modal.component.html',
+  styleUrls: [ './group-observation-error-modal.component.scss' ],
+  standalone: true,
+  imports: [ NotificationModalComponent ],
+})
+export class GroupObservationErrorModalComponent {
+  error = signal(inject<{ isForbidden: boolean }>(DIALOG_DATA));
+  title = computed(() => (this.error().isForbidden ? $localize`Cannot observe this group` : $localize`Oops...`));
+
+  dialogRef = inject(DialogRef);
+}
