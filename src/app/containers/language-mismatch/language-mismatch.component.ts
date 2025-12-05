d8fe57689c7adad6b2667a9296db5b6f0f4def35
Aleksandr Semenov <semenov.information@gmail.com>
2025-12-05 18:20:21 +0200

    Replaces language mismatch modal with angular cdk modal

--- src/app/containers/language-mismatch/language-mismatch.component.ts
+++ src/app/containers/language-mismatch/language-mismatch.component.ts
@@ -1,30 +1,21 @@
-import { Component, OnDestroy } from '@angular/core';
-import { EMPTY } from 'rxjs';
-import { catchError, filter, map, retry, switchMap, takeUntil } from 'rxjs/operators';
-import { mapPending } from 'src/app/utils/operators/map-pending';
+import { Component, effect, inject } from '@angular/core';
+import { filter, map, takeUntil } from 'rxjs/operators';
 import { UserSessionService } from '../../services/user-session.service';
 import { LocaleService } from '../../services/localeService';
-import { SharedModule } from 'primeng/api';
-import { LoadingComponent } from '../../ui-components/loading/loading.component';
-import { DialogModule } from 'primeng/dialog';
-import { NgIf, AsyncPipe } from '@angular/common';
-import { ButtonComponent } from 'src/app/ui-components/button/button.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Dialog } from '@angular/cdk/dialog';
+import {
+  LanguageMismatchModalComponent
+} from 'src/app/containers/language-mismatch/language-mismatch-modal/language-mismatch-modal.component';
 
 @Component({
   selector: 'alg-language-mismatch',
   templateUrl: './language-mismatch.component.html',
   styleUrls: [ './language-mismatch.component.scss' ],
   standalone: true,
-  imports: [
-    NgIf,
-    DialogModule,
-    LoadingComponent,
-    SharedModule,
-    AsyncPipe,
-    ButtonComponent,
-  ],
 })
-export class LanguageMismatchComponent implements OnDestroy {
+export class LanguageMismatchComponent {
+  private dialogService = inject(Dialog);
 
   readonly currentLanguage = this.localeService.currentLang?.tag;
 
@@ -36,34 +27,18 @@ export class LanguageMismatchComponent implements OnDestroy {
     })),
     takeUntil(this.localeService.navigatingToNewLanguage$),
   );
-  updating = false;
+  languageMismatch = toSignal(this.languageMismatch$);
 
-  private updateTempUserLanguage = this.sessionService.userProfile$.pipe(
-    filter(profile => profile.tempUser && profile.defaultLanguage !== this.currentLanguage),
-    switchMap(() => (this.currentLanguage ? this.sessionService.updateCurrentUser({ default_language: this.currentLanguage }) : EMPTY)),
-    retry(3),
-    // An error is not that problematic, no need to break the app for the language of a temp user.
-    catchError(() => EMPTY),
-  ).subscribe();
+  openModalEffect = effect(() => {
+    const languageMismatch = this.languageMismatch();
+    if (languageMismatch) {
+      this.dialogService.open(LanguageMismatchModalComponent, { disableClose: true, data: languageMismatch });
+    }
+  });
 
   constructor(
     private localeService: LocaleService,
     private sessionService: UserSessionService,
   ) { }
-
-  onUpdateUserLanguage(language?: string): void {
-    if (!language) throw new Error('language should be defined');
-    this.sessionService.updateCurrentUser({ default_language: language })
-      .pipe(mapPending())
-      .subscribe(updating => (this.updating = updating));
-  }
-
-  onVisitPlatformInUserLanguage(language: string): void {
-    this.localeService.navigateTo(language);
-  }
-
-  ngOnDestroy(): void {
-    this.updateTempUserLanguage.unsubscribe();
-  }
 
 }
