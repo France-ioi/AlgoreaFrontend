8126ffe30e50e662f20e55d6c02fdaa1b640aad3
Damien Leroy <dleroy@smad.be>
2025-12-08 17:47:40 +0100

    use signal for thread message + many small improvements

--- src/app/forum/containers/thread-message/thread-message.component.ts
+++ src/app/forum/containers/thread-message/thread-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { RawItemRoute } from 'src/app/models/routing/item-route';
 import { UserInfo } from './thread-user-info';
 import { AllowDisplayCodeSnippet } from '../../../pipes/allowDisplayCodeSnippet';
@@ -9,7 +9,13 @@ import { ScoreRingComponent } from '../../../ui-components/score-ring/score-ring
 import { NgClass, NgTemplateOutlet, DatePipe } from '@angular/common';
 import { BreakLinesPipe } from '../../../pipes/breakLines';
 import { ThreadId } from '../../models/threads';
-import { isMessageEvent, ThreadEvent } from '../../models/thread-events';
+import {
+  IsAttemptStartedEventPipe,
+  isMessageEvent,
+  IsMessageEventPipe,
+  IsSubmissionEventPipe,
+  ThreadEvent
+} from '../../models/thread-events';
 import { RelativeTimePipe } from '../../../pipes/relativeTime';
 
 @Component({
@@ -28,20 +34,24 @@ import { RelativeTimePipe } from '../../../pipes/relativeTime';
     AllowDisplayCodeSnippet,
     BreakLinesPipe,
     RelativeTimePipe,
+    IsMessageEventPipe,
+    IsAttemptStartedEventPipe,
+    IsSubmissionEventPipe,
   ],
 })
-export class ThreadMessageComponent implements OnChanges {
-  @Input({ required: true }) threadId!: ThreadId;
-  @Input({ required: true }) event!: ThreadEvent;
-  @Input() userCache: UserInfo[] = [];
-  @Input() canCurrentUserLoadAnswers = false;
-  @Input() itemRoute?: RawItemRoute;
-  userInfo?: UserInfo;
+export class ThreadMessageComponent {
+  threadId = input.required<ThreadId>();
+  event = input.required<ThreadEvent>();
+  userCache = input<UserInfo[]>([]);
+  canCurrentUserLoadAnswers = input<boolean>(false);
+  itemRoute = input<RawItemRoute>();
 
-  ngOnChanges(): void {
-    const userId = isMessageEvent(this.event) ? this.event.authorId : this.threadId.participantId;
-    const cachedInfo = this.userCache.find(user => user.id === userId);
-    this.userInfo = cachedInfo ?? { id: userId, isCurrentUser: false, isThreadParticipant: false };
-  }
+  userInfo = computed<UserInfo>(() => {
+    const event = this.event();
+    const id = isMessageEvent(event) ? event.authorId : this.threadId().participantId;
+    const userInfo = this.userCache().find(user => user.id === id);
+    return userInfo ?? { id, isCurrentUser: false, isThreadParticipant: this.threadId().participantId === id };
+  });
 
 }
+
