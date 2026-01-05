a15ac408981a03e41381433415fb3f9217932be7
Aleksandr Semenov <semenov.information@gmail.com>
2026-01-05 11:51:01 +0200

    Makes deprecation, prefer inject es rules as off

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
       { type: [ 'element', 'attribute' ], prefix: 'alg', style: 'kebab-case' },
     ],
     '@angular-eslint/no-output-native': 'off',
+    '@angular-eslint/prefer-inject': 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
@@ -105,7 +106,7 @@ module.exports = {
       ]
     }],
     'jasmine/no-focused-tests': ['error'],
-    'deprecation/deprecation': ['error'],
+    'deprecation/deprecation': ['off'],
     '@ngrx/prefer-effect-callback-in-block-statement': 'off',
     '@typescript-eslint/unbound-method': ['error', {
       'ignoreStatic': true
