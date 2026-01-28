93ce7e1cbd3a07a719e2b78eb3430e7eaadba6a6
Damien Leroy <dleroy@smad.be>
2026-01-28 11:49:18 +0100

    handle submissions/grade ws message to merge them in the thread

--- src/app/forum/store/current-thread/websocket-incoming-message.actions.ts
+++ src/app/forum/store/current-thread/websocket-incoming-message.actions.ts
@@ -1,10 +1,11 @@
 import { createActionGroup, props } from '@ngrx/store';
-import { MessageEvent } from '../../models/thread-events';
+import { MessageEvent, SubmissionEvent } from '../../models/thread-events';
 import { ThreadId } from '../../models/threads';
 
 export const websocketIncomingMessageActions = createActionGroup({
   source: 'Websocket incoming message Effect',
   events: {
     forumMessageReceived: props<{ threadId: ThreadId, message: MessageEvent }>(),
+    forumSubmissionReceived: props<{ threadId: ThreadId, submission: SubmissionEvent }>(),
   },
 });
--- src/app/forum/store/current-thread/websocket-incoming-message.effects.ts
+++ src/app/forum/store/current-thread/websocket-incoming-message.effects.ts
@@ -2,22 +2,59 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { inject } from '@angular/core';
 import { filter, map } from 'rxjs';
 import { websocketClientActions } from '../websocket/websocket.actions';
-import { userMessageForumWsMessageSchema } from '../../models/websocket-forum-messages';
+import {
+  ForumWsMessage,
+  forumWsMessageSchema,
+  GradeUpdateForumWsMessage,
+  isGradeUpdateForumWsMessage,
+  isNewSubmissionForumWsMessage,
+  isUserMessageForumWsMessage,
+  NewSubmissionForumWsMessage,
+} from '../../models/websocket-forum-messages';
 import { websocketIncomingMessageActions } from './websocket-incoming-message.actions';
-import { convertWsMessageToMessageEvent } from '../../models/thread-events-conversions';
+import {
+  convertGradeUpdateWsMessageToSubmissionEvent,
+  convertNewSubmissionWsMessageToSubmissionEvent,
+  convertWsMessageToMessageEvent,
+} from '../../models/thread-events-conversions';
+
+function isSubmissionRelatedMessage(msg: ForumWsMessage): msg is NewSubmissionForumWsMessage | GradeUpdateForumWsMessage {
+  return isNewSubmissionForumWsMessage(msg) || isGradeUpdateForumWsMessage(msg);
+}
 
 export const websocketIncomingMessageEffect = createEffect(
   (
     actions$ = inject(Actions),
   ) => actions$.pipe(
     ofType(websocketClientActions.messageReceived),
-    map(({ message }) => userMessageForumWsMessageSchema.safeParse(message)),
+    map(({ message }) => forumWsMessageSchema.safeParse(message)),
     filter(result => result.success),
     map(result => result.data),
+    filter(isUserMessageForumWsMessage),
     map(message => websocketIncomingMessageActions.forumMessageReceived({
       threadId: { participantId: message.participantId, itemId: message.itemId },
       message: convertWsMessageToMessageEvent(message),
     })),
+  ),
+  { functional: true }
+);
+
+export const websocketIncomingSubmissionEffect = createEffect(
+  (
+    actions$ = inject(Actions),
+  ) => actions$.pipe(
+    ofType(websocketClientActions.messageReceived),
+    map(({ message }) => forumWsMessageSchema.safeParse(message)),
+    filter(result => result.success),
+    map(result => result.data),
+    filter(isSubmissionRelatedMessage),
+    map(message => {
+      const threadId = { participantId: message.participantId, itemId: message.itemId };
+      const submission = isNewSubmissionForumWsMessage(message)
+        ? convertNewSubmissionWsMessageToSubmissionEvent(message)
+        : convertGradeUpdateWsMessageToSubmissionEvent(message);
+      return websocketIncomingMessageActions.forumSubmissionReceived({ threadId, submission });
+    }),
   ),
   { functional: true }
 );
