ca4c7c9da151b6d3ef550fcafd0bd21f0784397e
Damien Leroy <dleroy@smad.be>
2026-03-03 15:54:44 +0100

    ensure the component work with touch

--- src/app/ui-components/user-link-with-actions/user-link-with-actions.component.ts
+++ src/app/ui-components/user-link-with-actions/user-link-with-actions.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input, signal } from '@angular/core';
 import { RouterLink, UrlTree } from '@angular/router';
 import { TooltipDirective } from '../tooltip/tooltip.directive';
 import { formatUser, UserBaseWithId } from 'src/app/groups/models/user';
@@ -11,13 +11,32 @@ import { rawGroupRoute } from 'src/app/models/routing/group-route';
   styleUrls: [ './user-link-with-actions.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [ RouterLink, TooltipDirective ],
+  host: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    '[class.actions-visible]': 'showActions()',
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    '(click)': 'onHostClick($event)',
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    '(document:click)': 'onDocumentClick()',
+  },
 })
 export class UserLinkWithActionsComponent {
   private groupRouter = inject(GroupRouter);
 
   user = input.required<UserBaseWithId>();
   observeLink = input<string | UrlTree | readonly unknown[]>();
+  showActions = signal(false);
 
   label = computed(() => formatUser(this.user()));
   profileLink = computed(() => this.groupRouter.url(rawGroupRoute({ id: this.user().id, isUser: true })));
+
+  onHostClick(event: Event): void {
+    if (window.matchMedia('(hover: hover)').matches) return;
+    event.stopPropagation();
+    this.showActions.update(v => !v);
+  }
+
+  onDocumentClick(): void {
+    this.showActions.set(false);
+  }
 }
