125d2225d7f97c84995d226faaead0baf41d099d
Damien Leroy <dleroy@smad.be>
2026-03-05 14:56:45 +0100

    basic community page

--- src/app/community/containers/community-page/community-page.component.ts
+++ src/app/community/containers/community-page/community-page.component.ts
@@ -1,16 +1,27 @@
 import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { CurrentContentService } from '../../../services/current-content.service';
+import { fromCurrentContent } from '../../../store/navigation/current-content/current-content.store';
+import { ContentDisplayType, LayoutService } from '../../../services/layout.service';
 
 @Component({
   selector: 'alg-community-page',
-  template: '<p i18n>Community (coming soon)</p>',
+  templateUrl: './community-page.component.html',
+  styleUrls: [ './community-page.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CommunityPageComponent implements OnInit, OnDestroy {
+  private store = inject(Store);
   private currentContent = inject(CurrentContentService);
+  private layoutService = inject(LayoutService);
 
   ngOnInit(): void {
     this.currentContent.replace({ type: 'community' });
+    this.layoutService.configure({ contentDisplayType: ContentDisplayType.Show });
+    this.store.dispatch(fromCurrentContent.contentPageActions.changeContent({
+      route: 'community',
+      title: $localize`Community`,
+    }));
   }
 
   ngOnDestroy(): void {
